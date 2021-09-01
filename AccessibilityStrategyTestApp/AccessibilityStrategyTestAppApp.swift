e1d2c6bcd6ba16e1567f714155c068e150586491
Guill Lo <guill.bout@gmail.com>
2021-09-02 01:52:54 +0800

    first commit

---
+++ AccessibilityStrategyTestApp/AccessibilityStrategyTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityStrategyTestAppApp.swift
+//  AccessibilityStrategyTestApp
+//
+//  Created by Guillaume Leclerc on 02/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityStrategyTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilityStrategyTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AccessibilityStrategyTestApp
+//
+//  Created by Guillaume Leclerc on 02/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
