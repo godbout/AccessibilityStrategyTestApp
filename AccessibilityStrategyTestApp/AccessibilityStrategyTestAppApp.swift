a437fb6b8906753e78f31708dc2329cddb0b5609
Guill Lo <guill.bout@gmail.com>
2021-09-02 01:55:42 +0800

    add UITestsView

--- AccessibilityStrategyTestApp/AccessibilityStrategyTestAppApp.swift
+++ AccessibilityStrategyTestApp/AccessibilityStrategyTestAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AccessibilityStrategyTestAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            UITestsView()
         }
     }
 }
---
+++ AccessibilityStrategyTestApp/UITestsView.swift
@@ -0,0 +1,54 @@
+import SwiftUI
+
+
+struct TextView: NSViewRepresentable {
+
+    @Binding var text: String
+    typealias NSViewType = NSTextView
+
+    func makeNSView(context: Context) -> NSTextView {
+        let nsTextView = NSTextView()
+        
+        nsTextView.font = NSFont.userFixedPitchFont(ofSize: 16)
+        nsTextView.isAutomaticQuoteSubstitutionEnabled = false
+        
+        return nsTextView
+    }
+
+    func updateNSView(_ nsView: NSTextView, context: Context) {
+        nsView.string = text
+    }
+
+}
+
+struct UITestsView: View {
+
+    @State var textFieldValue = ""
+    @State var textViewValue = ""
+
+    var body: some View {
+        VStack {
+            TextField("single line text field for test", text: $textFieldValue)
+                
+            if #available(macOS 11.0, *) {
+                TextEditor(text: $textViewValue)
+            } else {
+                TextView(text: $textViewValue)
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 200, maxHeight: .infinity)
+            }
+
+            Button("wo'hevah", action: {})
+        }
+        .font(.system(size: 16, weight: .regular, design: .monospaced))
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding()
+    }
+
+}
+
+
+struct UITestsView_Previews: PreviewProvider {
+    static var previews: some View {
+        UITestsView()
+    }
+}
