b2ad65bc6cddbc951856440e121fbb6a937a1611
Guill Lo <guill.bout@gmail.com>
2021-09-09 21:27:26 +0800

    update UI Tests for VM d now that block cursor is within move

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyVisualMode/Linewise/ASUI_VML_d_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyVisualMode/Linewise/ASUI_VML_d_Tests.swift
@@ -20,7 +20,7 @@ extension ASUI_VML_d_Tests {
 we gonna use VM
 d here and we suppose
 one extra line in between!
-      to go to non blank of the line
+      ⛱️o go to non blank of the line
 """
         app.textViews.firstMatch.tap()
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
@@ -34,10 +34,11 @@ one extra line in between!
                      
         XCTAssertEqual(accessibilityElement?.value, """
 we gonna use VM
-      to go to non blank of the line
+      ⛱️o go to non blank of the line
 """
         )
         XCTAssertEqual(accessibilityElement?.caretLocation, 22)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 2)
     }
     
     func test_that_the_caret_will_go_the_the_end_limit_of_the_next_line_if_the_next_line_is_just_made_of_spaces() {
@@ -66,6 +67,7 @@ some more
 """
         )
         XCTAssertEqual(accessibilityElement?.caretLocation, 23)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 1)
     }
     
     func test_that_if_the_next_line_is_only_blanks_the_caret_goes_to_the_limit_of_the_line_before_the_linefeed() {
@@ -93,11 +95,12 @@ d here and we suppose
 """
         )
         XCTAssertEqual(accessibilityElement?.caretLocation, 46)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 1)
     }
     
     func test_that_removing_the_last_line_puts_the_caret_at_the_first_non_blank_of_the_previous_line() {
         let textInAXFocusedElement = """
-   we gonna remove the last
+   ⛱️e gonna remove the last
 line and caret should go up
 and it would be beautiful
 """
@@ -111,10 +114,11 @@ and it would be beautiful
         let accessibilityElement = applyMoveBeingTested()
 
         XCTAssertEqual(accessibilityElement?.value, """
-   we gonna remove the last
+   ⛱️e gonna remove the last
 """
         )
         XCTAssertEqual(accessibilityElement?.caretLocation, 3)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 2)
     }
     
     func test_that_if_the_head_is_before_the_anchor_it_works() {
@@ -136,6 +140,7 @@ and it would be beautiful
 """
         )
         XCTAssertEqual(accessibilityElement?.caretLocation, 3)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 1)
     }
     
     func test_that_if_the_whole_text_is_to_be_deleted_well_it_gets_deleted_LOL() {
@@ -157,6 +162,7 @@ t
 
         XCTAssertEqual(accessibilityElement?.value, "")
         XCTAssertEqual(accessibilityElement?.caretLocation, 0)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
     }
     
 }
