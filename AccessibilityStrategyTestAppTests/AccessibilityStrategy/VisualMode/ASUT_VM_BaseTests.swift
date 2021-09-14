9d6edef5e79b6ac89e27e419924d5f9c0e966000
Guill Lo <guill.bout@gmail.com>
2021-09-14 22:09:22 +0800

    refactor UT and UI Tests for w, W, e, E into UT UI for wordMotionForward

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/ASUT_VM_BaseTests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/ASUT_VM_BaseTests.swift
@@ -4,13 +4,14 @@ import XCTest
 class ASVM_BaseTests: XCTestCase {
     
     var asVisualMode: AccessibilityStrategyVisualMode!
+    var textEngine = TextEngine()
     
     
     override func setUp() {
         super.setUp()
         
+        // TODO: check if it's needed in setUp or could be set above
         asVisualMode = AccessibilityStrategyVisualMode()
-//        KindaVimEngine.shared.enterVisualMode()
     }    
     
 }
---
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyVisualMode/Helpers/ASUI_VM_wordMotionForward_Tests.swift
@@ -0,0 +1,40 @@
+import XCTest
+import KeyCombination
+import AccessibilityStrategy
+
+
+// other tests in Unit Tests. the UI is just to test special cases.
+class UIASVM_wordMotionForward_Tests: ASUI_VM_BaseTests {
+    
+    func test_that_when_we_reach_the_anchor_and_will_reverse_anchor_and_head_the_w_move_does_not_block_and_moves_properly() {
+        let textInAXFocusedElement = """
+in Visual Mode Characterwise we
+always move from the anchor, not
+from the caret location
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+       
+        applyMove { asNormalMode.h(on: $0) }
+        applyMove { asNormalMode.k(on: $0) }
+        applyMove { asVisualMode.vForEnteringFromNormalMode(on: $0) }
+        applyMove { asVisualMode.bForVisualStyleCharacterwise(on: $0) }
+        let accessibilityElement = applyMove { asVisualMode.wForVisualStyleCharacterwise(on: $0) }
+
+        XCTAssertEqual(accessibilityElement?.caretLocation, 54)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 6)
+    }
+    
+    func test_that_the_head_is_getting_updated_properly() {
+        let textInAXFocusedElement = "we-have to updated caretLocation before selectedLength!"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+             
+        applyMove { asVisualMode.vForEnteringFromNormalMode(on: $0) }
+        applyMove { asVisualMode.zeroForVisualStyleCharacterwise(on: $0) }
+        _ = applyMove { asVisualMode.EForVisualStyleCharacterwise(on: $0) }
+
+        XCTAssertEqual(AccessibilityStrategyVisualMode.head, 6)
+    }
+    
+}
