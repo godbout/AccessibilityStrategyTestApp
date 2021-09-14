44750e6b81d683349b3ca237b201a1b695567d03
Guill Lo <guill.bout@gmail.com>
2021-09-14 22:53:30 +0800

    add wordMotionBackward tests, refactor b, B, ge, gE tests

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC_e_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC_e_Tests.swift
@@ -1,6 +1,2 @@
-@testable import AccessibilityStrategy
-import XCTest
-
-
-// calling wordMotionForward. UT are there, tested with different forward moves (w, W, E).
+// same as b but with wordMotionForward
 class ASUT_VMC_e_Tests: ASVM_BaseTests {}
