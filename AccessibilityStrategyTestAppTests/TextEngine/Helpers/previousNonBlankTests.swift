396a80b52c6ea4991cce8b6d27031290dcf9c9fa
Guill Lo <guill.bout@gmail.com>
2021-09-22 17:25:54 +0800

    rename findPreviousNonBlank to previousNonBlank

--- AccessibilityStrategyTestAppTests/TextEngine/Helpers/previousNonBlankTests.swift
+++ AccessibilityStrategyTestAppTests/TextEngine/Helpers/previousNonBlankTests.swift
@@ -2,16 +2,16 @@
 import XCTest
 
 
-class findPreviousNonBlankTests: TextEngineBaseTests {}
+class previousNonBlankTests: TextEngineBaseTests {}
 
 
 // Both
-extension findPreviousNonBlankTests {
+extension previousNonBlankTests {
     
     func test_that_in_normal_setting_it_finds_the_previous_non_blank_location() {
         let text = "those are        spaces!"
         
-        let location = textEngine.findPreviousNonBlank(startingAt: 15, in: text)
+        let location = textEngine.previousNonBlank(startingAt: 15, in: text)
         
         XCTAssertEqual(location, 8)          
     }
@@ -19,7 +19,7 @@ extension findPreviousNonBlankTests {
     func test_that_if_the_caret_is_on_a_single_blank_then_it_returns_the_correct_location() {
         let text = "those are not many spaces!"
         
-        let location = textEngine.findPreviousNonBlank(startingAt: 13, in: text)
+        let location = textEngine.previousNonBlank(startingAt: 13, in: text)
         
         XCTAssertEqual(location, 12)  
     }
@@ -27,7 +27,7 @@ extension findPreviousNonBlankTests {
     func test_that_if_there_is_no_non_blank_at_the_start_then_it_returns_nil() {
         let text = "           many spaces!"
         
-        let location = textEngine.findPreviousNonBlank(startingAt: 11, in: text)
+        let location = textEngine.previousNonBlank(startingAt: 11, in: text)
         
         XCTAssertNil(location) 
     }
@@ -35,7 +35,7 @@ extension findPreviousNonBlankTests {
     func test_that_it_does_not_crash_if_the_caret_is_at_the_beginning_of_the_text() {
         let text = "this is the text that may crash"
         
-        let location = textEngine.findPreviousNonBlank(startingAt: 0, in: text)
+        let location = textEngine.previousNonBlank(startingAt: 0, in: text)
         
         XCTAssertNil(location) 
     }
@@ -44,7 +44,7 @@ extension findPreviousNonBlankTests {
 
 
 // TextViews
-extension findPreviousNonBlankTests {
+extension previousNonBlankTests {
     
     func test_that_it_does_stop_at_linefeeds() {
         let text = """
@@ -52,7 +52,7 @@ shouldn't go to the previous
      line
 """
 
-        let location = textEngine.findPreviousNonBlank(startingAt: 37, in: text)
+        let location = textEngine.previousNonBlank(startingAt: 37, in: text)
         
         XCTAssertEqual(location, 32)
     }
@@ -62,12 +62,12 @@ shouldn't go to the previous
 
 // emojis
 // see beginningOfWordBackward for the blah blah
-extension findPreviousNonBlankTests {
+extension previousNonBlankTests {
     
     func test_that_it_handles_emojis() {
         let text = "😨️😨️😨️        😨️😨️"
         
-        let location = textEngine.findPreviousNonBlank(startingAt: 17, in: text)
+        let location = textEngine.previousNonBlank(startingAt: 17, in: text)
         
         XCTAssertEqual(location, 6)
     }
