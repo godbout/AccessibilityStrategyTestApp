e43a03d7d617c23f5d299557c4ca0a9950eeb5a8
Guill Lo <guill.bout@gmail.com>
2021-09-28 17:19:41 +0800

    hopefully make o wrapped work on ci

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_o_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_o_Tests.swift
@@ -19,7 +19,7 @@ extension ASUI_NM_o_Tests {
    
     func test_that_if_a_line_does_not_end_with_a_linefeed_and_it_is_not_the_last_line_then_it_does_insert_two_linefeeds_and_reposition_the_caret_between_them() {
         let textInAXFocusedElement = """
-that's 😀️ a multiline 😀️😀️ and a long 😀️😀️ one that will be wrapped somewhere but we 😀️ don't know where LOL and 😀️😀️ i have to 😀️ test that shit
+that's 😀️ a multiline 😀️😀️ and a long 😀️😀️ one that will be wrapped somewhere but we 😀️ don't know where LOL and i have to 😀️ test that shit
 """
 
         
@@ -30,18 +30,18 @@ that's 😀️ a multiline 😀️😀️ and a long 😀️😀️ one that wil
         let accessibilityElement = applyMoveBeingTested()
         
         XCTAssertEqual(accessibilityElement?.text.value, """
-that's 😀️ a multiline 😀️😀️ and a long 😀️😀️ one that will be wrapped somewhere but we 😀️ don't know where LOL and 😀️
+that's 😀️ a multiline 😀️😀️ and a long 😀️😀️ one that will be wrapped somewhere but we 😀️ don't know where LOL and i 
 
-😀️ i have to 😀️ test that shit
+have to 😀️ test that shit
 """
         )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 123)
+        XCTAssertEqual(accessibilityElement?.caretLocation, 122)
         XCTAssertEqual(accessibilityElement?.selectedLength, 0)
     }
     
     func test_that_if_a_line_does_not_end_with_a_linefeed_but_is_the_last_line_then_it_works_normally_and_only_inserts_one_linefeed() {
         let textInAXFocusedElement = """
-that's 😀️ a multiline 😀️😀️ and a long 😀️😀️ one that will be wrapped somewhere but we 😀️ don't know where LOL and 😀️😀️ i have to 😀️ test that shit
+that's 😀️ a multiline 😀️😀️ and a long 😀️😀️ one that will be wrapped somewhere but we 😀️ don't know where LOL and i have to 😀️ test that shit
 """
         app.textViews.firstMatch.tap()
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
@@ -50,11 +50,11 @@ that's 😀️ a multiline 😀️😀️ and a long 😀️😀️ one that wil
         let accessibilityElement = applyMoveBeingTested()
         
         XCTAssertEqual(accessibilityElement?.text.value, """
-that's 😀️ a multiline 😀️😀️ and a long 😀️😀️ one that will be wrapped somewhere but we 😀️ don't know where LOL and 😀️😀️ i have to 😀️ test that shit
+that's 😀️ a multiline 😀️😀️ and a long 😀️😀️ one that will be wrapped somewhere but we 😀️ don't know where LOL and i have to 😀️ test that shit
 
 """
         )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 155)
+        XCTAssertEqual(accessibilityElement?.caretLocation, 148)
         XCTAssertEqual(accessibilityElement?.selectedLength, 0)
     }
     
