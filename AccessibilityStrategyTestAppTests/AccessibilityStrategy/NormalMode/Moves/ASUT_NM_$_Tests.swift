ebdb805c53bcc4551622eb155e4c659f22ef5cc6
Guill Lo <guill.bout@gmail.com>
2021-09-29 23:23:48 +0800

    add tests for NM $

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_$_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_$_Tests.swift
@@ -2,47 +2,171 @@ import XCTest
 @testable import AccessibilityStrategy
 
 
-// $ uses TE endOfLine. the location is fully tested there.
-// here we check the rest, i.e. length and text.
+// $ uses TE endOfLine. plenty of tests there.
+// here so more to be extra sure. cheap UT aww.
 class ASUT_NM_$_Tests: ASNM_BaseTests {
     
-    private func applyMove(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         return asNormalMode.dollarSign(on: element) 
     }
     
 }
 
 
-// Both
+// line
 extension ASUT_NM_$_Tests {
-
-    func test_that_it_ends_at_the_right_place_and_handles_emojis_too() {
+    
+    func test_conspicuously_that_it_does_not_stop_at_screen_lines_lines() {
         let text = """
-here we go with the $
-that goes at the en😀️
-of a real line!
+this move stops at screen lines, which means it will
+stop even without a linefeed. that's how special it is.
 """
         let element = AccessibilityTextElement(
             role: .textArea,
             value: text,
-            length: 60,
-            caretLocation: 29,
+            length: 108,
+            caretLocation: 5,
             selectedLength: 1,
-            selectedText: "e",
+            selectedText: "m",
             currentLine: AccessibilityTextElementLine(
                 fullTextValue: text,
-                fullTextLength: 60,
-                number: 2,
-                start: 22,
-                end: 45
+                fullTextLength: 108,
+                number: 1,
+                start: 0,
+                end: 33
             )
         )
         
-        let returnedElement = applyMove(on: element)
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 51)
+        XCTAssertEqual(returnedElement?.selectedLength, 1)
+        XCTAssertNil(returnedElement?.selectedText)
+    }
+     
+}
         
-        XCTAssertEqual(returnedElement?.caretLocation, 41)
+
+// Both
+extension ASUT_NM_$_Tests {
+    
+    func test_that_if_the_line_does_not_end_with_linefeed_it_goes_one_character_before_the_end() {
+        let text = "hello world and that's a long one that we gonna wrap 🗺️"
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 56,
+            caretLocation: 36,
+            selectedLength: 1,
+            selectedText: "a",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 56,
+                number: 2,
+                start: 30,
+                end: 56
+            )
+        )
+
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 53)
         XCTAssertEqual(returnedElement?.selectedLength, 3)
         XCTAssertNil(returnedElement?.selectedText)
     }
     
+    func test_that_it_sets_the_ATE_globalColumnNumber_to_nil() {
+        let text = """
+when using $
+the globalColumnNumber
+is set to nil so that next
+j or k will go to the line endLimit
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 98,
+            caretLocation: 49,
+            selectedLength: 1,
+            selectedText: " ",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 98,
+                number: 3,
+                start: 36,
+                end: 63
+            )
+        )
+        
+        AccessibilityTextElement.globalColumnNumber = 17
+        
+        _ = applyMoveBeingTested(on: element)
+
+        XCTAssertNil(AccessibilityTextElement.globalColumnNumber)
+    }
+
+}
+    
+
+// TextViews
+extension ASUT_NM_$_Tests {
+    
+    func test_that_if_the_line_ends_with_a_linefeed_it_goes_two_characters_before_the_end() {
+        let text = """
+indeed that is a multiline
+and yes my friend they all
+gonna be wrapped
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 70,
+            caretLocation: 18,
+            selectedLength: 1,
+            selectedText: "u",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 70,
+                number: 2,
+                start: 17,
+                end: 27
+            )
+        )
+
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 25)
+        XCTAssertEqual(returnedElement?.selectedLength, 1)
+        XCTAssertNil(returnedElement?.selectedText)
+    }
+    
+    func test_that_if_a_line_is_empty_it_does_not_move() {
+        let text = """
+g$ shouldn't go up one else
+
+it's a bug! my friend hehehehe
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 59,
+            caretLocation: 28,
+            selectedLength: 1,
+            selectedText: "\n",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 59,
+                number: 3,
+                start: 28,
+                end: 29
+            )
+        )
+
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 28)
+        XCTAssertEqual(returnedElement?.selectedLength, 1)
+        XCTAssertNil(returnedElement?.selectedText)
+    }
+
 }
