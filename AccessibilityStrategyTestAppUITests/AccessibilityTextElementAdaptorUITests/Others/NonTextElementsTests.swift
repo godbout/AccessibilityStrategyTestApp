2d9e035a1f32ec8533fc3c55e4bd82d45aa3181a
Guill Lo <guill.bout@gmail.com>
2021-10-01 18:30:13 +0800

    remove test that tried to push a button to AXUIElement. made no sense now

--- AccessibilityStrategyTestAppUITests/AccessibilityTextElementAdaptorUITests/Others/NonTextElementsTests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityTextElementAdaptorUITests/Others/NonTextElementsTests.swift
@@ -25,24 +25,3 @@ extension NonTextElementsTests {
     }
 
 }
-
-
-// to AXUIElement
-extension NonTextElementsTests {
-
-    func test_that_trying_to_convert_an_AccessibilityTextElement_to_an_AXUIElement_button_returns_false() {
-        let accessibilityElement = AccessibilityTextElement(
-            value: "hi i'm a button",
-            length: 25,
-            caretLocation: 5,
-            selectedLength: 1
-        )
-
-        app.buttons.firstMatch.tap()
-
-        let conversionSucceeded = AccessibilityTextElementAdaptor.toAXFocusedElement(from: accessibilityElement)
-
-        XCTAssertFalse(conversionSucceeded)
-    }
-
-}
