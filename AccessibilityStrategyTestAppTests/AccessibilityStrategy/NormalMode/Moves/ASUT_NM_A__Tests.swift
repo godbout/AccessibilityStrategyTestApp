2377f7b47d5ce7bfa37df0cc8a34b51e478c032c
Guill Lo <guill.bout@gmail.com>
2021-10-02 20:36:14 +0800

    add the line test for NM A

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_A__Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_A__Tests.swift
@@ -2,16 +2,50 @@
 import XCTest
 
 
-class ASNM_A__Tests: ASNM_BaseTests {
+class ASUT_NM_A__Tests: ASNM_BaseTests {
     
-    private func applyMove(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         return asNormalMode.A(on: element) 
     }
     
 }
 
 
-extension ASNM_A__Tests {
+// line
+extension ASUT_NM_A__Tests {
+    
+    func test_conspicuously_that_it_does_not_stop_at_screen_lines() {
+        let text = """
+this move does not stop at screen lines. it will just pass by
+them like nothing happened. that's how special it is.
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 115,
+            caretLocation: 29,
+            selectedLength: 1,
+            selectedText: "r",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 115,
+                number: 2,
+                start: 27,
+                end: 54
+            )
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 61)
+        XCTAssertEqual(returnedElement?.selectedLength, 0)
+        XCTAssertNil(returnedElement?.selectedText)
+    }
+     
+}
+
+
+extension ASUT_NM_A__Tests {
     
     func test_that_if_a_file_line_ends_with_a_linefeed_it_goes_after_the_last_visible_character_of_that_line() {
         let text = "yes A now goes to the end of file lines rather than screen lines because i was a dumbass LMAO"
@@ -31,7 +65,7 @@ extension ASNM_A__Tests {
             )
         )	
         
-        let returnedElement = applyMove(on: element)
+        let returnedElement = applyMoveBeingTested(on: element)
         
         XCTAssertEqual(returnedElement?.caretLocation, 93)
         XCTAssertEqual(returnedElement?.selectedLength, 0)
@@ -60,7 +94,7 @@ LMAO what a dumbass i am
             )
         )
         
-        let returnedElement = applyMove(on: element)
+        let returnedElement = applyMoveBeingTested(on: element)
         
         XCTAssertEqual(returnedElement?.caretLocation, 115)
         XCTAssertEqual(returnedElement?.selectedLength, 0)
@@ -89,7 +123,7 @@ and i'm doing this not because i'm a genius but because i'm pretty dumb LMAO
             )
         )
             
-       let returnedElement = applyMove(on: element)
+       let returnedElement = applyMoveBeingTested(on: element)
        
        XCTAssertEqual(returnedElement?.caretLocation, 60)
        XCTAssertEqual(returnedElement?.selectedLength, 0)
