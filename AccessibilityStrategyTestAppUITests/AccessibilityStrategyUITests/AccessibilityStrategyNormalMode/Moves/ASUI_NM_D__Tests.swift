37448fc352d122ef6984f4438de26044b095bbe7
Guill Lo <guill.bout@gmail.com>
2021-10-03 13:10:02 +0800

    update NM D UT to handle file lines

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_D__Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_D__Tests.swift
@@ -14,33 +14,29 @@ class UIASNM_D_Tests: ASUI_NM_BaseTests {
 }
 
 
-// yes, only one test. i know you will want to add 32 ones when you see that in 2 weeks
+// yes, only one test. i know you will want to add 32 other ones when you see that in 2 weeks
 // but here's the reason:
 // 1. as stated above, here we only need to test the caret repositioning (D calls C, C is already tested)
 // 2. the caret position will always go to the line endLimit, whether the line is empty or not. this is
-// tested in the endLimit tests. so where we mostly have nothing to do! :D
+// tested in the endLimit tests. so here we mostly have nothing to do. :D
 extension UIASNM_D_Tests {
 
     func test_that_in_any_case_the_caret_location_will_end_up_at_the_line_end_limit() {
         let textInAXFocusedElement = """
-D will delete till the end of line but not
-the linefeed (tested in C) and will go to the end
-limit even if the line is empty which😂️means it will not
-up one line and this is tested in the endLimit!
+D will delete till the end of line but not the linefeed (tested in C) and will go to the end limit even if the line is empty
+which😂️means it will not up one line and this is tested in the endLimit!
 """
         app.textViews.firstMatch.tap()
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
         
-        applyMove { asNormalMode.h(on: $0) }
-        applyMove { asNormalMode.k(on: $0) }
-        applyMove { asNormalMode.b(on: $0) }
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.w(on: $0) }
+        applyMove { asNormalMode.l(on: $0) }
         let accessibilityElement = applyMoveBeingTested()
         
         XCTAssertEqual(accessibilityElement?.text.value, """
-D will delete till the end of line but not
-the linefeed (tested in C) and will go to the end
-limit even if the line is empty which😂️
-up one line and this is tested in the endLimit!
+D will delete till the end of line but not the linefeed (tested in C) and will go to the end limit even if the line is empty
+which😂️
 """
         )
         XCTAssertEqual(accessibilityElement?.caretLocation, 130)
