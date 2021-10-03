6be9060f911d4825d62af5adcb61582bc17714ce
Guill Lo <guill.bout@gmail.com>
2021-10-03 17:51:41 +0800

    update UT for NM yy

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_yy_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_yy_Tests.swift
@@ -3,14 +3,50 @@ import XCTest
 
 
 // yy doesn't touch the caret position or anything else, it just copies
-// the line into the NSPasteBoard, so i guess we need to check this here
+// the line into the NSPasteBoard.
 class ASUT_NM_yy_Tests: ASNM_BaseTests {
     
-    private func applyMove(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         return asNormalMode.yy(on: element) 
     }
     
 }
+
+
+// line
+extension ASUT_NM_yy_Tests {
+    
+    func test_conspicuously_that_it_does_not_stop_at_screen_lines() {
+        let text = """
+this move does not stop at screen lines. it will just pass by
+them like nothing happened. that's how special it is.
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 115,
+            caretLocation: 40,
+            selectedLength: 1,
+            selectedText: " ",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 115,
+                number: 2,
+                start: 27,
+                end: 54
+            )
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "this move does not stop at screen lines. it will just pass by\n")
+        XCTAssertEqual(returnedElement?.caretLocation, 40)
+        XCTAssertEqual(returnedElement?.selectedLength, 1)
+        XCTAssertNil(returnedElement?.selectedText)
+    }
+     
+}
+
 
 
 // Both
@@ -34,9 +70,10 @@ extension ASUT_NM_yy_Tests {
             )
         )
         
-        let returnedElement = applyMove(on: element)
+        let returnedElement = applyMoveBeingTested(on: element)
         
         XCTAssertEqual(NSPasteboard.general.string(forType: .string), text)
+        XCTAssertEqual(returnedElement?.caretLocation, 12)
         XCTAssertEqual(returnedElement?.selectedLength, 1)
         XCTAssertNil(returnedElement?.selectedText)
     }
@@ -44,28 +81,29 @@ extension ASUT_NM_yy_Tests {
     func test_that_it_includes_the_end_of_the_line_linefeed() {
         let text = 
 """
-multiple lines
-my friend
+some very long multiple lines just to be sure that it
+workd properly my friend
 """
         let element = AccessibilityTextElement(
             role: .textArea,
             value: text,
-            length: 24,
-            caretLocation: 3,
+            length: 78,
+            caretLocation: 39,
             selectedLength: 1,
-            selectedText: "t",
+            selectedText: "e",
             currentLine: AccessibilityTextElementLine(
                 fullTextValue: text,
-                fullTextLength: 24,
-                number: 1,
-                start: 0,
-                end: 15
+                fullTextLength: 78,
+                number: 2,
+                start: 24,
+                end: 46
             )
         )
         
-        let returnedElement = applyMove(on: element)
+        let returnedElement = applyMoveBeingTested(on: element)
         
-        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "multiple lines\n")
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "some very long multiple lines just to be sure that it\n")
+        XCTAssertEqual(returnedElement?.caretLocation, 39)
         XCTAssertEqual(returnedElement?.selectedLength, 1)
         XCTAssertNil(returnedElement?.selectedText)
     }
@@ -73,34 +111,35 @@ my friend
     func test_that_it_does_not_include_the_linefeed_of_the_previous_line() {
         let text = 
             """
-multiple lines
-again
+some more long multiple lines for you because you're my best friend
+again yes i know i've said 😂️ this but i need them long
 my friend
 """
         let element = AccessibilityTextElement(
             role: .textArea,
             value: text,
-            length: 30,
-            caretLocation: 17,
-            selectedLength: 1,
-            selectedText: "a",
+            length: 135,
+            caretLocation: 95,
+            selectedLength: 3,
+            selectedText: "😂️",
             currentLine: AccessibilityTextElementLine(
                 fullTextValue: text,
-                fullTextLength: 30,
-                number: 2,
-                start: 15,
-                end: 21
+                fullTextLength: 135,
+                number: 5,
+                start: 95,
+                end: 120
             )
         )
         
-        let returnedElement = applyMove(on: element)
+        let returnedElement = applyMoveBeingTested(on: element)
         
-        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "again\n")
-        XCTAssertEqual(returnedElement?.selectedLength, 1)
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "again yes i know i've said 😂️ this but i need them long\n")
+        XCTAssertEqual(returnedElement?.caretLocation, 95)
+        XCTAssertEqual(returnedElement?.selectedLength, 3)
         XCTAssertNil(returnedElement?.selectedText)
     }
     
-    func test_that_if_the_TextElement_is_empty_it_works_and_copies_the_empty_line() {
+    func test_that_if_the_text_is_empty_it_works_and_copies_the_empty_line() {
         let text = ""
         let element = AccessibilityTextElement(
             role: .textField,
@@ -121,14 +160,15 @@ my friend
         NSPasteboard.general.clearContents()
         NSPasteboard.general.setString("test 1 of The 3 Cases", forType: .string)
         
-        let returnedElement = applyMove(on: element)
+        let returnedElement = applyMoveBeingTested(on: element)
         
         XCTAssertEqual(NSPasteboard.general.string(forType: .string), "")
+        XCTAssertEqual(returnedElement?.caretLocation, 0)
         XCTAssertEqual(returnedElement?.selectedLength, 0)
         XCTAssertNil(returnedElement?.selectedText)
     }
     
-    func test_that_if_the_caret_is_at_the_last_character_of_the_TextElement_and_on_an_empty_line_on_its_own_it_works_and_copies_the_line() {
+    func test_that_if_the_caret_is_at_the_last_character_of_the_text_and_on_an_empty_line_on_its_own_it_works_and_copies_the_line() {
         let text = """
 caret is on its
 own empty
@@ -154,50 +194,12 @@ line
         NSPasteboard.general.clearContents()
         NSPasteboard.general.setString("test 3 of The 3 Cases", forType: .string)
         
-        let returnedElement = applyMove(on: element)
+        let returnedElement = applyMoveBeingTested(on: element)
         
         XCTAssertEqual(NSPasteboard.general.string(forType: .string), "")
+        XCTAssertEqual(returnedElement?.caretLocation, 31)
         XCTAssertEqual(returnedElement?.selectedLength, 0)
         XCTAssertNil(returnedElement?.selectedText)
     }    
     
 }
-
-
-// emojis
-extension ASUT_NM_yy_Tests {
-    
-    func test_that_it_handles_emojis() {
-        let text = """
-😫️eed🍃️🍃️🍃️ to deal wi🛩️🛩️th
-those 🍃️🍃️🍃️🍃️🍃️🍃️ faces 🥺️☹️😂️
-"""
-        let element = AccessibilityTextElement(
-            role: .textArea,
-            value: text,
-            length: 74,
-            caretLocation: 12,
-            selectedLength: 3,
-            selectedText: "🍃️",
-            currentLine: AccessibilityTextElementLine(
-                fullTextValue: text,
-                fullTextLength: 74,
-                number: 1,
-                start: 0,
-                end: 35
-            )
-        )
-        
-        NSPasteboard.general.clearContents()
-        NSPasteboard.general.setString("", forType: .string)
-        
-        let returnedElement = applyMove(on: element)
-        
-        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "😫️eed🍃️🍃️🍃️ to deal wi🛩️🛩️th\n")
-        XCTAssertEqual(returnedElement?.selectedLength, 3)
-        XCTAssertNil(returnedElement?.selectedText)
-    }
-    
-}
-
-
