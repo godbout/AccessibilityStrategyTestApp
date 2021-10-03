65030b0160e9e9bf8da7fb99431ca69141cc4b5e
Guill Lo <guill.bout@gmail.com>
2021-10-03 21:08:18 +0800

    add FileLine ASUT for O

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_O__Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_O__Tests.swift
@@ -7,7 +7,47 @@ import XCTest
 // reposition the caret after pushing the new selectedText. therefore, unfortunately
 // we have to call the ATEAdaptor from within the move. hence, tests have to be made
 // in the UI Tests, as it's gonna modify the buffer. so most tests for O are there.
-class ASNM_O__Tests: ASNM_BaseTests {}
+class ASNM_O__Tests: ASNM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+        return asNormalMode.O(on: element) 
+    }
+    
+}
+
+
+// line
+extension ASNM_O__Tests {
+    
+    func test_conspicuously_that_it_does_not_stop_at_screen_lines() {
+        let text = """
+this move does not stop at screen lines. it will just pass by
+them like nothing happened. that's how special it is.
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 115,
+            caretLocation: 92,
+            selectedLength: 1,
+            selectedText: "a",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 115,
+                number: 5,
+                start: 80,
+                end: 101
+            )
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 62)
+        XCTAssertEqual(returnedElement?.selectedLength, 0)
+        XCTAssertEqual(returnedElement?.selectedText, "\n")
+    }
+     
+}
 
 
 // TextFields
@@ -31,7 +71,7 @@ extension ASNM_O__Tests {
             )
         )
         
-        let returnedElement = asNormalMode.O(on: element)
+		let returnedElement = applyMoveBeingTested(on: element)
         
         XCTAssertNil(returnedElement?.selectedText)
     }
