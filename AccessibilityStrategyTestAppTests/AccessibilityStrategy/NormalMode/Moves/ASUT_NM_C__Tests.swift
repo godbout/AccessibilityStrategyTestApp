3e0d08596124f8ba0261272b54111d171c4b2e53
Guill Lo <guill.bout@gmail.com>
2021-10-03 12:59:26 +0800

    update NM C UT to handle file lines

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_C__Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_C__Tests.swift
@@ -2,139 +2,133 @@
 import XCTest
 
 
-class ASNM_C__Tests: ASNM_BaseTests {
+class ASUT_NM_C__Tests: ASNM_BaseTests {
     
-    private func applyMove(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         return asNormalMode.C(on: element) 
     }
     
 }
 
 
-// TextField
-extension ASNM_C__Tests {
+// line
+extension ASUT_NM_C__Tests {
     
-    func test_that_in_normal_setting_it_deletes_from_the_caret_to_the_end_of_the_line() {
-        let text = "C should delete from the caret to the end of the line"
-        let element = AccessibilityTextElement(
-            role: .textField,
-            value: text,
-            length: 53,
-            caretLocation: 28,
-            selectedLength: 1,
-            selectedText: "e",
-            currentLine: AccessibilityTextElementLine(
-                fullTextValue: text,
-                fullTextLength: 53,
-                number: 1,
-                start: 0,
-                end: 53
-            )
-        )
-        
-        let returnedElement = applyMove(on: element)
-        
-        XCTAssertEqual(returnedElement?.caretLocation, 28)
-        XCTAssertEqual(returnedElement?.selectedLength, 25)
-        XCTAssertEqual(returnedElement?.selectedText, "")
-    }
-    
-}
-
-
-// TextViews
-extension ASNM_C__Tests {
-    
-    func test_that_in_normal_setting_it_deletes_from_the_caret_to_before_the_linefeed_at_the_end_of_the_line() {
+    func test_conspicuously_that_it_does_not_stop_at_screen_lines() {
         let text = """
-C should delete the second line
-but without the linefeed!
-else everything gonna stuck
+this move does not stop at screen lines. it will jus🚬️ pass by
+them like nothing happened. that's how special it is.
 """
         let element = AccessibilityTextElement(
             role: .textArea,
             value: text,
-            length: 85,
-            caretLocation: 40,
-            selectedLength: 1,
-            selectedText: "o",
+            length: 118,
+            caretLocation: 52,
+            selectedLength: 3,
+            selectedText: "🚬️",
             currentLine: AccessibilityTextElementLine(
                 fullTextValue: text,
-                fullTextLength: 85,
-                number: 2,
-                start: 32,
-                end: 58
+                fullTextLength: 118,
+                number: 3,
+                start: 49,
+                end: 64
+            )
+        )
+	
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 52)
+        XCTAssertEqual(returnedElement?.selectedLength, 12)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+     
+}
+
+
+// both
+extension ASUT_NM_C__Tests {
+    
+    func test_that_if_a_file_line_ends_with_a_linefeed_it_deletes_from_the_caret_to_before_that_linefeed() {
+        let text = """
+C will now work with file lines and is supposed to delete from the caret ☀️ to before the linefeed
+and of course this is in the case there is a linefeed at the end of the line.
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 177,
+            caretLocation: 55,
+            selectedLength: 1,
+            selectedText: "t",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 177,
+                number: 3,
+                start: 51,
+                end: 76
             )
         )
         
-        let returnedElement = applyMove(on: element)
+        let returnedElement = applyMoveBeingTested(on: element)
         
-        XCTAssertEqual(returnedElement?.caretLocation, 40)
-        XCTAssertEqual(returnedElement?.selectedLength, 17)
+        XCTAssertEqual(returnedElement?.caretLocation, 55)
+        XCTAssertEqual(returnedElement?.selectedLength, 43)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+    
+    func test_that_if_a_file_line_does_not_end_with_a_linefeed_it_deletes_from_the_caret_to_the_end_of_the_line() {
+		let text = "this time the line will not end with a linefeed so C should delete from the caret till the end!"
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 95,
+            caretLocation: 11,
+            selectedLength: 1,
+            selectedText: "h",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 95,
+                number: 1,
+                start: 0,
+                end: 39
+            )
+        )
+                
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 11)
+        XCTAssertEqual(returnedElement?.selectedLength, 84)
         XCTAssertEqual(returnedElement?.selectedText, "")
     }
     
     func test_that_it_does_not_delete_the_linefeed_even_for_an_empty_line() {
         let text = """
-now we have an empty line
+now we have an empty line and C should behave
 
-and that shouldn't be deleted by C
+and not delete that fucking shit
 """
         let element = AccessibilityTextElement(
             role: .textArea,
             value: text,
-            length: 61,
-            caretLocation: 26,
+            length: 80,
+            caretLocation: 46,
             selectedLength: 1,
             selectedText: "\n",
             currentLine: AccessibilityTextElementLine(
                 fullTextValue: text,
-                fullTextLength: 61,
-                number: 2,
-                start: 26,
-                end: 27
-                )
-            )
-        
-        let returnedElement = applyMove(on: element)
-        
-        XCTAssertEqual(returnedElement?.caretLocation, 26)
-        XCTAssertEqual(returnedElement?.selectedLength, 0)
-        XCTAssertNil(returnedElement?.selectedText)
-    }
-    
-}
-
-
-// emojis
-extension ASNM_C__Tests {
-    
-    func test_that_it_handles_emojis() {
-        let text = """
-need to deal with
-those faces 🥺️☹️😂️
-"""
-        let element = AccessibilityTextElement(
-            role: .textArea,
-            value: text,
-            length: 38,
-            caretLocation: 33,
-            selectedLength: 1,
-            selectedText: "a",
-            currentLine: AccessibilityTextElementLine(
-                fullTextValue: text,
-                fullTextLength: 38,
-                number: 2,
-                start: 18,
-                end: 38
+                fullTextLength: 80,
+                number: 4,
+                start: 46,
+                end: 47
             )
         )
         
-        let returnedElement = applyMove(on: element)
+        let returnedElement = applyMoveBeingTested(on: element)
         
-        XCTAssertEqual(returnedElement?.caretLocation, 33)
-        XCTAssertEqual(returnedElement?.selectedLength, 5)
-        XCTAssertEqual(returnedElement?.selectedText, "")
+        XCTAssertEqual(returnedElement?.caretLocation, 46)
+        XCTAssertEqual(returnedElement?.selectedLength, 0)
+        XCTAssertNil(returnedElement?.selectedText)
     }
     
 }
