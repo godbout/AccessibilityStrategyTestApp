4dc553d148dd3e21533e5ff55ed374d0300222cf
Guill Lo <guill.bout@gmail.com>
2021-10-05 17:54:43 +0800

    update NM yt UT to handle FileLines

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_yt_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_yt_Tests.swift
@@ -7,10 +7,45 @@ import XCTest
 // we don't bother with caretLocation and stuff coz they're completely untouched
 class ASUT_NM_yt_Tests: ASNM_BaseTests {
     
-    private func applyMove(to character: Character, on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+    private func applyMoveBeingTested(to character: Character, on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         return asNormalMode.yt(to: character, on: element) 
     }
     
+}
+
+
+// line
+extension ASUT_NM_yt_Tests {
+    
+    func test_conspicuously_that_it_does_not_stop_at_screen_lines() {
+        let text = """
+this move does not stop at screen lines. it will just pass by
+them like nothin🇫🇷️ happened. that's how special it is.
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 119,
+            caretLocation: 78,
+            selectedLength: 26,
+            selectedText: "🇫🇷️ happened. that's how",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 119,
+                number: 3,
+                start: 62,
+                end: 119
+            )
+        )
+        
+        let returnedElement = applyMoveBeingTested(to: "w", on: element)
+
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "🇫🇷️ happened. that's ho")
+        XCTAssertEqual(returnedElement?.caretLocation, 78)
+        XCTAssertEqual(returnedElement?.selectedLength, 5)
+        XCTAssertNil(returnedElement?.selectedText)
+    }
+     
 }
 
 
@@ -35,7 +70,7 @@ extension ASUT_NM_yt_Tests {
             )
         )
         
-        let returnedElement = applyMove(to: "s", on: element)
+        let returnedElement = applyMoveBeingTested(to: "s", on: element)
         
         XCTAssertEqual(NSPasteboard.general.string(forType: .string), "e yt on thi")
         XCTAssertEqual(returnedElement?.selectedLength, 1)
@@ -60,14 +95,14 @@ that is not there
                 fullTextLength: 44,
                 number: 2,
                 start: 11,
-                end: 27
+                end: 17
             )
         )
                 
         NSPasteboard.general.clearContents()
         NSPasteboard.general.setString("404 character not found", forType: .string)
         
-        let returnedElement = applyMove(to: "z", on: element)
+        let returnedElement = applyMoveBeingTested(to: "z", on: element)
         
         XCTAssertEqual(NSPasteboard.general.string(forType: .string), "404 character not found")
         XCTAssertEqual(returnedElement?.selectedLength, 1)
@@ -82,64 +117,30 @@ extension ASUT_NM_yt_Tests {
     
     func test_that_it_can_find_the_character_on_a_line_for_a_multiline() {
         let text = """
-yt on a multiline
+yt on a 🍌️ multiline
 should work 
 on a line
 """
         let element = AccessibilityTextElement(
             role: .textArea,
             value: text,
-            length: 40,
+            length: 44,
             caretLocation: 4,
             selectedLength: 1,
             selectedText: "n",
             currentLine: AccessibilityTextElementLine(
                 fullTextValue: text,
-                fullTextLength: 40,
+                fullTextLength: 44,
                 number: 1,
                 start: 0,
-                end: 18
+                end: 12
             )
         )
         
-        let returnedElement = applyMove(to: "m", on: element)
+        let returnedElement = applyMoveBeingTested(to: "m", on: element)
         
-        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "n a ")
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "n a 🍌️ ")
         XCTAssertEqual(returnedElement?.selectedLength, 1)
-        XCTAssertNil(returnedElement?.selectedText)
-    }
-    
-}
-
-
-// emojis
-extension ASUT_NM_yt_Tests {
-    
-    func test_that_it_handles_emojis() {
-        let text = """
-need to deal with
-t🍆️ose💨️💨️💨️ faces 🥺️☹️😂️ h😀️ha
-"""
-        let element = AccessibilityTextElement(
-            role: .textArea,
-            value: text,
-            length: 56,
-            caretLocation: 19,
-            selectedLength: 3,
-            selectedText: "🍆️",
-            currentLine: AccessibilityTextElementLine(
-                fullTextValue: text,
-                fullTextLength: 56,
-                number: 2,
-                start: 18,
-                end: 56
-            )
-        )
-        
-        let returnedElement = applyMove(to: "h", on: element)
-        
-        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "🍆️ose💨️💨️💨️ faces 🥺️☹️😂️ ")
-        XCTAssertEqual(returnedElement?.selectedLength, 3)
         XCTAssertNil(returnedElement?.selectedText)
     }
     
