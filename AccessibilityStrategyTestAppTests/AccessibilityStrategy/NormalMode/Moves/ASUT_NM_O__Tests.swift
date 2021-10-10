ab502246509aca3178fe56e18ae58903cae67f23
Guill Lo <guill.bout@gmail.com>
2021-10-10 22:32:47 +0800

    update NM O UT and UI. now UI less needed. many tests moved to UT

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_O__Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_O__Tests.swift
@@ -2,12 +2,10 @@
 import XCTest
 
 
-// O is a special move as it places the caret before the new linefeed
-// it creates. i don't think it's possible with the AX API to not have to
-// reposition the caret after pushing the new selectedText. therefore, unfortunately
-// we have to call the ATEAdaptor from within the move. hence, tests have to be made
-// in the UI Tests, as it's gonna modify the buffer. so most tests for O are there.
-class ASNM_O__Tests: ASNM_BaseTests {
+// most of the tests are here. there's still two in UI though as
+// if we insert a new line before the first line, the caret needs to be
+// repositioned. hence using the ATEAdaptor. hence the UI Tests.
+class ASUT_NM_O__Tests: ASNM_BaseTests {
     
     private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
         return asNormalMode.O(on: element) 
@@ -17,7 +15,7 @@ class ASNM_O__Tests: ASNM_BaseTests {
 
 
 // line
-extension ASNM_O__Tests {
+extension ASUT_NM_O__Tests {
     
     func test_conspicuously_that_it_does_not_stop_at_screen_lines() {
         let text = """
@@ -42,16 +40,16 @@ them like nothing happened. that's how special it is.
         
         let returnedElement = applyMoveBeingTested(on: element)
 
-        XCTAssertEqual(returnedElement?.caretLocation, 62)
-        XCTAssertEqual(returnedElement?.selectedLength, 0)
-        XCTAssertEqual(returnedElement?.selectedText, "\n")
+        XCTAssertEqual(returnedElement?.caretLocation, 0)
+        XCTAssertEqual(returnedElement?.selectedLength, 61)
+        XCTAssertEqual(returnedElement?.selectedText, "this move does not stop at screen lines. it will just pass by\n")
     }
      
 }
 
 
 // TextFields
-extension ASNM_O__Tests {
+extension ASUT_NM_O__Tests {
     
     func test_that_for_a_TextField_it_does_nothing() {
         let text = "O shouldn't do anything in a TextField!"
@@ -74,6 +72,159 @@ extension ASNM_O__Tests {
 		let returnedElement = applyMoveBeingTested(on: element)
         
         XCTAssertNil(returnedElement?.selectedText)
+    }
+    
+}
+
+
+// TextViews
+extension ASUT_NM_O__Tests {
+    
+    func test_that_in_normal_setting_it_creates_a_new_line_above_the_current_one() {
+        let text = """
+tha😄️t's a mu😄️ltiline
+an😄️😄️d O will
+create a n😄️ew line
+abo😄️ve!
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 72,
+            caretLocation: 33,
+            selectedLength: 1,
+            selectedText: "d",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 72,
+                number: 3,
+                start: 25,
+                end: 37
+            )
+        )
+                
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 0)
+        XCTAssertEqual(returnedElement?.selectedLength, 24)
+        XCTAssertEqual(returnedElement?.selectedText, "tha😄️t's a mu😄️ltiline\n")
+    }
+    
+    func test_that_if_on_an_empty_line_it_will_still_create_a_line_above() {
+        let text = """
+there is now
+
+an empty line
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 27,
+            caretLocation: 13,
+            selectedLength: 1,
+            selectedText: "\n",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 27,
+                number: 2,
+                start: 13,
+                end: 14
+            )
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 0)
+        XCTAssertEqual(returnedElement?.selectedLength, 12)
+        XCTAssertEqual(returnedElement?.selectedText, "there is now\n")
+    }
+
+    func test_that_if_on_the_last_empty_line_it_creates_a_line_below_and_the_caret_stays_on_the_current_line() {
+        let text = """
+now the caret
+will be on
+the last empty line
+
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 45,
+            caretLocation: 45,
+            selectedLength: 0,
+            selectedText: "",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 45,
+                number: 6,
+                start: 45,
+                end: 45
+            )
+        )
+
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 25)
+        XCTAssertEqual(returnedElement?.selectedLength, 19)
+        XCTAssertEqual(returnedElement?.selectedText, "the last empty line\n")
+    }
+
+    func test_that_if_on_the_last_non_empty_line_it_creates_a_line_below_and_the_caret_stays_on_the_current_line() {
+        let text = """
+now the caret
+will be on
+the last empty line
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 44,
+            caretLocation: 40,
+            selectedLength: 1,
+            selectedText: "l",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 44,
+                number: 5,
+                start: 34,
+                end: 44
+            )
+        )
+
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 14)
+        XCTAssertEqual(returnedElement?.selectedLength, 10)
+        XCTAssertEqual(returnedElement?.selectedText, "will be on\n")
+    }
+
+    func test_that_it_creates_a_line_above_and_goes_to_the_same_indentation_as_the_current_line() {
+        let text = """
+now there's
+    some indent
+but it should work
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 46,
+            caretLocation: 18,
+            selectedLength: 1,
+            selectedText: "m",
+            currentLine: AccessibilityTextElementLine(
+                fullTextValue: text,
+                fullTextLength: 46,
+                number: 2,
+                start: 12,
+                end: 21
+            )
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 0)
+        XCTAssertEqual(returnedElement?.selectedLength, 11)
+        XCTAssertEqual(returnedElement?.selectedText, "now there's\n    ")
     }
     
 }
--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_O__Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_O__Tests.swift
@@ -2,9 +2,7 @@ import XCTest
 @testable import AccessibilityStrategy
 
 
-// test that it works on FileLine and not ScreenLine is done in UT.
-// here we have those tests coz we need the AX to reposition the block cursor
-// have the move is done.
+// see ASUT O for blah blah
 class ASUI_NM_O__Tests: ASUI_NM_BaseTests {
     
     private func applyMoveBeingTested() -> AccessibilityTextElement? {
@@ -17,34 +15,7 @@ class ASUI_NM_O__Tests: ASUI_NM_BaseTests {
 // TextViews
 extension ASUI_NM_O__Tests {
     
-    func test_that_in_normal_setting_it_creates_a_new_line_above_the_current_one() {
-        let textInAXFocusedElement = """
-tha😄️t's a mu😄️ltiline
-an😄️😄️d O will
-create a n😄️ew line
-abo😄️ve!
-"""
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-                
-        applyMove { asNormalMode.h(on: $0) }
-        applyMove { asNormalMode.k(on: $0) }
-        applyMove { asNormalMode.k(on: $0) }
-        let accessibilityElement = applyMoveBeingTested()
-        
-        XCTAssertEqual(accessibilityElement?.text.value, """
-tha😄️t's a mu😄️ltiline
-
-an😄️😄️d O will
-create a n😄️ew line
-abo😄️ve!
-"""
-        )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 25)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-    }
-    
-    func test_that_if_at_the_first_line_it_will_create_a_new_line_above() {
+    func test_that_if_at_the_first_line_it_creates_a_new_line_above_and_reposition_the_caret_on_that_new_line() {
         let textInAXFocusedElement = """
 caret on the first
 line and it should
@@ -68,101 +39,7 @@ still create a line above
         XCTAssertEqual(accessibilityElement?.caretLocation, 0)
         XCTAssertEqual(accessibilityElement?.selectedLength, 0)
     }
-
-    func test_that_if_on_an_empty_line_it_will_still_create_a_line_above() {
-        let textInAXFocusedElement = """
-there is now
-
-an empty line
-"""
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.h(on: $0) }
-        applyMove { asNormalMode.k(on: $0) }
-        let accessibilityElement = applyMoveBeingTested()
-
-        XCTAssertEqual(accessibilityElement?.text.value, """
-there is now
-
-
-an empty line
-"""
-        )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 13)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-    }
-
-    func test_that_if_on_the_last_empty_line_it_creates_a_line_below_and_the_caret_stays_on_the_current_line() {
-        let textInAXFocusedElement = """
-now the caret
-will be on
-the last empty line
-
-"""
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-
-        let accessibilityElement = applyMoveBeingTested()
-
-        XCTAssertEqual(accessibilityElement?.text.value, """
-now the caret
-will be on
-the last empty line
-
-
-"""
-        )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 45)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-    }
-
-    func test_that_if_on_the_last_non_empty_line_it_creates_a_line_below_and_the_caret_stays_on_the_current_line() {
-        let textInAXFocusedElement = """
-now the caret
-will be on
-the last empty line
-"""
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-
-        let accessibilityElement = applyMoveBeingTested()
-
-        XCTAssertEqual(accessibilityElement?.text.value, """
-now the caret
-will be on
-
-the last empty line
-"""
-        )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 25)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-    }
-
-    func test_that_it_creates_a_line_above_and_goes_to_the_same_indentation_as_the_current_line() {
-        let textInAXFocusedElement = """
-now there's
-    some indent
-but it should work
-"""
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.h(on: $0) }
-        applyMove { asNormalMode.k(on: $0) }
-        let accessibilityElement = applyMoveBeingTested()
-
-        XCTAssertEqual(accessibilityElement?.text.value, """
-now there's
     
-    some indent
-but it should work
-"""
-        )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 16)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-    }
-
     func test_that_if_keeps_the_indentation_even_if_it_is_on_the_first_line() {
         let textInAXFocusedElement = """
    now indent on the first line
@@ -178,37 +55,6 @@ but it should work
 """
         )
         XCTAssertEqual(accessibilityElement?.caretLocation, 3)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-    }
-    
-}
-
-
-// word wrap for now. not sure we gonna keep this way of doing.
-// like do we insert like Vim does, on top of the real line, or on top of the visible line?
-// currently in code for `o` and `O` it is according to visible line, but decision is not
-// definitive.
-extension ASUI_NM_O__Tests {
-    
-    func test_that_in_normal_setting_it_does_insert_two_linefeeds_above_and_reposition_the_caret_between_them() {
-        let textInAXFocusedElement = """
-that's 😀️ a multiline 😀️😀️ and a long 😀️😀️ one that will be wrapped somewhere but we 😀️ don't know where LOL and i have to 😀️ test that shit
-"""
-
-        
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.k(on: $0) }        
-        let accessibilityElement = applyMoveBeingTested()
-        
-        XCTAssertEqual(accessibilityElement?.text.value, """
-that's 😀️ a multiline 😀️😀️ and a long 😀️😀️ one that will 
-
-be wrapped somewhere but we 😀️ don't know where LOL and i have to 😀️ test that shit
-"""
-        )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 63)
         XCTAssertEqual(accessibilityElement?.selectedLength, 0)
     }
     
