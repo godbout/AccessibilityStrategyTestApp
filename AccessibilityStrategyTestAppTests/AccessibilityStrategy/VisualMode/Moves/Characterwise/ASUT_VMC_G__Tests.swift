990060be51d281e6dde990e6282c13ee44a599f5
Guill Lo <guill.bout@gmail.com>
2021-10-13 22:45:48 +0800

    move VMC and VML G from UI to UT

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC_G__Tests.swift
@@ -0,0 +1,83 @@
+import XCTest
+import AccessibilityStrategy
+
+
+class ASUT_VMC_G__Tests: ASVM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+        return asVisualMode.GForVisualStyleCharacterwise(on: element)
+    }
+
+}
+
+
+// Both
+extension ASUT_VMC_G__Tests {
+
+    // this only happens if the new head location and the anchor are on the same line!
+    func test_that_if_the_new_head_location_is_before_the_anchor_then_it_selects_from_the_new_head_location_until_the_anchor() {
+        let text = "        💩️o here we 💩️ gonna test vG"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 38,
+            caretLocation: 31,
+            selectedLength: 6,
+            selectedText: "test v",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 38,
+                number: 1,
+                start: 0,
+                end: 38
+            )!
+        )
+                
+        AccessibilityStrategyVisualMode.anchor = 36
+        AccessibilityStrategyVisualMode.head = 31
+
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 8)
+        XCTAssertEqual(returnedElement?.selectedLength, 29)
+    }
+
+}
+
+
+// TextViews
+extension ASUT_VMC_G__Tests {
+    
+    func test_that_if_the_new_head_location_is_after_the_anchor_then_it_selects_from_anchor_to_the_new_head_location() {
+        let text = """
+we gonna put the caret before
+the anchor and do
+the move and it should
+     all work fine
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 89,
+            caretLocation: 17,
+            selectedLength: 30,
+            selectedText: "caret before\nthe anchor and do",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 89,
+                number: 2,
+                start: 13,
+                end: 23
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 46
+        AccessibilityStrategyVisualMode.head = 17
+
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 46)
+        XCTAssertEqual(returnedElement?.selectedLength, 31)
+    }
+    
+}
---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Linewise/ASUT_VML_G__Tests.swift
@@ -0,0 +1,116 @@
+import XCTest
+import AccessibilityStrategy
+
+
+class ASUT_VML_G__Tests: ASVM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+        return asVisualMode.GForVisualStyleLinewise(on: element)
+    }
+
+}
+
+
+// Both
+extension ASUT_VML_G__Tests {
+    
+    func test_that_if_the_TextElement_is_just_a_single_line_then_it_keeps_the_whole_line_selected() {
+        let text = "        so here we gonna test VG"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 32,
+            caretLocation: 0,
+            selectedLength: 32,
+            selectedText: "        so here we gonna test VG",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 32,
+                number: 1,
+                start: 0,
+                end: 32
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 0
+        AccessibilityStrategyVisualMode.head = 31
+
+        let accessibilityElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(accessibilityElement?.caretLocation, 0)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 32)
+    }
+    
+}
+
+
+// TextViews
+extension ASUT_VML_G__Tests {
+    
+    func test_that_if_the_head_is_after_or_at_the_same_line_as_the_anchor_then_it_selects_from_the_anchor_to_the_end_of_the_text() {
+        let text = """
+so now this is gonna
+😂️ be a longer one
+and we're gonna
+select until
+the end
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 77,
+            caretLocation: 21,
+            selectedLength: 20,
+            selectedText: "😂️ be a longer one\n",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 77,
+                number: 3,
+                start: 21,
+                end: 30
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 21
+        AccessibilityStrategyVisualMode.head = 40
+
+        let accessibilityElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(accessibilityElement?.caretLocation, 21)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 56)
+    }
+    
+    func test_that_if_the_head_is_before_the_line_of_the_anchor_then_it_selects_from_the_anchor_to_the_end_of_the_text() {
+        let text = """
+so now this is gonna
+😂️ be a longer one
+and we're gonna
+select until
+the end
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 77,
+            caretLocation: 21,
+            selectedLength: 36,
+            selectedText: "😂️ be a longer one\nand we're gonna\n",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 77,
+                number: 3,
+                start: 21,
+                end: 30
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 56
+        AccessibilityStrategyVisualMode.head = 21
+
+        let accessibilityElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(accessibilityElement?.caretLocation, 41)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 36)
+    }
+    
+}
