a48c1c3e470bbda78dda8a737dceaeb4543b77fe
Guill Lo <guill.bout@gmail.com>
2021-10-13 22:09:56 +0800

    move VMC h tests from UI to UT

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC_h_Tests.swift
@@ -0,0 +1,172 @@
+import XCTest
+import AccessibilityStrategy
+
+
+class ASUT_VMC_h_Tests: ASVM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+        return asVisualMode.hForVisualStyleCharacterwise(on: element)
+    }
+
+}
+    
+
+// Both
+extension ASUT_VMC_h_Tests {
+    
+    func test_that_if_the_selection_spans_over_a_single_line_and_the_head_is_after_the_anchor_then_it_goes_towards_the_beginning_of_the_line_and_reduces_the_selection_by_one() {
+        let text = "hello wo🌍️rld"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 14,
+            caretLocation: 5,
+            selectedLength: 9,
+            selectedText: " wo🌍️rld",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 14,
+                number: 1,
+                start: 0,
+                end: 14
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 5
+        AccessibilityStrategyVisualMode.head = 13
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 5)
+        XCTAssertEqual(returnedElement?.selectedLength, 8)
+    }
+    
+    func test_that_if_the_selection_spans_over_a_single_line_and_the_head_is_before_the_anchor_then_it_goes_towards_the_beginning_of_the_line_and_extends_the_selection_by_one() {
+        let text = """
+here is some text for VM h
+with head before anchor
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 50,
+            caretLocation: 37,
+            selectedLength: 8,
+            selectedText: "before a",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 50,
+                number: 5,
+                start: 37,
+                end: 44
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 44
+        AccessibilityStrategyVisualMode.head = 37
+
+        let returnedElement = applyMoveBeingTested(on: element)
+                
+        XCTAssertEqual(returnedElement?.caretLocation, 36)
+        XCTAssertEqual(returnedElement?.selectedLength, 9)
+    }
+    
+}
+
+
+// TextViews
+extension ASUT_VMC_h_Tests {
+    
+    func test_that_if_the_selection_spans_over_multiple_lines_and_the_head_is_before_the_anchor_then_it_goes_towards_the_beginning_of_the_line_and_extends_the_selection() {
+        let text = """
+span over multiple lines
+with head before anchor
+for VM h
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 57,
+            caretLocation: 19,
+            selectedLength: 14,
+            selectedText: "lines\nwith hea",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 57,
+                number: 3,
+                start: 19,
+                end: 25
+            )!
+        )
+                
+        AccessibilityStrategyVisualMode.anchor = 32
+        AccessibilityStrategyVisualMode.head = 19
+      
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 18)
+        XCTAssertEqual(returnedElement?.selectedLength, 15)
+    }
+    
+    func test_that_if_the_selection_spans_over_multiple_lines_and_the_head_is_after_the_anchor_then_it_goes_towards_the_beginning_of_the_line_and_reduces_the_selection() {
+        let text = """
+span over multiple lines
+with head after anchor
+for VM h and that should
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 72,
+            caretLocation: 0,
+            selectedLength: 29,
+            selectedText: "span over multiple lines\nwith",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 72,
+                number: 1,
+                start: 0,
+                end: 10
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 0
+        AccessibilityStrategyVisualMode.head = 28
+
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 0)
+        XCTAssertEqual(returnedElement?.selectedLength, 28)
+    }
+    
+    func test_that_it_stops_at_the_beginning_of_lines_and_does_not_continue_moving_backward_on_the_previous_lines() {
+        let text = """
+span over multiple lines
+w askljaslkasdlfjak
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 44,
+            caretLocation: 0,
+            selectedLength: 26,
+            selectedText: "span over multiple lines\nw",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 44,
+                number: 1,
+                start: 0,
+                end: 10
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 0
+        AccessibilityStrategyVisualMode.head = 25
+
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 0)
+        XCTAssertEqual(returnedElement?.selectedLength, 26)
+    }
+    
+}
