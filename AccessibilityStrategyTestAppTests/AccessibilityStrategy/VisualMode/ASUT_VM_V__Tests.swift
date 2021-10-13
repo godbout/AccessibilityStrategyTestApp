91e9a5e63461c0eafba953ef747e448cd2991216
Guill Lo <guill.bout@gmail.com>
2021-10-13 23:25:50 +0800

    move VM v and V tests from UI to UT

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/ASUT_VM_V__Tests.swift
@@ -0,0 +1,80 @@
+import XCTest
+import AccessibilityStrategy
+
+
+// this is `V` when entering from NM
+class ASUT_VM_V__Tests: ASVM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+        return asVisualMode.VForEnteringFromNormalMode(on: element)
+    }
+    
+}
+
+
+// Both
+extension ASUT_VM_V__Tests {
+    
+    func test_that_it_selects_the_whole_line_even_if_it_does_not_end_with_a_linefeed() {
+        let text = "a sentence without a linefeed"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 29,
+            caretLocation: 28,
+            selectedLength: 1,
+            selectedText: "d",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 29,
+                number: 1,
+                start: 0,
+                end: 29
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 0)
+        XCTAssertEqual(returnedElement?.selectedLength, 29)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.anchor, 0)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.head, 28)
+    }
+    
+}
+
+
+// TextAreas
+extension ASUT_VM_V__Tests {
+       
+    func test_that_if_we_just_entered_VisualMode_with_V_from_NormalMode_it_sets_the_anchor_and_caret_to_start_of_the_line_and_head_and_selection_to_end_of_line() {
+        let text = """
+now that's one with
+a linefeed at the end
+!
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 43,
+            caretLocation: 20,
+            selectedLength: 1,
+            selectedText: "a",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 43,
+                number: 3,
+                start: 20,
+                end: 31
+            )!
+        )
+                
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 20)
+        XCTAssertEqual(returnedElement?.selectedLength, 22)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.anchor, 20)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.head, 41)
+    }
+   
+}
---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/ASUT_VM_v_Tests.swift
@@ -0,0 +1,110 @@
+import XCTest
+import AccessibilityStrategy
+
+
+// see `V` for blah blah
+class ASUT_VM_v_Tests: ASVM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+        return asVisualMode.vForEnteringFromNormalMode(on: element)
+    }
+    
+}
+
+
+extension ASUT_VM_v_Tests {
+    
+    func test_that_if_we_just_entered_VisualMode_with_v_from_NormalMode_it_sets_the_anchor_and_head_to_the_caret_location() {
+        let text = """
+entering with v from
+normal mode 😀️eans anchor
+and head are nil
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 64,
+            caretLocation: 33,
+            selectedLength: 3,
+            selectedText: "😀️",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 64,
+                number: 4,
+                start: 33,
+                end: 41
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 33)
+        XCTAssertEqual(returnedElement?.selectedLength, 3)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.anchor, 33)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.head, 33)
+    }
+    
+    func test_that_if_we_just_entered_VisualMode_with_v_from_NormalMode_and_the_caret_location_is_over_the_line_end_limit_then_it_sets_the_caret_anchor_and_head_to_the_line_end_limit() {
+        let text = """
+entering with v from
+normal mode but with
+caret out of boundaries
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 65,
+            caretLocation: 40,
+            selectedLength: 1,
+            selectedText: "h",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 65,
+                number: 4,
+                start: 33,
+                end: 42
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+       
+        XCTAssertEqual(returnedElement?.caretLocation, 40)
+        XCTAssertEqual(returnedElement?.selectedLength, 1)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.anchor, 40)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.head, 40)
+    }
+    
+    // only tested for vForEnteringFromNormalMode but done for all vs and Vs.
+    // not tested because UI Tests are expensive, and it's a small thing. but adding this comment
+    // because it's good to be reminded of it.
+    func test_that_entering_VisualMode_with_v_from_NormalMode_does_not_override_the_globalColumnNumber() {
+        let text = """
+if we $ before entering VM the GCN
+should still stay at nil
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 59,
+            caretLocation: 58,
+            selectedLength: 1,
+            selectedText: "l",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 59,
+                number: 6,
+                start: 48,
+                end: 59
+            )!
+        )
+        
+        // setting it manually here as this is not a UI test,
+        // so the selectedLength didSet is not called.
+        AccessibilityTextElement.globalColumnNumber = 23
+        
+        _ = applyMoveBeingTested(on: element)
+       
+        XCTAssertEqual(AccessibilityTextElement.globalColumnNumber, 23)
+    }
+
+}
