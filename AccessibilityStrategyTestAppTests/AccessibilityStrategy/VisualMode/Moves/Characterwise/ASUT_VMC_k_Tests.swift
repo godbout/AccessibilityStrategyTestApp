0c6f5c0b3aafae6a37d00c284b163ef27a782b1e
Guill Lo <guill.bout@gmail.com>
2021-10-16 12:48:45 +0800

    update VM k tests

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC_k_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC_k_Tests.swift
@@ -1,4 +1,3 @@
-// TODO: do
 import XCTest
 import AccessibilityStrategy
 
@@ -45,164 +44,219 @@ extension ASUT_VMC_k_Tests {
 }
 
 
-//// TextViews
-//extension ASUI_VMC_gk_Tests {
-//    
-//    func test_that_if_the_head_is_before_the_anchor_then_it_goes_to_the_line_above_the_head_on_the_same_column_number_and_selects_from_that_new_head_location_to_the_anchor() {
-//        let textInAXFocusedElement = """
-//wow that one is gonna rip my ass off lol
-//and it's getting even harder now that
-//the wrapped lines and shit is understood
-//"""
-//        app.textViews.firstMatch.tap()
-//        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-//        
-//        applyMove { asNormalMode.h(on: $0) }
-//        applyMove { asNormalMode.gk(on: $0) }
-//        applyMove { asVisualMode.vForEnteringFromNormalMode(on: $0) }
-//        let accessibilityElement = applyMoveBeingTested()
-//
-//        XCTAssertEqual(accessibilityElement?.caretLocation, 78)
-//        XCTAssertEqual(accessibilityElement?.selectedLength, 19)
-//    }
-//    
-//    func test_that_if_the_head_is_after_the_anchor_and_both_are_on_the_same_line_then_it_goes_to_the_line_above_the_head_on_the_same_column_number_and_selects_from_that_new_head_location_to_the_anchor() {
-//        let textInAXFocusedElement = """
-//wow that one is gonna rip my ass off lol
-//and it's getting even harder now that
-//the wrapped lines and shit is understood
-//"""
-//        app.textViews.firstMatch.tap()
-//        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-//       
-//        applyMove { asNormalMode.zero(on: $0) }
-//        applyMove { asVisualMode.vForEnteringFromNormalMode(on: $0) }
-//        applyMove { asVisualMode.eForVisualStyleCharacterwise(on: $0) }
-//        let accessibilityElement = applyMoveBeingTested()
-//
-//        XCTAssertEqual(accessibilityElement?.caretLocation, 65)
-//        XCTAssertEqual(accessibilityElement?.selectedLength, 15)
-//    }
-//    
-//    func test_that_if_the_head_is_after_the_anchor_and_both_are_not_on_the_same_line_and_the_new_head_location_is_after_the_anchor_then_it_goes_to_the_line_above_the_head_on_the_same_column_number_and_selects_from_the_anchor_to_that_new_head_location() {
-//        let textInAXFocusedElement = """
-//wow that one is gonna rip my ass off lol
-//and it's getting even harder now that
-//the wrapped lines and shit is understood
-//"""
-//        app.textViews.firstMatch.tap()
-//        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-//        
-//        applyMove { asNormalMode.zero(on: $0) }
-//        applyMove { asVisualMode.vForEnteringFromNormalMode(on: $0) }
-//        applyMove { asVisualMode.eForVisualStyleCharacterwise(on: $0) }
-//        applyMove { asVisualMode.gjForVisualStyleCharacterwise(on: $0) }
-//        applyMove { asVisualMode.eForVisualStyleCharacterwise(on: $0) }
-//        let accessibilityElement = applyMoveBeingTested()
-//       
-//        XCTAssertEqual(accessibilityElement?.caretLocation, 79)
-//        XCTAssertEqual(accessibilityElement?.selectedLength, 4)
-//    }
-//    
-//    func test_that_if_the_head_is_after_the_anchor_and_both_are_not_on_the_same_line_and_the_new_head_location_is_before_the_anchor_then_it_goes_to_the_line_above_the_head_on_the_same_column_number_and_selects_from_that_new_head_location_to_the_anchor() {
-//        let textInAXFocusedElement = """
-//wow that one is gonna rip my ass off lol
-//and it's getting even harder now that
-//the wrapped lines and shit is understood
-//"""
-//        app.textViews.firstMatch.tap()
-//        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-//        
-//        applyMove { asNormalMode.h(on: $0) }
-//        applyMove { asNormalMode.gk(on: $0) }
-//        applyMove { asVisualMode.vForEnteringFromNormalMode(on: $0) }
-//        applyMove { asVisualMode.gDollarSignForVisualStyleCharacterwise(on: $0) }
-//        applyMove { asVisualMode.eForVisualStyleCharacterwise(on: $0) }
-//        let accessibilityElement = applyMoveBeingTested()
-//
-//        XCTAssertEqual(accessibilityElement?.caretLocation, 82)
-//        XCTAssertEqual(accessibilityElement?.selectedLength, 15)
-//    }
-//    
-//    // see j for blah blah
-//    func test_that_it_keeps_track_of_the_column_number() {
-//        let textInAXFocusedElement = """
-//extra long one here
-//ass off lol
-//gonna rip my
-//wow that one is
-//"""
-//        app.textViews.firstMatch.tap()
-//        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-//              
-//        applyMove { asNormalMode.h(on: $0) }
-//        applyMove { asVisualMode.vForEnteringFromNormalMode(on: $0) }
-//        applyMove { asVisualMode.bForVisualStyleCharacterwise(on: $0) }
-//        applyMove { asVisualMode.gkForVisualStyleCharacterwise(on: $0) }
-//        applyMove { asVisualMode.gkForVisualStyleCharacterwise(on: $0) }
-//        let accessibilityElement = applyMoveBeingTested()
-//
-//        XCTAssertEqual(accessibilityElement?.caretLocation, 13)
-//        XCTAssertEqual(accessibilityElement?.selectedLength, 47)
-//    }
-//    
-//    func test_that_if_the_caret_is_at_the_last_character_of_the_TextElement_and_on_an_empty_line_it_works_and_selects_from_the_last_character_to_some_character_of_the_previous_line() {
-//        let textInAXFocusedElement = """
-//caret is on its
-//own empty
-//    line
-//
-//"""
-//        app.textViews.firstMatch.tap()
-//        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-//               
-//        applyMove { asNormalMode.h(on: $0) }
-//        applyMove { asVisualMode.vForEnteringFromNormalMode(on: $0) }
-//        applyMove { asNormalMode.l(on: $0) }
-//        applyMove { asVisualMode.gkForVisualStyleCharacterwise(on: $0) }
-//        let accessibilityElement = applyMoveBeingTested()
-//
-//        // same test as NM k:
-//        // real location will depend on screenLineColumnNumber, so we test that it moved up that's all
-//        // we don't care where exactly on the previous line
-//        XCTAssertNotEqual(accessibilityElement?.caretLocation, 31)
-//        XCTAssertNotEqual(accessibilityElement?.selectedLength, 0)
-//    }
-//    
-//    func test_that_if_the_ATE_screenLineColumnNumber_is_nil_k_goes_to_the_end_limit_of_the_previous_line() {
-//        let textInAXFocusedElement = """
-//and also to the end of the next next line!
-//coz used $ to go end of line📏️
-//globalColumnNumber is nil
-//"""
-//
-//        app.textViews.firstMatch.tap()
-//        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-//        
-//        applyMove { asNormalMode.b(on: $0) }
-//        applyMove { asNormalMode.b(on: $0) }
-//        applyMove { asVisualMode.vForEnteringFromNormalMode(on: $0) }
-//        applyMove { asVisualMode.gDollarSignForVisualStyleCharacterwise(on: $0) }
-//        let accessibilityElement = applyMoveBeingTested()
-//
-//        XCTAssertEqual(accessibilityElement?.caretLocation, 74)
-//        XCTAssertEqual(accessibilityElement?.selectedLength, 21)
-//        
-//        let secondPass = applyMoveBeingTested()
-//                
-//        XCTAssertEqual(secondPass?.caretLocation, 66)
-//        XCTAssertEqual(secondPass?.selectedLength, 29)
-//        
-//        // see VMC j Tests for blah blah
-//        let applyJ = applyMove { asVisualMode.gjForVisualStyleCharacterwise(on: $0) }
-//        
-//        XCTAssertEqual(applyJ?.caretLocation, 74)
-//        XCTAssertEqual(applyJ?.selectedLength, 21)
-//
-//        let applyKAgain = applyMoveBeingTested()
-//        
-//        XCTAssertEqual(applyKAgain?.caretLocation, 66)
-//        XCTAssertEqual(applyKAgain?.selectedLength, 29)
-//    }
-//    
-//}
+// TextViews
+extension ASUT_VMC_k_Tests {
+    
+    func test_that_if_the_head_is_before_the_anchor_then_it_goes_to_the_line_above_the_head_on_the_same_column_number_and_selects_from_that_new_head_location_to_the_anchor() {
+        let text = """
+wow that one is gonna rip my ass off lol
+and it's getting even harder now that
+the wrapped lines and shit is understood
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 119,
+            caretLocation: 74,
+            selectedLength: 4,
+            selectedText: "that",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 119,
+                number: 8,
+                start: 74,
+                end: 79
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 77
+        AccessibilityStrategyVisualMode.head = 74
+        
+        AccessibilityTextElement.fileLineColumnNumber = 34
+        AccessibilityTextElement.screenLineColumnNumber = 1
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 33)
+        XCTAssertEqual(returnedElement?.selectedLength, 45)
+    }
+    
+    func test_that_if_the_head_is_after_the_anchor_and_both_are_on_the_same_line_then_it_goes_to_the_line_above_the_head_on_the_same_column_number_and_selects_from_that_new_head_location_to_the_anchor() {
+        let text = """
+wow that one is gonna rip my ass off lol
+and it's getting even harder now that
+the wrapped lines and shit is understood
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 119,
+            caretLocation: 79,
+            selectedLength: 3,
+            selectedText: "the",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 119,
+                number: 9,
+                start: 79,
+                end: 91
+            )!
+        )
+               
+        AccessibilityStrategyVisualMode.anchor = 79
+        AccessibilityStrategyVisualMode.head = 81
+        
+        AccessibilityTextElement.fileLineColumnNumber = 3
+        AccessibilityTextElement.screenLineColumnNumber = 3
+
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 43)
+        XCTAssertEqual(returnedElement?.selectedLength, 37)
+    }
+    
+    func test_that_if_the_head_is_after_the_anchor_and_both_are_not_on_the_same_line_and_the_new_head_location_is_after_the_anchor_then_it_goes_to_the_line_above_the_head_on_the_same_column_number_and_selects_from_the_anchor_to_that_new_head_location() {
+        let text = """
+wow that one is gonna rip my ass off lol
+and it's getting even harder now that
+the wrapped lines and shit is understood
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 119,
+            caretLocation: 41,
+            selectedLength: 49,
+            selectedText: "and it's getting even harder now that\nthe wrapped",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 119,
+                number: 5,
+                start: 41,
+                end: 50
+            )!
+        )
+                
+        AccessibilityStrategyVisualMode.anchor = 41
+        AccessibilityStrategyVisualMode.head = 89
+        
+        AccessibilityTextElement.fileLineColumnNumber = 11
+        AccessibilityTextElement.screenLineColumnNumber = 11
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+       
+        XCTAssertEqual(returnedElement?.caretLocation, 41)
+        XCTAssertEqual(returnedElement?.selectedLength, 11)
+    }
+    
+    func test_that_if_the_head_is_after_the_anchor_and_both_are_not_on_the_same_line_and_the_new_head_location_is_before_the_anchor_then_it_goes_to_the_line_above_the_head_on_the_same_column_number_and_selects_from_that_new_head_location_to_the_anchor() {
+        let text = """
+wow that one is gonna rip my ass off lol
+and it's getting even harder now that
+the wrapped lines and shit is understood
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 119,
+            caretLocation: 77,
+            selectedLength: 5,
+            selectedText: "t\nthe",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 119,
+                number: 8,
+                start: 74,
+                end: 79
+            )!
+        )
+                
+        AccessibilityStrategyVisualMode.anchor = 77
+        AccessibilityStrategyVisualMode.head = 81
+        
+        AccessibilityTextElement.fileLineColumnNumber = 3
+        AccessibilityTextElement.screenLineColumnNumber = 3
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 43)
+        XCTAssertEqual(returnedElement?.selectedLength, 35)
+    }
+    
+    // see j for blah blah
+    func test_that_it_keeps_track_of_the_column_number() {
+        let text = """
+extra long one here
+ass off lol
+gonna rip my
+wow that one is
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 60,
+            caretLocation: 31,
+            selectedLength: 29,
+            selectedText: """
+gonna rip my
+wow that one is
+""",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 60,
+                number: 3,
+                start: 20,
+                end: 32
+            )!
+        )
+                
+        AccessibilityStrategyVisualMode.anchor = 59
+        AccessibilityStrategyVisualMode.head = 31
+        
+        AccessibilityTextElement.fileLineColumnNumber = 12
+        AccessibilityTextElement.screenLineColumnNumber = 12
+
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement?.caretLocation, 11)
+        XCTAssertEqual(returnedElement?.selectedLength, 49)
+    }
+    
+    func test_that_if_the_caret_is_at_the_last_character_of_the_TextElement_and_on_an_empty_line_it_works_and_selects_from_the_last_character_to_some_character_of_the_previous_line() {
+        let text = """
+caret is on its
+own empty
+    line
+
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 35,
+            caretLocation: 35,
+            selectedLength: 0,
+            selectedText: "",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 35,
+                number: 5,
+                start: 35,
+                end: 35
+            )!
+        )
+             
+        AccessibilityStrategyVisualMode.anchor = 35
+        AccessibilityStrategyVisualMode.head = 35
+        
+        AccessibilityTextElement.fileLineColumnNumber = 1
+        AccessibilityTextElement.screenLineColumnNumber = 1
+               
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 26)
+        // element returns 10 but it will look like 9 as we can't physically
+        // select the last empty line character.
+        XCTAssertEqual(returnedElement?.selectedLength, 10)
+    }
+    
+}
