36683e6aafdc58d9457799108348b269ceb6b737
Guill Lo <guill.bout@gmail.com>
2021-10-16 12:48:45 +0800

    update VM k tests

---
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyVisualMode/Characterwise/ASUI_VMC_k_Tests.swift
@@ -0,0 +1,53 @@
+import XCTest
+import AccessibilityStrategy
+
+
+class ASUI_VMC_k_Tests: ASUI_VM_BaseTests {
+    
+    private func applyMoveBeingTested() -> AccessibilityTextElement? {
+        return applyMove { asVisualMode.kForVisualStyleCharacterwise(on: $0)}
+    }
+
+}
+
+
+// TextViews
+extension ASUI_VMC_k_Tests {
+       
+    func test_that_if_the_ATE_ColumnNumbers_are_nil_k_goes_to_the_end_limit_of_the_previous_line() {
+        let text = """
+and also to the end of the next next line!
+coz used $ to go end of line📏️
+globalColumnNumber is nil
+"""
+
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(text)
+        
+        applyMove { asNormalMode.b(on: $0) }
+        applyMove { asNormalMode.b(on: $0) }
+        applyMove { asVisualMode.vForEnteringFromNormalMode(on: $0) }
+        applyMove { asVisualMode.dollarSignForVisualStyleCharacterwise(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+
+        XCTAssertEqual(accessibilityElement?.caretLocation, 74)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 21)
+        
+        let secondPass = applyMoveBeingTested()
+                
+        XCTAssertEqual(secondPass?.caretLocation, 42)
+        XCTAssertEqual(secondPass?.selectedLength, 53)
+        
+        // see VMC j Tests for blah blah
+        let applyJ = applyMove { asVisualMode.jForVisualStyleCharacterwise(on: $0) }
+        
+        XCTAssertEqual(applyJ?.caretLocation, 74)
+        XCTAssertEqual(applyJ?.selectedLength, 21)
+
+        let applyKAgain = applyMoveBeingTested()
+        
+        XCTAssertEqual(applyKAgain?.caretLocation, 42)
+        XCTAssertEqual(applyKAgain?.selectedLength, 53)
+    }
+       
+}
