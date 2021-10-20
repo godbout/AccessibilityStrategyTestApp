8695ae2348add2fe892ae69516f2678bdc27fd6e
Guill Lo <guill.bout@gmail.com>
2021-10-20 13:47:09 +0800

    add VMC _ tests and VMC g^ UI tests

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC___Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC___Tests.swift
@@ -1,1 +1,7 @@
-// see 0 for blah blah
+import XCTest
+import AccessibilityStrategy
+
+
+// currently VMC _ calls VMC ^ as we don't handle counts.
+// so tests are in VMC ^.
+class ASUT_VMC_underscore_Tests: ASVM_BaseTests {}
