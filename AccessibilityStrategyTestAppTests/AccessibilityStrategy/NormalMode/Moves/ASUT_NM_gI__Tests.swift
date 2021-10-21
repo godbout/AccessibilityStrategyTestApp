25c47f20510459127ab6cff8a006fbeb751c9145
Guill Lo <guill.bout@gmail.com>
2021-10-21 22:09:11 +0800

    add NM gI test

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_gI__Tests.swift
@@ -0,0 +1,7 @@
+import XCTest
+@testable import AccessibilityStrategy
+
+
+// gI calls 0, so all tests are there.
+// the switch to IM is tested in KVE of course.
+class ASUT_NM_gI_Tests: ASNM_BaseTests {}
