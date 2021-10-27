ce8cfd540789d343cba6c975c88a13e0f15baf50
Guill Lo <guill.bout@gmail.com>
2021-10-27 17:54:28 +0800

    update NonTextElements UI Tests as Monterey and SwiftUI focus on TextField by default

--- AccessibilityStrategyTestAppUITests/AccessibilityTextElementAdaptorUITests/Others/NonTextElementsTests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityTextElementAdaptorUITests/Others/NonTextElementsTests.swift
@@ -2,25 +2,21 @@ import XCTest
 import AccessibilityStrategy
 
 
+// using Monterey and SwiftUI, by default the TextField will always
+// have the caret. so even if we tap on a button or the window, the focused AXUIElement
+// will still be the TextField. therefore in order to get a AXUIElement that is not
+// a TextField or TextView, we just mock pressing tab.
 class NonTextElementsTests: ATEA_BaseTests {}
 
 
 // from AXUIElement
 extension NonTextElementsTests {
     
-    func test_that_trying_to_convert_anAXUIElement_window_to_an_AccessibilityTextElement_returns_nil() {
-        app.windows.firstMatch.tap()
-       
+    func test_that_trying_to_convert_something_else_than_the_TextField_or_TextView_returns_nil() {
+        app.typeKey(.tab, modifierFlags: [])
+     
         let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
         
-        XCTAssertNil(accessibilityElement)
-    }
-
-    func test_that_trying_to_convert_an_AXUIElement_button_to_an_AccessibilityTextElement_returns_nil() {
-        app.buttons.firstMatch.tap()
-
-        let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
-
         XCTAssertNil(accessibilityElement)
     }
 
