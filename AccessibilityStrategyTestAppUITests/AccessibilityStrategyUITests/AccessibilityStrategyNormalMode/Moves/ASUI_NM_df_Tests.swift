2a0aad3f6911b8dcebca316e856c79a97fbe6031
Guill Lo <guill.bout@gmail.com>
2021-12-02 03:18:48 +0800

    add PGR test for NM df

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_df_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_df_Tests.swift
@@ -5,9 +5,9 @@ import XCTest
 // see dF for blah blah
 class ASUI_NM_df_Tests: ASUI_NM_BaseTests {
     
-    private func applyMoveBeingTested(with character: Character) -> AccessibilityTextElement? {
+    private func applyMoveBeingTested(with character: Character, pgR: Bool = false) -> AccessibilityTextElement? {
         return applyMove(with: character) { character, focusedElement in
-            asNormalMode.df(to: character, on: focusedElement)
+            asNormalMode.df(to: character, on: focusedElement, pgR: pgR)
         }
     }
     
@@ -31,4 +31,24 @@ extension ASUI_NM_df_Tests {
         XCTAssertEqual(accessibilityElement?.selectedLength, 2)
     }
     
+}
+
+
+// PGR
+extension ASUI_NM_df_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = "gonna us⛱️ df on this sentence"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.gZero(on: $0) }
+        applyMove { asNormalMode.l(on: $0) }
+       
+        let accessibilityElement = applyMoveBeingTested(with: "s", pgR: true)
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, "⛱️ df on this sentence")
+        XCTAssertEqual(accessibilityElement?.caretLocation, 0)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 2)
+    }
 }
