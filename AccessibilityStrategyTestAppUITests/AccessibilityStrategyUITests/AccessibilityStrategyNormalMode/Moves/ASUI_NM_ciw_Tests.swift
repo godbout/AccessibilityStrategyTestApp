b06fa779248b67e48d791664a471e0cb075f3add
Guill Lo <guill.bout@gmail.com>
2021-12-03 00:09:14 +0800

    move ciw UI to UI Tests, adapt for PGR

---
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ciw_Tests.swift
@@ -0,0 +1,57 @@
+@testable import AccessibilityStrategy
+import XCTest
+
+
+class ASUI_NM_ciw_Tests: ASUI_NM_BaseTests {
+    
+    private func applyMoveBeingTested(pgR: Bool = false) -> AccessibilityTextElement? {
+        return applyMove { asNormalMode.ciw(on: $0, pgR: pgR) }
+    }
+    
+}
+
+
+// both
+extension ASUI_NM_ciw_Tests {
+    
+    func test_that_when_it_finds_an_inner_word_it_selects_the_range_and_will_delete_the_selection() {
+        let textInAXFocusedElement = "that's some cute      text in here don't you think?"
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.l(on: $0) }
+        applyMove { asNormalMode.F(to: "c", on: $0) }
+        applyMove { asNormalMode.l(on: $0) }
+        applyMove { asNormalMode.l(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, "that's some       text in here don't you think?")
+        XCTAssertEqual(accessibilityElement?.caretLocation, 12)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
+    }
+    
+}
+
+
+// PGR
+extension ASUI_NM_ciw_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = "that's some cute      text in here don't you think?"
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.l(on: $0) }
+        applyMove { asNormalMode.F(to: "c", on: $0) }
+        applyMove { asNormalMode.l(on: $0) }
+        applyMove { asNormalMode.l(on: $0) }
+        let accessibilityElement = applyMoveBeingTested(pgR: true)
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, "that's some      text in here don't you think?")
+        XCTAssertEqual(accessibilityElement?.caretLocation, 11)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
+    }
+    
+}
