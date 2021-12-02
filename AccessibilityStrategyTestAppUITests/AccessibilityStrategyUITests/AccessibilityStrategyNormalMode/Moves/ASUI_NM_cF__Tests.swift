e2af5af14954a352c45866e34db003159488a6f6
Guill Lo <guill.bout@gmail.com>
2021-12-02 22:10:08 +0800

    add PRG test for NM cF

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cF__Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cF__Tests.swift
@@ -59,3 +59,31 @@ e
     }
     
 }
+
+
+// PGR
+extension ASUI_NM_cF__Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = """
+cF on a multiline
+should work
+on a 📏️📏️ line
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.l(on: $0) }
+        let accessibilityElement = applyMoveBeingTested(to: "o", pgR: true)
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+cF on a multiline
+should worke
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 29)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
+    }
+    
+}
