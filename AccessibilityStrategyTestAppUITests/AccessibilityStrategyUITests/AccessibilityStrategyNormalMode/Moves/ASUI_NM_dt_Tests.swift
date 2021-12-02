2d124ca2e5cc8ec83e1b6359024ce4ad8f365366
Guill Lo <guill.bout@gmail.com>
2021-12-02 19:12:21 +0800

    add PGR test for NM dt

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_dt_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_dt_Tests.swift
@@ -5,9 +5,9 @@ import XCTest
 // see dF for blah blah
 class ASUI_NM_dt_Tests: ASUI_NM_BaseTests {
     
-    private func applyMoveBeingTested(with character: Character) -> AccessibilityTextElement? {
+    private func applyMoveBeingTested(with character: Character, pgR: Bool = false) -> AccessibilityTextElement? {
         return applyMove(with: character) { character, focusedElement in
-            asNormalMode.dt(to: character, on: focusedElement)
+            asNormalMode.dt(to: character, on: focusedElement, pgR: pgR)
         }
     }
     
@@ -28,6 +28,27 @@ extension ASUI_NM_dt_Tests {
         
         XCTAssertEqual(accessibilityElement?.fileText.value, "gse dt on this sentence")
         XCTAssertEqual(accessibilityElement?.caretLocation, 1)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 1)
+    }
+    
+}
+
+
+// PGR
+extension ASUI_NM_dt_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = "gonna use dt on this sentence"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.gZero(on: $0) }
+        applyMove { asNormalMode.l(on: $0) }
+       
+        let accessibilityElement = applyMoveBeingTested(with: "s", pgR: true)
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, "se dt on this sentence")
+        XCTAssertEqual(accessibilityElement?.caretLocation, 0)
         XCTAssertEqual(accessibilityElement?.selectedLength, 1)
     }
     
