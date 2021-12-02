d8c403cd11669a2622ed417e932ff434d3184db2
Guill Lo <guill.bout@gmail.com>
2021-12-02 23:38:31 +0800

    move some ct UT to UI Tests, and adapt coz PGR

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ct_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ct_Tests.swift
@@ -12,98 +12,8 @@ class ASUT_NM_ct_Tests: ASNM_BaseTests {
 }
 
 
-// count
-extension ASUT_NM_ct_Tests {
-    
-    func test_that_it_implements_the_count_system() {
-        let text = "here we gonna delete up to 🕑️ characters rather than 🦴️!"
-        let element = AccessibilityTextElement(
-            role: .textField,
-            value: text,
-            length: 58,
-            caretLocation: 19,
-            selectedLength: 1,
-            selectedText: "e",
-            currentScreenLine: ScreenLine(
-                fullTextValue: text,
-                fullTextLength: 58,
-                number: 1,
-                start: 0,
-                end: 58
-            )!
-        )
-        
-        let returnedElement = applyMoveBeingTested(times: 2, to: "e", on: element)
-
-        XCTAssertEqual(returnedElement?.caretLocation, 19)
-        XCTAssertEqual(returnedElement?.selectedLength, 27)
-        XCTAssertEqual(returnedElement?.selectedText, "")
-    }
-    
-}
-
-
-// line
-extension ASUT_NM_ct_Tests {
-    
-    func test_conspicuously_that_it_does_not_stop_at_screen_lines() {
-        let text = """
-this move does not stop at screen lines. it will just pass by
-them like nothing happened. that's how special it is.
-"""
-        let element = AccessibilityTextElement(
-            role: .textArea,
-            value: text,
-            length: 116,
-            caretLocation: 73,
-            selectedLength: 1,
-            selectedText: "o",
-            currentScreenLine: ScreenLine(
-                fullTextValue: text,
-                fullTextLength: 116,
-                number: 9,
-                start: 72,
-                end: 80
-            )!
-        )
-        
-        let returnedElement = applyMoveBeingTested(to: "w", on: element)
-
-        XCTAssertEqual(returnedElement?.caretLocation, 73)
-        XCTAssertEqual(returnedElement?.selectedLength, 26)
-        XCTAssertEqual(returnedElement?.selectedText, "")
-    }
-     
-}
-
-
 // Both
 extension ASUT_NM_ct_Tests {
-    
-    func test_that_in_normal_setting_it_selects_the_text_from_the_caret_to_before_the_character_found() {
-        let text = "gonna use ct on 🛳️🛳️🛳️🛳️🛳️🛳️ this sentence"
-        let element = AccessibilityTextElement(
-            role: .textField,
-            value: text,
-            length: 48,
-            caretLocation: 8,
-            selectedLength: 1,
-            selectedText: "e",
-            currentScreenLine: ScreenLine(
-                fullTextValue: text,
-                fullTextLength: 48,
-                number: 1,
-                start: 0,
-                end: 48
-            )!
-        )
-        
-        let returnedElement = applyMoveBeingTested(to: "s", on: element)
-        
-        XCTAssertEqual(returnedElement?.caretLocation, 8)
-        XCTAssertEqual(returnedElement?.selectedLength, 30)
-        XCTAssertEqual(returnedElement?.selectedText, "")
-    }
     
     func test_that_if_the_character_is_not_found_then_it_does_nothing() {
         let text = """
@@ -132,41 +42,6 @@ that is not there
         XCTAssertEqual(returnedElement?.caretLocation, 14)
         XCTAssertEqual(returnedElement?.selectedLength, 1)
         XCTAssertNil(returnedElement?.selectedText)
-    }
-    
-}
-
-
-// TextViews
-extension ASUT_NM_ct_Tests {
-    
-    func test_that_it_can_find_the_character_on_a_line_for_a_multiline() {
-        let text = """
-ct on a multiline
-should work
-on a line
-"""
-        let element = AccessibilityTextElement(
-            role: .textArea,
-            value: text,
-            length: 39,
-            caretLocation: 19,
-            selectedLength: 1,
-            selectedText: "h",
-            currentScreenLine: ScreenLine(
-                fullTextValue: text,
-                fullTextLength: 39,
-                number: 3,
-                start: 18,
-                end: 30
-            )!
-        )
-        
-        let returnedElement = applyMoveBeingTested(to: "w", on: element)
-        
-        XCTAssertEqual(returnedElement?.caretLocation, 19)
-        XCTAssertEqual(returnedElement?.selectedLength, 6)
-        XCTAssertEqual(returnedElement?.selectedText, "")
     }
     
 }
