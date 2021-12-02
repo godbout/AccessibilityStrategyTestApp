7ef141e375e92161146adb07bbdb36d10bc8f1f0
Guill Lo <guill.bout@gmail.com>
2021-12-02 22:41:01 +0800

    move count from UT cf to UI cf

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cf_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cf_Tests.swift
@@ -4,8 +4,30 @@ import XCTest
 
 class ASUI_NM_cf_Tests: ASUI_NM_BaseTests {
     
-    private func applyMoveBeingTested(to character: Character, pgR: Bool = false) -> AccessibilityTextElement? {
-        return applyMove { asNormalMode.cf(times: 1, to: character, on: $0, pgR: pgR) }
+    private func applyMoveBeingTested(times count: Int = 1, to character: Character, pgR: Bool = false) -> AccessibilityTextElement? {
+        return applyMove { asNormalMode.cf(times: count, to: character, on: $0, pgR: pgR) }
+    }
+    
+}
+
+
+// count
+extension ASUI_NM_cf_Tests {
+    
+    func test_that_it_implements_the_count_system() {
+        let textInAXFocusedElement = "here we gonna delete up to 🕑️ characters rather than 🦴️!"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.l(on: $0) }
+        applyMove { asNormalMode.F(to: "u", on: $0) }
+        applyMove { asNormalMode.ge(on: $0) }
+        let accessibilityElement = applyMoveBeingTested(times: 2, to: "e")
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, "here we gonna deletr than 🦴️!")
+        XCTAssertEqual(accessibilityElement?.caretLocation, 19)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
     }
     
 }
@@ -55,6 +77,35 @@ on a line
 """
         )
         XCTAssertEqual(accessibilityElement?.caretLocation, 18)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
+    }
+    
+}
+
+
+// PGR
+extension ASUI_NM_cf_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = """
+cf on a multiline
+should work
+on a line
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.gk(on: $0) }
+        applyMove { asNormalMode.zero(on: $0) }
+        let accessibilityElement = applyMoveBeingTested(to: "w", pgR: true)
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+cf on a multilineork
+on a line
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 17)
         XCTAssertEqual(accessibilityElement?.selectedLength, 0)
         XCTAssertEqual(accessibilityElement?.selectedText, "")
     }
