152b2a31510f9ca492addff87d3181a71fe26dd9
Guill Lo <guill.bout@gmail.com>
2021-12-03 01:56:24 +0800

    move ciInnerBrackets and all related to UI Tests, adapt for PGR

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ci(_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ci(_Tests.swift
@@ -2,5 +2,6 @@
 import XCTest
 
 
-// calling ciInnerBrackets, all the Unit Tests are there.
+// calling ciInnerBrackets, all the tests are there.
+// there's one UI Tests for ci(, to check that we pass the pgR correctly to ciInnerBrackets.
 class ASUT_NM_ciLeftParenthesis_Tests: ASNM_BaseTests {}
--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ci[_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ci[_Tests.swift
@@ -2,25 +2,24 @@ import XCTest
 @testable import AccessibilityStrategy
 
 
-// this is calling ciInnerBrackets so all the tests are there.
-// here we just test that we pass the pgR parameter correctly to ciInnerBrackets.
-class ASUI_NM_ciLeftParenthesis_Tests: ASUI_NM_BaseTests {
+// see ci(
+class ASUI_NM_ciLeftBracket_Tests: ASUI_NM_BaseTests {
 
     private func applyMoveBeingTested(pgR: Bool) -> AccessibilityTextElement? {
-        return applyMove { asNormalMode.ciLeftParenthesis(on: $0, pgR: pgR) }
+        return applyMove { asNormalMode.ciLeftBracket(on: $0, pgR: pgR) }
     }
 
 }
 
 
 // PGR
-extension ASUI_NM_ciLeftParenthesis_Tests {
+extension ASUI_NM_ciLeftBracket_Tests {
     
     func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
         let textInAXFocusedElement = """
-this case is when ( is not followed
+this case is when [ is not followed
 by a linefeed and
-     ) is preceded by a linefeed
+     ] is preceded by a linefeed
 """
         app.textViews.firstMatch.tap()
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
@@ -31,7 +30,7 @@ by a linefeed and
         
         XCTAssertEqual(accessibilityElement?.fileText.value, """
 this case is when 
-     ) is preceded by a linefeed
+     ] is preceded by a linefeed
 """
         )
         XCTAssertEqual(accessibilityElement?.caretLocation, 18)
