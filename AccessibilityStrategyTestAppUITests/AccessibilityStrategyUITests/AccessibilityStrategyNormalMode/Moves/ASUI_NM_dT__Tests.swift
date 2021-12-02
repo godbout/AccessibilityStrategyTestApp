3e6edd98c79fa0abed38906d5d29fd70f36e95a3
Guill Lo <guill.bout@gmail.com>
2021-12-02 19:08:26 +0800

    add PGR test for NM dT

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_dT__Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_dT__Tests.swift
@@ -5,9 +5,9 @@ import XCTest
 // see dF for blah blah
 class ASUI_NM_dT__Tests: ASUI_NM_BaseTests {
     
-    private func applyMoveBeingTested(with character: Character) -> AccessibilityTextElement? {
+    private func applyMoveBeingTested(with character: Character, pgR: Bool = false) -> AccessibilityTextElement? {
         return applyMove(with: character) { character, focusedElement in
-            asNormalMode.dT(to: character, on: focusedElement)
+            asNormalMode.dT(to: character, on: focusedElement, pgR: pgR)
         }
     }
     
@@ -41,3 +41,34 @@ on a line
     }
     
 }
+
+
+// PGR
+extension ASUI_NM_dT__Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = """
+dT on a multiline
+should wor⛱️
+on a line
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.h(on: $0) }
+        applyMove { asNormalMode.gk(on: $0) }
+        applyMove { asNormalMode.dollarSign(on: $0) }
+        let accessibilityElement = applyMoveBeingTested(with: "w", pgR: true)
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+dT on a multiline
+should ⛱️
+on a line
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 25)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 2)
+    }
+    
+}
+
