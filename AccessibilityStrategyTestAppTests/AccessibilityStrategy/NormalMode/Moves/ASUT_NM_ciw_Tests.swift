3dc0c38819fda56894093ace2de1ce0f7ae8fb02
Guill Lo <guill.bout@gmail.com>
2021-12-03 00:09:14 +0800

    move ciw UI to UI Tests, adapt for PGR

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ciw_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ciw_Tests.swift
@@ -4,42 +4,5 @@ import XCTest
 
 // ciw uses TE.innerWord. this is already fully tested.
 // contrary to TE.aWord, innerWord never returns nil because Vim always finds an innerWord.
-// here we just test what is special to the ciw move itself (caretLocation, selectedLength correct, selectedText).
-class ASUT_NM_ciw_Tests: ASNM_BaseTests {
-    
-    private func applyMove(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
-        return asNormalMode.ciw(on: element) 
-    }
-    
-}
-
-
-// Both
-extension ASUT_NM_ciw_Tests {
-
-    func test_that_when_it_finds_an_inner_word_it_selects_the_range_and_will_delete_the_selection() {
-        let text = "that's some cute     text in here don't you think?"
-        let element = AccessibilityTextElement(
-            role: .textArea,
-            value: text,
-            length: 32,
-            caretLocation: 14,
-            selectedLength: 1,
-            selectedText: " ",
-            currentScreenLine: ScreenLine(
-                fullTextValue: text,
-                fullTextLength: 32,
-                number: 1,
-                start: 0,
-                end: 16
-            )!
-        )
-        
-        let returnedElement = applyMove(on: element)
-        
-        XCTAssertEqual(returnedElement?.caretLocation, 12)
-        XCTAssertEqual(returnedElement?.selectedLength, 4)
-        XCTAssertEqual(returnedElement?.selectedText, "")    
-    }
-    
-}
+// because of PGR the few tests to do here have been moved to UI Tests.
+class ASUT_NM_ciw_Tests: ASNM_BaseTests {}
