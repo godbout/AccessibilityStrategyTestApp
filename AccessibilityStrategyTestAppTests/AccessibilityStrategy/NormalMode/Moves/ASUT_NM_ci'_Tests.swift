f786a963b749bb7bea66bfe66c8ca6ae51ad53d6
Guill Lo <guill.bout@gmail.com>
2021-12-03 19:36:06 +0800

    move NM ciInnerQuotedString, ci', ci", ci` from UT to UI coz PGR

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ci'_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ci'_Tests.swift
@@ -2,62 +2,7 @@
 import XCTest
 
 
-class ASNM_ciSingleQuote_Tests: ASNM_BaseTests {
-    
-    private func applyMove(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
-        return asNormalMode.ciSingleQuote(on: element, pgR: false) 
-    }
-    
-}
-
-// Both
-extension ASNM_ciSingleQuote_Tests {
-    
-    func test_that_if_it_succeeds_it_drops_the_block_cursor() {
-        let text = "so if that one 'succeeds' it's gonna drop the bc in its own way"        
-        let element = AccessibilityTextElement(
-            role: .textField,
-            value: text,
-            length: 63,
-            caretLocation: 21,
-            selectedLength: 1,
-            selectedText: "e",
-            currentScreenLine: ScreenLine(
-                fullTextValue: text,
-                fullTextLength: 63,
-                number: 1,
-                start: 0,
-                end: 63
-            )!
-        )
-        
-        let returnedElement = applyMove(on: element)
-        
-        // the way to check for ciSingleQuote that it loses the block cursor is through the selectedText
-        XCTAssertEqual(returnedElement?.selectedText, "")
-    }
-    
-    func test_that_if_it_does_not_succeed_it_keeps_the_current_block_cursor_which_is_on() {
-        let text = "that one is gonna fail coz no quote"        
-        let element = AccessibilityTextElement(
-            role: .textField,
-            value: text,
-            length: 35,
-            caretLocation: 28,
-            selectedLength: 1,
-            selectedText: "o",
-            currentScreenLine: ScreenLine(
-                fullTextValue: text,
-                fullTextLength: 35,
-                number: 1,
-                start: 0,
-                end: 35
-            )!
-        )
-        
-        let returnedElement = applyMove(on: element)
-        
-        XCTAssertNil(returnedElement?.selectedText)
-    }
-    
-}
+// same mindset as with ci( and co.
+// calling ciInnerQuotedString, so all the tests are there.
+// some UI Tests for each ci' and co, to check that we pass the pgR param correctly to ciInnerQuotedString
+class ASNM_ciSingleQuote_Tests: ASNM_BaseTests {}
--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ci'_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ci'_Tests.swift
@@ -2,22 +2,24 @@ import XCTest
 @testable import AccessibilityStrategy
 
 
-// see ci'
-class ASUI_NM_ciDoubleQuote_Tests: ASUI_NM_BaseTests {
+// same mindset as ci( and co.
+// this is calling ciInnerQuoteString. all the tests are there.
+// here we just test that we pass the pgR parameter correctly to cInnerQuoteString.
+class ASUI_NM_ciSingleQuote_Tests: ASUI_NM_BaseTests {
 
     private func applyMoveBeingTested(pgR: Bool) -> AccessibilityTextElement? {
-        return applyMove { asNormalMode.ciInnerQuotedString(using: "\"", on: $0, pgR: pgR) }
+        return applyMove { asNormalMode.ciInnerQuotedString(using: "'", on: $0, pgR: pgR) }
     }
 
 }
 
 
 // PGR
-extension ASUI_NM_ciDoubleQuote_Tests {
+extension ASUI_NM_ciSingleQuote_Tests {
     
     func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
         let textInAXFocusedElement = """
-finally dealing with the "real stuff"!
+finally dealing with the 'real stuff'!
 """
         app.textFields.firstMatch.tap()
         app.textFields.firstMatch.typeText(textInAXFocusedElement)
@@ -26,7 +28,7 @@ finally dealing with the "real stuff"!
         
         
         XCTAssertEqual(accessibilityElement?.fileText.value, """
-finally dealing with the "!
+finally dealing with the '!
 """
         )
         XCTAssertEqual(accessibilityElement?.caretLocation, 25)
