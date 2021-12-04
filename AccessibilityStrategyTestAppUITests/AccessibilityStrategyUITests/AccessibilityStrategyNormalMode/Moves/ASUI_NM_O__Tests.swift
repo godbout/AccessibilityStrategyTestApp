9017bed2dc5d28953eb989e6d76613204d51f1da
Guill Lo <guill.bout@gmail.com>
2021-12-04 19:26:59 +0800

    move NM O UT to UIT

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_O__Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_O__Tests.swift
@@ -15,6 +15,134 @@ class ASUI_NM_O__Tests: ASUI_NM_BaseTests {
 // TextViews
 extension ASUI_NM_O__Tests {
     
+    func test_that_in_normal_setting_it_creates_a_new_line_above_the_current_one() {
+        let textInAXFocusedElement = """
+tha😄️t's a mu😄️ltiline
+an😄️😄️d O will
+create a n😄️ew line
+abo😄️ve!
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+               
+        applyMove { asNormalMode.gg(on: $0) }
+        applyMove { asNormalMode.j(on: $0) }
+        applyMove { asNormalMode.f(times: 1, to: "d", on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+tha😄️t's a mu😄️ltiline
+
+an😄️😄️d O will
+create a n😄️ew line
+abo😄️ve!
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 25)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
+    }
+    
+    func test_that_if_on_an_empty_line_it_will_still_create_a_line_above() {
+        let textInAXFocusedElement = """
+there is now
+
+an empty line
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+               
+        applyMove { asNormalMode.l(on: $0) }
+        applyMove { asNormalMode.k(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+there is now
+
+
+an empty line
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 13)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
+    }
+    
+    func test_that_if_on_the_last_empty_line_it_creates_a_line_above_and_the_caret_goes_on_that_line() {
+        let textInAXFocusedElement = """
+now the caret
+will be on
+the last empty line
+
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+               
+        applyMove { asNormalMode.l(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+now the caret
+will be on
+the last empty line
+
+
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 45)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
+    }
+    
+    func test_that_if_on_the_last_non_empty_line_it_creates_a_line_above_and_the_caret_goes_on_that_line() {
+        let textInAXFocusedElement = """
+now the caret
+will be on
+the last empty line
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+               
+        applyMove { asNormalMode.l(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+now the caret
+will be on
+
+the last empty line
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 25)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
+    }
+    
+    func test_that_it_creates_a_line_above_and_goes_to_the_same_indentation_as_the_current_line() {
+        let textInAXFocusedElement = """
+now there's
+    some indent
+but it should work
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+               
+        applyMove { asNormalMode.l(on: $0) }
+        applyMove { asNormalMode.k(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+now there's
+    
+    some indent
+but it should work
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 16)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
+    }
+    
     func test_that_if_at_the_first_line_it_creates_a_new_line_above_and_reposition_the_caret_on_that_new_line() {
         let textInAXFocusedElement = """
 caret on the first
