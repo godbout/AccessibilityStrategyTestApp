c2bb8b7625415ebaab0077d205341c1a279a12a2
Guill Lo <guill.bout@gmail.com>
2021-12-05 00:35:43 +0800

    add PGR UIT for VMC c (real this time. before it was move from UT to UIT)

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyVisualMode/Characterwise/ASUI_VMC_c_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyVisualMode/Characterwise/ASUI_VMC_c_Tests.swift
@@ -4,8 +4,8 @@ import XCTest
 
 class ASUI_VMC_c_Tests: ASUI_VM_BaseTests {
     
-    private func applyMoveBeingTested() -> AccessibilityTextElement? {
-        return applyMove { asVisualMode.cForVisualStyleCharacterwise(on: $0)}
+    private func applyMoveBeingTested(pgR: Bool = false) -> AccessibilityTextElement? {
+        return applyMove { asVisualMode.cForVisualStyleCharacterwise(on: $0, pgR: pgR)}
     }
 
 }
@@ -53,6 +53,29 @@ like samause
 """
         )
         XCTAssertEqual(accessibilityElement?.caretLocation, 8)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
+    }
+    
+}
+
+
+// PGR
+extension ASUI_VMC_c_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = "ok so VM c (hahaha) on a single line"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.f(times: 2, to: "h", on: $0) }
+        applyMove { asVisualMode.vForEnteringFromNormalMode(on: $0) }
+        applyMove { asVisualMode.fForVisualStyleCharacterwise(to: "i", on: $0) }
+        let accessibilityElement = applyMoveBeingTested(pgR: true)
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, "ok so VM c (hngle line")
+        XCTAssertEqual(accessibilityElement?.caretLocation, 13)
         XCTAssertEqual(accessibilityElement?.selectedLength, 0)
         XCTAssertEqual(accessibilityElement?.selectedText, "")
     }
