ae545ee41a0b97db0567cc0f69851417256f1eb2
Guill Lo <guill.bout@gmail.com>
2021-12-05 00:41:31 +0800

    add PGR UIT for VML c and also move old UT from UIT

---
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyVisualMode/Linewise/ASUI_VML_c_Tests.swift
@@ -0,0 +1,76 @@
+import XCTest
+@testable import AccessibilityStrategy
+
+
+class ASUI_VML_c_Tests: ASUI_VM_BaseTests {
+    
+    private func applyMoveBeingTested(pgR: Bool = false) -> AccessibilityTextElement? {
+        return applyMove { asVisualMode.cForVisualStyleLinewise(on: $0, pgR: pgR)}
+    }
+
+}
+
+
+// Both
+extension ASUI_VML_c_Tests {
+
+    func test_that_in_normal_setting_it_deletes_the_selected_lines_but_without_the_last_linefeed_of_the_selection_if_any() {
+        let textInAXFocusedElement = """
+VM c in Linewise
+will delete the selected lines
+but the below line will not go up
+at least if we're not at the end of the text
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.gg(on: $0) }
+        applyMove { asNormalMode.j(on: $0) }
+        applyMove { asVisualMode.VForEnteringFromNormalMode(on: $0) }
+        applyMove { asVisualMode.jForVisualStyleLinewise(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+VM c in Linewise
+
+at least if we're not at the end of the text
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 17)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
+    } 
+
+}
+
+
+// PGR
+extension ASUI_VML_c_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = """
+VM c in Linewise
+will delete the selected lines
+but the below line will not go up
+at least if we're not at the end of the text
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.gg(on: $0) }
+        applyMove { asNormalMode.j(on: $0) }
+        applyMove { asVisualMode.VForEnteringFromNormalMode(on: $0) }
+        applyMove { asVisualMode.jForVisualStyleLinewise(on: $0) }
+        let accessibilityElement = applyMoveBeingTested(pgR: true)
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+VM c in Linewise
+at least if we're not at the end of the text
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 16)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
+    }
+    
+}
