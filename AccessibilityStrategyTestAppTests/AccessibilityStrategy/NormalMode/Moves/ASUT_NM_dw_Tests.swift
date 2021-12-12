68149fd50cd3cc8c10bd2d6712f2e634f6e2d453
Guill Lo <guill.bout@gmail.com>
2021-12-12 22:57:21 +0800

    add AS NM dw UT and UIT

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_dw_Tests.swift
@@ -0,0 +1,6 @@
+@testable import AccessibilityStrategy
+import XCTest
+
+
+// UIT, PGR
+class ASUT_NM_dw_Tests: ASNM_BaseTests {}
--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_dw_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_dw_Tests.swift
@@ -2,19 +2,18 @@
 import XCTest
 
 
-// careful. `dW` is NOT `cW` + caret relocation.
-// `cW` may act like `cE` in some cases, but `dW` never acts like `dE`.
-class ASUI_NM_dW__Tests: ASUI_NM_BaseTests {
+// see `dW` for blah blah
+class ASUI_NM_dw_Tests: ASUI_NM_BaseTests {
     
     private func applyMoveBeingTested(pgR: Bool = false) -> AccessibilityTextElement? {
-        return applyMove { asNormalMode.dW(on: $0, pgR: pgR) }
+        return applyMove { asNormalMode.dw(on: $0, pgR: pgR) }
     }
     
 }
 
 
 // both
-extension ASUI_NM_dW__Tests {
+extension ASUI_NM_dw_Tests {
     
     func test_that_the_block_cursor_ends_up_at_the_right_place() {
         let textInAXFocusedElement = "😂️😂️😂️😂️hehehe gonna use ce on this sentence"
@@ -25,17 +24,17 @@ extension ASUI_NM_dW__Tests {
         applyMove { asNormalMode.l(on: $0) }
         let accessibilityElement = applyMoveBeingTested()
         
-        XCTAssertEqual(accessibilityElement?.fileText.value, "😂️gonna use ce on this sentence")
+        XCTAssertEqual(accessibilityElement?.fileText.value, "😂️hehehe gonna use ce on this sentence")
         XCTAssertEqual(accessibilityElement?.caretLocation, 3)
         XCTAssertEqual(accessibilityElement?.selectedLength, 1)
-        XCTAssertEqual(accessibilityElement?.selectedText, "g")
+        XCTAssertEqual(accessibilityElement?.selectedText, "h")
     }
-   
+    
 }
 
 
 // PGR
-extension ASUI_NM_dW__Tests {
+extension ASUI_NM_dw_Tests {
     
     func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
         let textInAXFocusedElement = "😂️😂️😂️😂️hehehe gonna use ce on this sentence"
@@ -46,10 +45,10 @@ extension ASUI_NM_dW__Tests {
         applyMove { asNormalMode.l(on: $0) }
         let accessibilityElement = applyMoveBeingTested(pgR: true)
         
-        XCTAssertEqual(accessibilityElement?.fileText.value, "gonna use ce on this sentence")
+        XCTAssertEqual(accessibilityElement?.fileText.value, "hehehe gonna use ce on this sentence")
         XCTAssertEqual(accessibilityElement?.caretLocation, 0)
         XCTAssertEqual(accessibilityElement?.selectedLength, 1)
-        XCTAssertEqual(accessibilityElement?.selectedText, "g")
+        XCTAssertEqual(accessibilityElement?.selectedText, "h")
     }
         
 }
