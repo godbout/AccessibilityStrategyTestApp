7b2f1bb583872664129094b2ad93d55206cd1af6
Guill Lo <guill.bout@gmail.com>
2021-12-14 23:17:03 +0800

    fix test that tries to convert a non text element after updating to Xcode 13.2

--- AccessibilityStrategyTestAppUITests/AccessibilityTextElementAdaptorUITests/Others/NonTextElementsTests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityTextElementAdaptorUITests/Others/NonTextElementsTests.swift
@@ -6,6 +6,8 @@ import AccessibilityStrategy
 // have the caret. so even if we tap on a button or the window, the focused AXUIElement
 // will still be the TextField. therefore in order to get a AXUIElement that is not
 // a TextField or TextView, we just mock pressing tab.
+// edit: seems there's a change with Xcode 13.2. now the tab sends to the TextEditor and the
+// test fail LMAO. so doing shift-tab now.
 class NonTextElementsTests: ATEA_BaseTests {}
 
 
@@ -13,7 +15,7 @@ class NonTextElementsTests: ATEA_BaseTests {}
 extension NonTextElementsTests {
     
     func test_that_trying_to_convert_something_else_than_the_TextField_or_TextView_returns_nil() {
-        app.typeKey(.tab, modifierFlags: [])
+        app.typeKey(.tab, modifierFlags: [.shift])
      
         let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
         
