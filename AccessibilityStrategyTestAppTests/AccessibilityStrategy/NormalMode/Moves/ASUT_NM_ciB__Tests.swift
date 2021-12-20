4d18f1d5032811b2fb1a0740e728bf307805ecbd
Guill Lo <guill.bout@gmail.com>
2021-12-20 19:53:55 +0800

    add UT and UIT for ciB

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ciB__Tests.swift
@@ -0,0 +1,6 @@
+@testable import AccessibilityStrategy
+import XCTest
+
+
+// this is literally calling `ci{`
+class ASUT_NM_ciB__Tests: ASNM_BaseTests {}
---
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ciB__Tests.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import AccessibilityStrategy
+
+
+// literally calling `ci{`, all all necessary tests are there
+class ASUI_NM_ciB__Tests: ASUI_NM_BaseTests {}
