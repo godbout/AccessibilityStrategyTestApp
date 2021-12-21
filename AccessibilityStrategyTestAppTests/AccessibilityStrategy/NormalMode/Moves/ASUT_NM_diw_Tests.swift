de01f518f7991d3b60f45c3ea0bcd1efa6b598f3
Guill Lo <guill.bout@gmail.com>
2021-12-21 15:54:10 +0800

    add UT and UIT for AS NM diw

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_diw_Tests.swift
@@ -0,0 +1,6 @@
+@testable import AccessibilityStrategy
+import XCTest
+
+
+// see D for blah blah
+class ASUT_NM_diw_Tests: ASNM_BaseTests {}
---
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_diw_Tests.swift
@@ -0,0 +1,88 @@
+import XCTest
+@testable import AccessibilityStrategy
+
+
+// see daw for blah blah
+class ASUI_NM_diw_Tests: ASUI_NM_BaseTests {
+    
+    private func applyMoveBeingTested(pgR: Bool = false) -> AccessibilityTextElement? {
+        return applyMove { asNormalMode.diw(on: $0, pgR: pgR) }
+    }
+    
+}
+
+
+extension ASUI_NM_diw_Tests {
+    
+    func test_that_the_block_cursor_ends_up_at_the_right_place() {
+        let textInAXFocusedElement = """
+like honestly that one should be
+   pretty😂️much     📏️traight forward if you ask me
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.h(on: $0) }
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.w(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+like honestly that one should be
+   😂️much     📏️traight forward if you ask me
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 36)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 3)
+    }
+    
+    func test_that_if_the_caret_ends_up_after_the_end_limit_then_it_is_moved_back_to_the_end_limit() {
+        let textInAXFocusedElement = """
+repositionin🇫🇷️ of-what
+the block cursor is important!
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.ge(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+repositionin🇫🇷️ of-
+the block cursor is important!
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 20)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 1)
+    }
+
+}
+
+
+// PGR
+extension ASUI_NM_diw_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = """
+like honestly that one should be
+   pretty-much     📏️traight forward if you ask me
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.h(on: $0) }
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.w(on: $0) }
+        let accessibilityElement = applyMoveBeingTested(pgR: true)
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+like honestly that one should be
+  -much     📏️traight forward if you ask me
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 35)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 1)
+    }
+    
+}
