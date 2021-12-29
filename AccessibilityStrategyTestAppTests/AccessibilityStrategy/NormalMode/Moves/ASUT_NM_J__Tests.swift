2361239325c6791831dd544c06a95f0664c2a201
Guill Lo <guill.bout@gmail.com>
2021-12-29 18:08:53 +0800

    add ASUT UIT for NM J

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_J__Tests.swift
@@ -0,0 +1,46 @@
+@testable import AccessibilityStrategy
+import XCTest
+
+
+// most tests in UIT coz delete/paste and PGR
+class ASUT_NM_J__Tests: ASNM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+        return asNormalMode.J(on: element, pgR: false)
+    }
+    
+}
+
+
+// Both
+extension ASUT_NM_J__Tests {
+    
+    func test_that_if_the_character_is_not_found_then_it_does_nothing() {
+        let text = """
+hehe no linefeed here mofo
+"""
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 26,
+            caretLocation: 15,
+            selectedLength: 1,
+            selectedText: "d",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 26,
+                number: 1,
+                start: 0,
+                end: 26
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 15)
+        XCTAssertEqual(returnedElement?.selectedLength, 1)
+        XCTAssertNil(returnedElement?.selectedText)
+    }
+    
+}
+
---
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_J__Tests.swift
@@ -0,0 +1,106 @@
+import XCTest
+@testable import AccessibilityStrategy
+
+
+class ASUI_NM_J__Tests: ASUI_NM_BaseTests {
+    
+    private func applyMoveBeingTested(pgR: Bool = false) -> AccessibilityTextElement? {
+        return applyMove { asNormalMode.J(on: $0, pgR: pgR) }
+    }
+    
+}
+
+
+
+extension ASUI_NM_J__Tests {
+    
+    func test_that_in_normal_setting_it_replaces_the_linefeed_at_the_end_of_the_current_line_by_a_space() {
+        let textInAXFocusedElement = """
+gonna try to fuse line 1
+with line 2
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+               
+        applyMove { asNormalMode.gg(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+gonna try to fuse line 1 with line 2
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 24)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 1)
+        XCTAssertEqual(accessibilityElement?.selectedText, " ")
+    }
+    
+    func test_that_it_deletes_the_indentation_of_the_next_line() {
+        let textInAXFocusedElement = """
+gonna try to fuse line 1
+    with line 2
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+               
+        applyMove { asNormalMode.gg(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+gonna try to fuse line 1 with line 2
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 24)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 1)
+        XCTAssertEqual(accessibilityElement?.selectedText, " ")
+    }
+    
+    func test_that_if_the_next_line_is_an_empty_line_it_does_not_replace_the_linefeed_by_space_but_instead_deletes_it() {
+        let textInAXFocusedElement = """
+next line is empty and it works differently
+
+LMAO
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+               
+        applyMove { asNormalMode.gg(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+next line is empty and it works differently
+LMAO
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 42)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 1)
+        XCTAssertEqual(accessibilityElement?.selectedText, "y")
+    }
+    
+}
+
+
+// PGR
+extension ASUI_NM_J__Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = """
+gonna try to fuse line 1
+with line 2
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+               
+        applyMove { asNormalMode.gg(on: $0) }
+        let accessibilityElement = applyMoveBeingTested(pgR: true)
+
+        XCTAssertEqual(accessibilityElement?.fileText.value, """
+gonna try to fuse line   with line 2
+"""
+        )
+        XCTAssertEqual(accessibilityElement?.caretLocation, 23)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 1)
+        XCTAssertEqual(accessibilityElement?.selectedText, " ")
+    }
+        
+}
+
