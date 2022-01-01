3fa16d4870e7ccbb6dba1c5799075cd043df9bd1
Guill Lo <guill.bout@gmail.com>
2022-01-02 03:08:11 +0800

    AS NM ciw UT back :D

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ciw_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ciw_Tests.swift
@@ -4,5 +4,71 @@ import XCTest
 
 // ciw uses TE.innerWord. this is already fully tested.
 // contrary to TE.aWord, innerWord never returns nil because Vim always finds an innerWord.
-// because of PGR the few tests to do here have been moved to UI Tests.
-class ASUT_NM_ciw_Tests: ASUT_NM_BaseTests {}
+// PGR in UIT.
+class ASUT_NM_ciw_Tests: ASUT_NM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+        return asNormalMode.ciw(on: element, pgR: false) 
+    } 
+}
+
+
+// copy deleted text
+extension ASUT_NM_ciw_Tests {
+    
+    func test_that_it_copies_the_deleted_text_in_the_pasteboard() {
+        let text = "that's some cute-boobies      text in here don't you think?"
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 59,
+            caretLocation: 14,
+            selectedLength: 1,
+            selectedText: "t",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 59,
+                number: 1,
+                start: 0,
+                end: 49
+            )!
+        )
+       
+        copyToClipboard(text: "some fake shit")
+        _ = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "cute")
+    }
+    
+}
+
+
+// both
+extension ASUT_NM_ciw_Tests {
+    
+    func test_that_when_it_finds_an_inner_word_it_selects_the_range_and_will_delete_the_selection() {
+        let text = "that's some cute-boobies      text in here don't you think?"
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 59,
+            caretLocation: 14,
+            selectedLength: 1,
+            selectedText: "t",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 59,
+                number: 1,
+                start: 0,
+                end: 49
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 12)
+        XCTAssertEqual(returnedElement?.selectedLength, 4)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+    
+}
--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ciw_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ciw_Tests.swift
@@ -2,61 +2,8 @@
 import XCTest
 
 
-class ASUI_NM_ciw_Tests: ASUI_NM_BaseTests {
-    
-    private func applyMoveBeingTested(pgR: Bool = false) -> AccessibilityTextElement? {
-        return applyMove { asNormalMode.ciw(on: $0, pgR: pgR) }
-    }
-    
-}
-
-
-// copy deleted text
-extension ASUI_NM_ciw_Tests {
-    
-    func test_that_it_copies_the_deleted_text_in_the_pasteboard() {
-        let textInAXFocusedElement = "that's some cute-boobies      text in here don't you think?"
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.l(on: $0) }
-        applyMove { asNormalMode.F(to: "c", on: $0) }
-        applyMove { asNormalMode.l(on: $0) }
-        applyMove { asNormalMode.l(on: $0) }
-        copyToClipboard(text: "some fake shit")
-        _ = applyMoveBeingTested()
-        
-        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "cute")
-    }
-    
-}
-
-
-// both
-extension ASUI_NM_ciw_Tests {
-    
-    func test_that_when_it_finds_an_inner_word_it_selects_the_range_and_will_delete_the_selection() {
-        let textInAXFocusedElement = "that's some cute-boobies      text in here don't you think?"
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.l(on: $0) }
-        applyMove { asNormalMode.F(to: "c", on: $0) }
-        applyMove { asNormalMode.l(on: $0) }
-        applyMove { asNormalMode.l(on: $0) }
-        let accessibilityElement = applyMoveBeingTested()
-        
-        XCTAssertEqual(accessibilityElement?.fileText.value, "that's some -boobies      text in here don't you think?")
-        XCTAssertEqual(accessibilityElement?.caretLocation, 12)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-        XCTAssertEqual(accessibilityElement?.selectedText, "")
-    }
-    
-}
-
-
 // PGR
-extension ASUI_NM_ciw_Tests {
+class ASUI_NM_ciw_Tests: ASUI_NM_BaseTests {
     
     func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
         let textInAXFocusedElement = "that's some cute-boobies      text in here don't you think?"
@@ -67,7 +14,7 @@ extension ASUI_NM_ciw_Tests {
         applyMove { asNormalMode.F(to: "c", on: $0) }
         applyMove { asNormalMode.l(on: $0) }
         applyMove { asNormalMode.l(on: $0) }
-        let accessibilityElement = applyMoveBeingTested(pgR: true)
+        let accessibilityElement = applyMove { asNormalMode.ciw(on: $0, pgR: true) }
         
         XCTAssertEqual(accessibilityElement?.fileText.value, "that's some-boobies      text in here don't you think?")
         XCTAssertEqual(accessibilityElement?.caretLocation, 11)
