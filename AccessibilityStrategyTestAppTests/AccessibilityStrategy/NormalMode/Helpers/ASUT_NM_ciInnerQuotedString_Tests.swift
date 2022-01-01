c5a15a6ef3230e5b424635cc310551ec3b887d55
Guill Lo <guill.bout@gmail.com>
2022-01-02 03:34:21 +0800

    ASUT ciInnerQuotedString back

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Helpers/ASUT_NM_ciInnerQuotedString_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Helpers/ASUT_NM_ciInnerQuotedString_Tests.swift
@@ -7,15 +7,218 @@ import XCTest
 // the rest is tested in UI, because PGR.
 class ASUT_NM_ciInnerQuotedString_Tests: ASUT_NM_BaseTests {
     
-    private func applyMove(using quote: Character, on element: AccessibilityTextElement?, _ bipped: inout Bool) -> AccessibilityTextElement? {
+    private func applyMoveBeingTested(using quote: Character, on element: AccessibilityTextElement?, _ bipped: inout Bool) -> AccessibilityTextElement? {
         return asNormalMode.ciInnerQuotedString(using: quote, on: element, pgR: false, &bipped)
     }
     
 }
 
 
+// copy deleted text
 extension ASUT_NM_ciInnerQuotedString_Tests {
+    
+    func test_that_it_copies_the_deleted_text_in_the_pasteboard() {
+        let text = """
+finally dealing with the "real stuff"!
+"""
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 38,
+            caretLocation: 29,
+            selectedLength: 1,
+            selectedText: "l",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 38,
+                number: 1,
+                start: 0,
+                end: 38
+            )!
+        )
         
+        copyToClipboard(text: "some fake shit")
+        var bipped = false
+        _ = applyMoveBeingTested(using: "\"", on: element, &bipped)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "real stuff")
+    }
+    
+}
+
+
+// bipped
+extension ASUT_NM_ciInnerQuotedString_Tests {
+    
+    func test_that_it_does_not_Bip_when_it_can_find() {
+        let text = """
+finally dealing with the "real stuff"!
+"""
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 38,
+            caretLocation: 16,
+            selectedLength: 1,
+            selectedText: "w",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 38,
+                number: 1,
+                start: 0,
+                end: 38
+            )!
+        )
+        
+        var bipped = false
+        let _ = applyMoveBeingTested(using: "\"", on: element, &bipped)
+        
+        XCTAssertFalse(bipped)
+    }
+        
+    func test_that_it_Bips_when_it_cannot_find() {
+        let text = """
+finally dealing with the "real stuff!
+"""
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 37,
+            caretLocation: 15,
+            selectedLength: 1,
+            selectedText: " ",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 37,
+                number: 1,
+                start: 0,
+                end: 37
+            )!
+        )
+        
+        var bipped = false
+        let _ = applyMoveBeingTested(using: "\"", on: element, &bipped)
+        
+        XCTAssertTrue(bipped)
+    }
+    
+}
+
+
+extension ASUT_NM_ciInnerQuotedString_Tests {
+    
+    func test_that_if_the_caret_is_between_quotes_the_content_within_the_quotes_is_deleted_and_the_caret_moves() {
+        let text = """
+finally dealing with the "real stuff"!
+"""
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 38,
+            caretLocation: 29,
+            selectedLength: 1,
+            selectedText: "l",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 38,
+                number: 1,
+                start: 0,
+                end: 38
+            )!
+        )
+        
+        var bipped = false
+        let returnedElement = applyMoveBeingTested(using: "\"", on: element, &bipped)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 26)
+        XCTAssertEqual(returnedElement?.selectedLength, 10)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+        
+    func test_that_if_the_caret_is_before_the_quotes_then_the_content_within_is_deleted_and_the_caret_moves() {
+        let text = """
+now the caret 💨️💨️💨️ is before the ` shit with 🥺️☹️😂️ h😀️ha👅️ ` backtick quotes hhohohoo🤣️
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 98,
+            caretLocation: 31,
+            selectedLength: 1,
+            selectedText: "r",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 98,
+                number: 1,
+                start: 0,
+                end: 60
+            )!
+        )
+        
+        var bipped = false
+        let returnedElement = applyMoveBeingTested(using: "`", on: element, &bipped)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 39)
+        XCTAssertEqual(returnedElement?.selectedLength, 30)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+
+    func test_that_if_there_are_three_quotes_then_the_correct_content_is_deleted_and_the_caret_moves() {
+        let text = """
+that's ' three quotes ' in there
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 32,
+            caretLocation: 17,
+            selectedLength: 1,
+            selectedText: "o",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 32,
+                number: 1,
+                start: 0,
+                end: 32
+            )!
+        )
+        
+        var bipped = false
+        let returnedElement = applyMoveBeingTested(using: "'", on: element, &bipped)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 8)
+        XCTAssertEqual(returnedElement?.selectedLength, 14)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+    
+    func test_that_current_when_the_caret_is_at_a_quote_it_deletes_the_correct_content() {
+        let text = """
+that's " four quotes " in " there "
+"""
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 35,
+            caretLocation: 26,
+            selectedLength: 1,
+            selectedText: "\"",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 35,
+                number: 1,
+                start: 0,
+                end: 35
+            )!
+        )
+        
+        var bipped = false
+        let returnedElement = applyMoveBeingTested(using: "\"", on: element, &bipped)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 27)
+        XCTAssertEqual(returnedElement?.selectedLength, 7)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+    
     func test_that_if_there_is_only_one_quote_no_content_is_deleted_and_the_caret_does_not_move() {
         let text = """
 a text with only one quote ' lol
@@ -37,7 +240,7 @@ a text with only one quote ' lol
         )
         
         var bipped = false
-        let returnedElement = applyMove(using: "'", on: element, &bipped)
+        let returnedElement = applyMoveBeingTested(using: "'", on: element, &bipped)
         
         XCTAssertNil(returnedElement?.selectedText)
     }
@@ -61,7 +264,7 @@ a text with only one quote ' lol
         )
         
         var bipped = false
-        let returnedElement = applyMove(using: "'", on: element, &bipped)
+        let returnedElement = applyMoveBeingTested(using: "'", on: element, &bipped)
         
         XCTAssertNil(returnedElement?.selectedText)
     }
@@ -89,61 +292,9 @@ now the "caret" is after the quotes
         )
         
         var bipped = false
-        let returnedElement = applyMove(using: "\"", on: element, &bipped)
+        let returnedElement = applyMoveBeingTested(using: "\"", on: element, &bipped)
         
         XCTAssertNil(returnedElement?.selectedText)
     }
     
-    func test_that_it_does_not_Bip_when_it_can_find() {
-        let text = """
-finally dealing with the "real stuff"!
-"""
-        let element = AccessibilityTextElement(
-            role: .textField,
-            value: text,
-            length: 38,
-            caretLocation: 16,
-            selectedLength: 1,
-            selectedText: "w",
-            currentScreenLine: ScreenLine(
-                fullTextValue: text,
-                fullTextLength: 38,
-                number: 1,
-                start: 0,
-                end: 38
-            )!
-        )
-        
-        var bipped = false
-        let _ = applyMove(using: "\"", on: element, &bipped)
-        
-        XCTAssertFalse(bipped)
-    }
-        
-    func test_that_it_Bips_when_it_cannot_find() {
-        let text = """
-finally dealing with the "real stuff!
-"""
-        let element = AccessibilityTextElement(
-            role: .textField,
-            value: text,
-            length: 37,
-            caretLocation: 15,
-            selectedLength: 1,
-            selectedText: " ",
-            currentScreenLine: ScreenLine(
-                fullTextValue: text,
-                fullTextLength: 37,
-                number: 1,
-                start: 0,
-                end: 37
-            )!
-        )
-        
-        var bipped = false
-        let _ = applyMove(using: "\"", on: element, &bipped)
-        
-        XCTAssertTrue(bipped)
-    }
-
 }
--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Helpers/ASUI_NM_ciInnerQuotedString_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Helpers/ASUI_NM_ciInnerQuotedString_Tests.swift
@@ -5,121 +5,6 @@ import XCTest
 // see ASUT ciInnerQuotedString for blah blah
 class ASUI_NM_ciInnerQuotedString_Tests: ASUI_NM_BaseTests {
     
-    private func applyMoveBeingTested(using quote: Character, pgR: Bool = false) -> AccessibilityTextElement? {
-        var bipped = false
-        
-        return applyMove { asNormalMode.ciInnerQuotedString(using: quote, on: $0, pgR: pgR, &bipped) }
-    }
-    
-}
-
-
-// copy deleted text
-extension ASUI_NM_ciInnerQuotedString_Tests {
-    
-    func test_that_it_copies_the_deleted_text_in_the_pasteboard() {
-        let textInAXFocusedElement = """
-finally dealing with the "real stuff"!
-"""
-        app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.F(to: "l", on: $0) }
-        copyToClipboard(text: "some fake shit")
-        _ = applyMoveBeingTested(using: "\"")
-        
-        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "real stuff")
-    }
-    
-}
-
-
-extension ASUI_NM_ciInnerQuotedString_Tests {
-    
-    func test_that_if_the_caret_is_between_quotes_the_content_within_the_quotes_is_deleted_and_the_caret_moves() {
-        let textInAXFocusedElement = """
-finally dealing with the "real stuff"!
-"""
-        app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.F(to: "l", on: $0) }
-        let accessibilityElement = applyMoveBeingTested(using: "\"")
-        
-        XCTAssertEqual(accessibilityElement?.fileText.value, """
-finally dealing with the ""!
-"""
-        )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 26)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-        XCTAssertEqual(accessibilityElement?.selectedText, "")
-    }
-        
-    func test_that_if_the_caret_is_before_the_quotes_then_the_content_within_is_deleted_and_the_caret_moves() {
-        let textInAXFocusedElement = """
-now the caret 💨️💨️💨️ is before the ` shit with 🥺️☹️😂️ h😀️ha👅️ ` backtick quotes hhohohoo🤣️
-"""
-        app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.F(to: "r", on: $0) }
-        let accessibilityElement = applyMoveBeingTested(using: "`")
-        
-        XCTAssertEqual(accessibilityElement?.fileText.value, """
-now the caret 💨️💨️💨️ is before the `` backtick quotes hhohohoo🤣️
-"""
-        )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 39)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-        XCTAssertEqual(accessibilityElement?.selectedText, "")
-    }
-
-    func test_that_if_there_are_three_quotes_then_the_correct_content_is_deleted_and_the_caret_moves() {
-        let textInAXFocusedElement = """
-that's ' three quotes ' in there
-"""
-        app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.F(to: "o", on: $0) }
-        let accessibilityElement = applyMoveBeingTested(using: "'")
-        
-        XCTAssertEqual(accessibilityElement?.fileText.value, """
-that's '' in there
-"""
-        )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 8)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-        XCTAssertEqual(accessibilityElement?.selectedText, "")
-    }
-    
-    
-    func test_that_current_when_the_caret_is_at_a_quote_it_deletes_the_correct_content() {
-        let textInAXFocusedElement = """
-that's " four quotes " in " there "
-"""
-        app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.h(on: $0) }
-        applyMove { asNormalMode.F(to: "\"", on: $0) }
-        let accessibilityElement = applyMoveBeingTested(using: "\"")
-        
-        XCTAssertEqual(accessibilityElement?.fileText.value, """
-that's " four quotes " in ""
-"""
-        )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 27)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-        XCTAssertEqual(accessibilityElement?.selectedText, "")
-    }
-        
-}
-
-
-// PGR
-extension ASUI_NM_ciInnerQuotedString_Tests {
-    
     func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
         let textInAXFocusedElement = """
 finally dealing with the "real stuff"!
@@ -128,7 +13,8 @@ finally dealing with the "real stuff"!
         app.textFields.firstMatch.typeText(textInAXFocusedElement)
         
         applyMove { asNormalMode.F(to: "l", on: $0) }
-        let accessibilityElement = applyMoveBeingTested(using: "\"", pgR: true)
+        var bipped = false
+        let accessibilityElement = applyMove { asNormalMode.ciInnerQuotedString(using: "\"", on: $0, pgR: true, &bipped) }
         
         XCTAssertEqual(accessibilityElement?.fileText.value, """
 finally dealing with the "!
