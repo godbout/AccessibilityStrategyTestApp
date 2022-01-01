0aadf6d58c949b98cd603c68e60d885dbc2f4cba
Guill Lo <guill.bout@gmail.com>
2022-01-02 03:18:06 +0800

    ASUT NM C back

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_C__Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_C__Tests.swift
@@ -2,6 +2,141 @@
 import XCTest
 
 
-// all tests moved to UI Tests due to PGR that requires a new implementation
-// where we use the AX within the move itself to bypass possible restrictions. hence UI Tests.
-class ASUT_NM_C__Tests: ASUT_NM_BaseTests {}
+class ASUT_NM_C__Tests: ASUT_NM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+        return asNormalMode.C(on: element, pgR: false) 
+    } 
+    
+}
+
+
+
+// copy deleted text
+extension ASUT_NM_C__Tests {
+    
+    func test_that_it_copies_the_deleted_text_in_the_pasteboard() {
+        let text = """
+C will now work with file lines and is supposed to delete from the caret ☀️ to before the linefeed
+and of course this is in the case there is a linefeed at the end of the line.
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 176,
+            caretLocation: 55,
+            selectedLength: 1,
+            selectedText: "t",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 176,
+                number: 2,
+                start: 51,
+                end: 99
+            )!
+        )
+        
+        copyToClipboard(text: "some fake shit")
+        _ = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "te from the caret ☀️ to before the linefeed")
+    }
+    
+}
+
+
+// both
+extension ASUT_NM_C__Tests {
+    
+    func test_that_if_a_file_line_does_not_end_with_a_linefeed_it_deletes_from_the_caret_to_the_end_of_the_line() {
+        let text = """
+this time the line will not end with a linefeed so C should delete from the caret till the end!
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 95,
+            caretLocation: 11,
+            selectedLength: 1,
+            selectedText: "h",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 95,
+                number: 1,
+                start: 0,
+                end: 53
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 11)
+        XCTAssertEqual(returnedElement?.selectedLength, 84)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+    
+}
+
+
+// TextViews
+extension ASUT_NM_C__Tests {
+
+    func test_that_if_a_file_line_ends_with_a_linefeed_it_deletes_from_the_caret_to_before_that_linefeed() {
+        let text = """
+C will now work with file lines and is supposed to delete from the caret ☀️ to before the linefeed
+and of course this is in the case there is a linefeed at the end of the line.
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 176,
+            caretLocation: 55,
+            selectedLength: 1,
+            selectedText: "t",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 176,
+                number: 2,
+                start: 51,
+                end: 99
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+                
+        XCTAssertEqual(returnedElement?.caretLocation, 55)
+        XCTAssertEqual(returnedElement?.selectedLength, 43)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+    
+    func test_that_it_does_not_delete_the_linefeed_even_for_an_empty_line() {
+        let text = """
+now we have an empty line and C should behave
+
+and not delete that fucking shit
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 79,
+            caretLocation: 46,
+            selectedLength: 0,
+            selectedText: "",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 79,
+                number: 2,
+                start: 46,
+                end: 47
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+                
+        XCTAssertEqual(returnedElement?.caretLocation, 46)
+        XCTAssertEqual(returnedElement?.selectedLength, 0)
+        XCTAssertNil(returnedElement?.selectedText)
+    }
+
+}
+
--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_C__Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_C__Tests.swift
@@ -2,117 +2,8 @@
 import XCTest
 
 
-class ASUI_NM_C__Tests: ASUI_NM_BaseTests {
-    
-    private func applyMoveBeingTested(pgR: Bool = false) -> AccessibilityTextElement? {
-        return applyMove { asNormalMode.C(on: $0, pgR: pgR) }
-    }
-    
-}
-
-
-// copy deleted text
-extension ASUI_NM_C__Tests {
-    
-    func test_that_it_copies_the_deleted_text_in_the_pasteboard() {
-        let textInAXFocusedElement = """
-C will now work with file lines and is supposed to delete from the caret ☀️ to before the linefeed
-and of course this is in the case there is a linefeed at the end of the line.
-"""
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.gg(on: $0) }
-        applyMove { asNormalMode.f(times: 3, to: "t", on: $0) }
-        copyToClipboard(text: "some fake shit")
-        _ = applyMoveBeingTested()
-        
-        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "te from the caret ☀️ to before the linefeed")
-    }
-    
-}
-
-
-// both
-extension ASUI_NM_C__Tests {
-    
-    func test_that_if_a_file_line_does_not_end_with_a_linefeed_it_deletes_from_the_caret_to_the_end_of_the_line() {
-        let textInAXFocusedElement = """
-this time the line will not end with a linefeed so C should delete from the caret till the end!
-"""
-        app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.zero(on: $0) }
-        applyMove { asNormalMode.f(times: 2, to: "h", on: $0) }
-        let accessibilityElement = applyMoveBeingTested()
-        
-        XCTAssertEqual(accessibilityElement?.fileText.value, """
-this time t
-"""
-        )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 11)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-        XCTAssertEqual(accessibilityElement?.selectedText, "")
-    }
-    
-}
-
-
-// TextViews
-extension ASUI_NM_C__Tests {
-
-    func test_that_if_a_file_line_ends_with_a_linefeed_it_deletes_from_the_caret_to_before_that_linefeed() {
-        let textInAXFocusedElement = """
-C will now work with file lines and is supposed to delete from the caret ☀️ to before the linefeed
-and of course this is in the case there is a linefeed at the end of the line.
-"""
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.gg(on: $0) }
-        applyMove { asNormalMode.f(times: 3, to: "t", on: $0) }
-        let accessibilityElement = applyMoveBeingTested()
-                
-        XCTAssertEqual(accessibilityElement?.fileText.value, """
-C will now work with file lines and is supposed to dele
-and of course this is in the case there is a linefeed at the end of the line.
-"""
-        )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 55)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-        XCTAssertEqual(accessibilityElement?.selectedText, "")
-    }
-    
-    func test_that_it_does_not_delete_the_linefeed_even_for_an_empty_line() {
-        let textInAXFocusedElement = """
-now we have an empty line and C should behave
-
-and not delete that fucking shit
-"""
-        app.textViews.firstMatch.tap()
-        app.textViews.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.zero(on: $0) }
-        applyMove { asNormalMode.ge(on: $0) }
-        let accessibilityElement = applyMoveBeingTested()
-                
-        XCTAssertEqual(accessibilityElement?.fileText.value, """
-now we have an empty line and C should behave
-
-and not delete that fucking shit
-"""
-        )
-        XCTAssertEqual(accessibilityElement?.caretLocation, 46)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-        XCTAssertEqual(accessibilityElement?.selectedText, "")
-    }
-
-}
-
-
 // PGR
-extension ASUI_NM_C__Tests {
+class ASUI_NM_C__Tests: ASUI_NM_BaseTests {
     
     func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
         let textInAXFocusedElement = """
@@ -124,7 +15,7 @@ and of course this is in the case there is a linefeed at the end of the line.
         
         applyMove { asNormalMode.gg(on: $0) }
         applyMove { asNormalMode.f(times: 3, to: "t", on: $0) }
-        let accessibilityElement = applyMoveBeingTested(pgR: true)
+        let accessibilityElement = applyMove { asNormalMode.C(on: $0, pgR: true) }
                 
         XCTAssertEqual(accessibilityElement?.fileText.value, """
 C will now work with file lines and is supposed to del
