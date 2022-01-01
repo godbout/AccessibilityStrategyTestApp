b0b7e19000b9a50a4e10c72965fe3e9c617e61e7
Guill Lo <guill.bout@gmail.com>
2022-01-02 04:02:46 +0800

    ASUT caw back

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_<<_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_<<_Tests.swift
@@ -2,5 +2,6 @@
 import XCTest
 
 
-// UIT.
+// those are like d moves. we need to replace the caret positiona after the delete
+// so UIT.
 class ASUT_NM_leftChevronLeftChevron_Tests: ASUT_NM_BaseTests {}
--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_caw_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_caw_Tests.swift
@@ -2,8 +2,7 @@
 import XCTest
 
 
-// here we test that the inout param is correct.
-// rest of tests are in UIT. see ciw for blah blah
+// see ciw for blah blah
 class ASUT_NM_caw_Tests: ASUT_NM_BaseTests {
     
     private func applyMoveBeingTested(on element: AccessibilityTextElement?, pgR: Bool = false, _ bipped: inout Bool) -> AccessibilityTextElement? {
@@ -13,6 +12,99 @@ class ASUT_NM_caw_Tests: ASUT_NM_BaseTests {
 }
 
 
+// copy deleted text
+extension ASUT_NM_caw_Tests {
+    
+    func test_that_it_copies_the_deleted_text_in_the_pasteboard() {
+        let text = "that's some cute      text in here don't you think?"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 51,
+            caretLocation: 14,
+            selectedLength: 1,
+            selectedText: "t",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 51,
+                number: 1,
+                start: 0,
+                end: 51
+            )!
+        )
+        
+        copyToClipboard(text: "some fake shit")
+        var bipped = false
+        _ = applyMoveBeingTested(on: element, &bipped)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "cute      ")
+    }
+    
+}
+
+
+// Both
+extension ASUT_NM_caw_Tests {
+    
+    func test_that_when_it_finds_a_word_it_selects_the_range_and_will_delete_the_selection() {
+        let text = "that's some cute      text in here don't you think?"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 51,
+            caretLocation: 14,
+            selectedLength: 1,
+            selectedText: "t",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 51,
+                number: 1,
+                start: 0,
+                end: 51
+            )!
+        )
+        
+        var bipped = false
+        let returnedElement = applyMoveBeingTested(on: element, &bipped)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 12)
+        XCTAssertEqual(returnedElement?.selectedLength, 10)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+
+    func test_that_when_it_cannot_find_a_word_the_caret_goes_to_the_end_limit_of_the_text() {
+        let text = """
+some text
+and also a lot of spaces at the end of this line        
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 66,
+            caretLocation: 60,
+            selectedLength: 1,
+            selectedText: " ",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 66,
+                number: 2,
+                start: 10,
+                end: 66
+            )!
+        )
+        
+        var bipped = false
+        let returnedElement = applyMoveBeingTested(on: element, &bipped)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 65)
+        XCTAssertEqual(returnedElement?.selectedLength, 1)
+        XCTAssertNil(returnedElement?.selectedText)
+   }
+    
+}
+
+
+// bipped
 extension ASUT_NM_caw_Tests {
     
     func test_that_it_does_not_Bip_when_it_can_find_aWord() {
