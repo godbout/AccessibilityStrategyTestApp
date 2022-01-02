514a7e5f353ce1dcbbbe16351985e51896028dbd
Guill Lo <guill.bout@gmail.com>
2022-01-02 16:48:44 +0800

    add back applyMoveBeingTested style in UIT as it is more readable

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ce_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ce_Tests.swift
@@ -5,6 +5,15 @@ import XCTest
 // PGR
 class ASUI_NM_ce_Tests: ASUI_NM_BaseTests {
     
+    private func applyMoveBeingTested(pgR: Bool) -> AccessibilityTextElement? {
+        return applyMove { asNormalMode.ce(on: $0, pgR: pgR) }
+    }
+    
+}
+
+
+extension ASUI_NM_ce_Tests {
+    
     func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
         let textInAXFocusedElement = "😂️😂️😂️😂️ gonna use ce on this sentence"
         app.textFields.firstMatch.tap()
@@ -12,8 +21,7 @@ class ASUI_NM_ce_Tests: ASUI_NM_BaseTests {
         
         applyMove { asNormalMode.zero(on: $0) }
         applyMove { asNormalMode.l(on: $0) }
-        let accessibilityElement = applyMove { asNormalMode.ce(on: $0, pgR: true) }
-           
+        let accessibilityElement = applyMoveBeingTested(pgR: true)
         
         XCTAssertEqual(accessibilityElement?.fileText.value, " gonna use ce on this sentence")
         XCTAssertEqual(accessibilityElement?.caretLocation, 0)
