bae85a9b2365df5f79c3ac90dcfbb1b5b643b653
Guill Lo <guill.bout@gmail.com>
2022-01-02 17:22:35 +0800

    ASUT cgg back

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_cgg_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_cgg_Tests.swift
@@ -2,5 +2,79 @@
 import XCTest
 
 
-// moves to UI coz PGR
-class ASUT_NM_cgg_Tests: ASUT_NM_BaseTests {}
+class ASUT_NM_cgg_Tests: ASUT_NM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement?) -> AccessibilityTextElement? {
+        return asNormalMode.cgg(on: element, pgR: false)
+    }
+    
+}
+
+
+// Both
+extension ASUT_NM_cgg_Tests {
+    
+    func test_that_it_deletes_the_line_up_to_the_firstNonBlankLimit() {
+        let text = "    this is a single line ‼️‼️‼️"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 32,
+            caretLocation: 14,
+            selectedLength: 1,
+            selectedText: "s",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 32,
+                number: 1,
+                start: 0,
+                end: 32
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 4)
+        XCTAssertEqual(returnedElement?.selectedLength, 28)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+    
+}
+
+
+// TextViews
+extension ASUT_NM_cgg_Tests {
+    
+    func test_that_it_deletes_from_the_firstNonBlankLimit_of_the_current_line_to_the_end_of_the_TextView() {
+        let text = """
+  blah blah some line
+some more
+haha geh
+need to deal with
+those faces 🥺️☹️😂️
+
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 80,
+            caretLocation: 37,
+            selectedLength: 1,
+            selectedText: "g",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 80,
+                number: 3,
+                start: 32,
+                end: 41
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 2)
+        XCTAssertEqual(returnedElement?.selectedLength, 38)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+    
+}
