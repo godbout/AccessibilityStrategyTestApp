e86db16cdfac827d256b0e00e7cc2f75c373de0d
Guill Lo <guill.bout@gmail.com>
2022-01-02 14:42:28 +0800

    ASUT cB back

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cB__Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cB__Tests.swift
@@ -2,46 +2,18 @@
 import XCTest
 
 
+// PGR
 class ASUI_NM_cB__Tests: ASUI_NM_BaseTests {
     
-    private func applyMoveBeingTested(pgR: Bool = false) -> AccessibilityTextElement? {
-        return applyMove { asNormalMode.cB(on: $0, pgR: pgR) }
-    }
-    
-}
-
-
-// both
-extension ASUI_NM_cB__Tests {
-    
-    func test_that_in_normal_setting_it_selects_the_text_from_the_caret_to_the_character_found() {
-        let textInAXFocusedElement = "so we gonna⏰️⏰️trytouse cb here and see 😂️😂️ if it works ⏰️"
-        app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.F(to: "u", on: $0) }
-        let accessibilityElement = applyMoveBeingTested()
-        
-        XCTAssertEqual(accessibilityElement?.fileText.value, "so we use cb here and see 😂️😂️ if it works ⏰️")
-        XCTAssertEqual(accessibilityElement?.caretLocation, 6)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-        XCTAssertEqual(accessibilityElement?.selectedText, "")
-    }
-   
-}
-
-
-// PGR
-extension ASUI_NM_cB__Tests {
-
     func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
         let textInAXFocusedElement = "so we gonna⏰️⏰️trytouse cb here and see 😂️😂️ if it works ⏰️"
         app.textFields.firstMatch.tap()
         app.textFields.firstMatch.typeText(textInAXFocusedElement)
         
         applyMove { asNormalMode.F(to: "u", on: $0) }
-        let accessibilityElement = applyMoveBeingTested(pgR: true)
+        let accessibilityElement = applyMove { asNormalMode.cB(on: $0, pgR: true) }
         
+
         XCTAssertEqual(accessibilityElement?.fileText.value, "so weuse cb here and see 😂️😂️ if it works ⏰️")
         XCTAssertEqual(accessibilityElement?.caretLocation, 5)
         XCTAssertEqual(accessibilityElement?.selectedLength, 0)
