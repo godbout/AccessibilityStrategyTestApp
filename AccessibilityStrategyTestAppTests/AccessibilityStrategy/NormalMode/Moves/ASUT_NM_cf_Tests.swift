8b80ab921ecd86ffd0e0e366d000cbb25aed75ac
Guill Lo <guill.bout@gmail.com>
2022-01-02 17:09:20 +0800

    ASUT cf back

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_cf_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_cf_Tests.swift
@@ -13,8 +13,64 @@ class ASUT_NM_cf_Tests: ASUT_NM_BaseTests {
 }
 
 
+// count
+extension ASUT_NM_cf_Tests {
+    
+    func test_that_it_implements_the_count_system() {
+        let text = "here we gonna delete up to 🕑️ characters rather than 🦴️!"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 58,
+            caretLocation: 19,
+            selectedLength: 1,
+            selectedText: "e",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 58,
+                number: 1,
+                start: 0,
+                end: 58
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(times: 2, to: "e", on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 19)
+        XCTAssertEqual(returnedElement?.selectedLength, 28)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
+    
+}
+
+
 // Both
 extension ASUT_NM_cf_Tests {
+    
+    func test_that_in_normal_setting_it_selects_the_text_from_the_caret_to_the_character_found() {
+        let text = "😂️😂️😂️😂️ gonna use cf on this sentence"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 42,
+            caretLocation: 3,
+            selectedLength: 3,
+            selectedText: "😂️",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 42,
+                number: 1,
+                start: 0,
+                end: 42
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(to: "s", on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 3)
+        XCTAssertEqual(returnedElement?.selectedLength, 18)
+        XCTAssertEqual(returnedElement?.selectedText, "")
+    }
     
     func test_that_if_the_character_is_not_found_then_it_does_nothing() {
         let text = """
@@ -44,6 +100,41 @@ that is not there
         XCTAssertEqual(returnedElement?.caretLocation, 14)
         XCTAssertEqual(returnedElement?.selectedLength, 1)
         XCTAssertNil(returnedElement?.selectedText)
+    }
+    
+}
+
+
+// TextViews
+extension ASUT_NM_cf_Tests {
+    
+    func test_that_it_can_find_the_character_on_a_line_for_a_multiline() {
+        let text = """
+cf on a multiline
+should work
+on a line
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 39,
+            caretLocation: 18,
+            selectedLength: 1,
+            selectedText: "s",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 39,
+                number: 2,
+                start: 18,
+                end: 30
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(to: "w", on: element)
+        
+        XCTAssertEqual(returnedElement?.caretLocation, 18)
+        XCTAssertEqual(returnedElement?.selectedLength, 8)
+        XCTAssertEqual(returnedElement?.selectedText, "")
     }
     
 }
