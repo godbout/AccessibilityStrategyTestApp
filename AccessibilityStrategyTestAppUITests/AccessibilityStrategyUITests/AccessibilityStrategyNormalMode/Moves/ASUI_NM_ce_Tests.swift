c5c44dcadb1bb2e2f9cf6c5530eb4ff2f6bbb648
Guill Lo <guill.bout@gmail.com>
2022-01-04 23:09:02 +0800

    merge UIT cE ce to cEe

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ce_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ce_Tests.swift
@@ -2,33 +2,5 @@
 import XCTest
 
 
-class ASUI_NM_ce_Tests: ASUI_NM_BaseTests {
-    
-    private func applyMoveBeingTested(pgR: Bool) -> AccessibilityTextElement? {
-        var state = VimEngineState(pgR: pgR)
-        
-        return applyMove { asNormalMode.ce(on: $0, &state) }
-    }
-    
-}
-
-
-// PGR
-extension ASUI_NM_ce_Tests {
-    
-    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
-        let textInAXFocusedElement = "😂️😂️😂️😂️ gonna use ce on this sentence"
-        app.textFields.firstMatch.tap()
-        app.textFields.firstMatch.typeText(textInAXFocusedElement)
-        
-        applyMove { asNormalMode.zero(on: $0) }
-        applyMove { asNormalMode.l(on: $0) }
-        let accessibilityElement = applyMoveBeingTested(pgR: true)
-        
-        XCTAssertEqual(accessibilityElement?.fileText.value, " gonna use ce on this sentence")
-        XCTAssertEqual(accessibilityElement?.caretLocation, 0)
-        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
-        XCTAssertEqual(accessibilityElement?.selectedText, "")
-    }
-   
-}
+// see cEe for blah blah
+class ASUI_NM_ce_Tests: ASUI_NM_BaseTests {}
---
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Supermen/ASUI_NM_cEe_Tests.swift
@@ -0,0 +1,35 @@
+@testable import AccessibilityStrategy
+import XCTest
+
+
+// no blah blah :D
+class ASUI_NM_cEe_Tests: ASUI_NM_BaseTests {
+    
+    private func applyMoveBeingTested(pgR: Bool) -> AccessibilityTextElement? {
+        var state = VimEngineState(pgR: pgR)
+        
+        return applyMove { asNormalMode.cEe(on: $0, using: asNormalMode.e, &state) }
+    }
+    
+}
+
+
+// PGR
+extension ASUI_NM_cEe_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = "😂️😂️😂️😂️ gonna use ce on this sentence"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.l(on: $0) }
+        let accessibilityElement = applyMoveBeingTested(pgR: true)
+        
+        XCTAssertEqual(accessibilityElement?.fileText.value, " gonna use ce on this sentence")
+        XCTAssertEqual(accessibilityElement?.caretLocation, 0)
+        XCTAssertEqual(accessibilityElement?.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement?.selectedText, "")
+    }
+   
+}
