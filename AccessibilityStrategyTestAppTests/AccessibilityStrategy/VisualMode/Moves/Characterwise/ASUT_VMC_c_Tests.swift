81acbc66675739982618fd3eeb9c6bc26475aba0
Guill Lo <guill.bout@gmail.com>
2022-01-08 16:03:44 +0800

    move ASUI VMC c to ASUT now that we refactored VMC c PGR

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC_c_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC_c_Tests.swift
@@ -1,6 +1,123 @@
 @testable import AccessibilityStrategy
 import XCTest
+import VimEngineState
 
 
-// moved to UIT coz PGR.
-class ASUT_VMC_c_Tests: ASVM_BaseTests {}
+// PGR in UIT.
+class ASUT_VMC_c_Tests: ASVM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement) -> AccessibilityTextElement {
+        var state = VimEngineState(pgR: false, visualModeStyle: .characterwise)
+        
+        return applyMoveBeingTested(on: element, &state)
+    }
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement, _ vimEngineState: inout VimEngineState) -> AccessibilityTextElement {
+        return asVisualMode.c(on: element, &vimEngineState)
+    }
+    
+}
+
+
+// Bip, copy deletion and LYS
+// no that accurate. an empty text shouldn't copy nothing to clipboard.
+// currently does i believe. those edge cases are complicated and painful
+// so fuck'em for now.
+extension ASUT_VMC_c_Tests {
+
+    func test_that_it_always_does_not_Bip_and_sets_the_LastYankStyle_to_Characterwise_and_copies_the_deletion_even_for_an_empty_line() {
+        let text = """
+all that VM d does
+in characterwi😂️e is deleting
+the selection!
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 64,
+            caretLocation: 14,
+            selectedLength: 19,
+            selectedText: "does\nin characterwi",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 64,
+                number: 1,
+                start: 0,
+                end: 19
+            )!
+        )
+        
+        copyToClipboard(text: "some fake shit")
+        var state = VimEngineState(lastMoveBipped: true, lastYankStyle: .linewise)
+        _ = applyMoveBeingTested(on: element, &state)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), """
+does
+in characterwi
+"""
+        )
+        XCTAssertEqual(state.lastYankStyle, .characterwise)
+        XCTAssertFalse(state.lastMoveBipped)
+    }
+    
+}
+
+
+// Both
+extension ASUT_VMC_c_Tests {
+
+    func test_that_when_the_selection_is_spanning_on_a_single_line_it_deletes_the_selected_text() {
+        let text = "ok so VM c (hahaha) on a single line"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 36,
+            caretLocation: 14,
+            selectedLength: 13,
+            selectedText: "haha) on a si",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 36,
+                number: 1,
+                start: 0,
+                end: 36
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement.caretLocation, 14)
+        XCTAssertEqual(returnedElement.selectedLength, 13)
+        XCTAssertEqual(returnedElement.selectedText, "")
+    }
+    
+    func test_that_when_the_selection_is_spanning_on_multiple_lines_it_deletes_the_selected_text() {
+        let text = """
+like same as above
+but on multiple
+lines because
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 48,
+            caretLocation: 8,
+            selectedLength: 36,
+            selectedText: "e as above\nbut on multiple\nlines bec",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 48,
+                number: 1,
+                start: 0,
+                end: 19
+            )!
+        )
+                
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement.caretLocation, 8)
+        XCTAssertEqual(returnedElement.selectedLength, 36)
+        XCTAssertEqual(returnedElement.selectedText, "")
+    }
+    
+}
