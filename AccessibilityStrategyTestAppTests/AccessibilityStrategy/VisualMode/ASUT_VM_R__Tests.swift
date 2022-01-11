48875784ab342d8cda145321bbd503ab7eab68ef
Guill Lo <guill.bout@gmail.com>
2022-01-11 17:54:18 +0800

    add UT for VM R S

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/ASUT_VM_R__Tests.swift
@@ -0,0 +1,6 @@
+@testable import AccessibilityStrategy
+import XCTest
+
+
+// currently calls S so no tests here.
+class ASUT_VM_R__Tests: ASVM_BaseTests {}
---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/ASUT_VM_S__Tests.swift
@@ -0,0 +1,6 @@
+@testable import AccessibilityStrategy
+import XCTest
+
+
+// currently calls C so no tests here.
+class ASUT_VM_S__Tests: ASVM_BaseTests {}
