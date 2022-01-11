48fa04f09cbeca78f407cd83a2dda7dc1497fd91
Guill Lo <guill.bout@gmail.com>
2022-01-11 17:10:13 +0800

    add UIT cl for PGR, update UT cl for LYS copy

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cl_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cl_Tests.swift
@@ -1,8 +1,38 @@
-//
-//  ASUI_NM_cl_Tests.swift
-//  AccessibilityStrategyNormaModeUITests
-//
-//  Created by Guillaume Leclerc on 11/01/2022.
-//
+import XCTest
+@testable import AccessibilityStrategy
+import VimEngineState
 
-import Foundation
+
+class ASUI_NM_cl_Tests: ASUI_NM_BaseTests {
+    
+    private func applyMoveBeingTested() -> AccessibilityTextElement {
+        var state = VimEngineState()
+        
+        return applyMoveBeingTested(&state)
+    }
+        
+    private func applyMoveBeingTested(_ vimEngineState: inout VimEngineState) -> AccessibilityTextElement {
+        return applyMove { asNormalMode.cl(on: $0, &vimEngineState) }
+    }
+    
+}
+
+
+// PGR
+extension ASUI_NM_cl_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = "x should delete the right character"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+
+        applyMove { asNormalMode.b(on: $0) }
+        var state = VimEngineState(pgR: true)
+        let accessibilityElement = applyMoveBeingTested(&state)
+
+        XCTAssertEqual(accessibilityElement.fileText.value, "x should delete the rightharacter")
+        XCTAssertEqual(accessibilityElement.caretLocation, 25)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+    }
+    
+}
