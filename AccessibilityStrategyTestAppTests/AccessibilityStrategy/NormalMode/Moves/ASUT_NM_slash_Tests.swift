96df80ced338def752978983682826550a9c020c
Guill Lo <guill.bout@gmail.com>
2022-02-10 21:17:28 +0800

    add UT for ASNM / for count, various empty stuff that crash, etc

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_slash_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_slash_Tests.swift
@@ -67,7 +67,6 @@ extension ASUT_NM_slash_Tests {
 }
 
 
-
 // Both
 extension ASUT_NM_slash_Tests {
     
@@ -121,7 +120,60 @@ extension ASUT_NM_slash_Tests {
         XCTAssertNil(returnedElement.selectedText)
     }
     
-    func test_that_if_there_is_a_slash_in_the_searchString_then_the_searchPattern_is_what_comes_before_that_slash() {
+    func test_that_it_does_not_crash_if_the_text_is_empty() {
+        let text = ""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 0,
+            caretLocation: 0,
+            selectedLength: 0,
+            selectedText: "",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 0,
+                number: 1,
+                start: 0,
+                end: 0
+            )!
+        )
+            
+        let returnedElement = applyMoveBeingTested(to: "hehe", on: element)
+        
+        XCTAssertEqual(returnedElement.caretLocation, 0)
+        XCTAssertEqual(returnedElement.selectedLength, 0)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+    func test_that_if_the_searchPattern_is_not_found_then_the_caret_does_not_move() {
+        let text = """
+gonna look for a search pattern that is not there
+and the caret shouldn't move else pan pan cul cul
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 99,
+            caretLocation: 24,
+            selectedLength: 1,
+            selectedText: "p",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 99,
+                number: 1,
+                start: 0,
+                end: 50
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(to: "noooooooope", on: element)
+        
+        XCTAssertEqual(returnedElement.caretLocation, 24)
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+    func test_that_if_there_is_a_slash_in_the_searchString_then_the_searchPattern_is_considered_what_comes_before_that_slash() {
         let text = "ok so now we gonna use a searchString with a / inside"
         let element = AccessibilityTextElement(
             role: .textField,
@@ -146,59 +198,34 @@ extension ASUT_NM_slash_Tests {
         XCTAssertNil(returnedElement.selectedText)
     }
     
-//    func test_that_if_the_character_is_not_found_then_the_caret_does_not_move() {
-//        let text = """
-//gonna look for a character that is not there
-//and the caret shouldn't move else pan pan cul cul
-//"""
-//        let element = AccessibilityTextElement(
-//            role: .textArea,
-//            value: text,
-//            length: 94,
-//            caretLocation: 22,
-//            selectedLength: 1,
-//            selectedText: "c",
-//            currentScreenLine: ScreenLine(
-//                fullTextValue: text,
-//                fullTextLength: 94,
-//                number: 3,
-//                start: 17,
-//                end: 27
-//            )!
-//        )
-//        
-//        let returnedElement = applyMoveBeingTested(to: "z", on: element)
-//        
-//        XCTAssertEqual(returnedElement.caretLocation, 22)
-//        XCTAssertEqual(returnedElement.selectedLength, 1)
-//        XCTAssertNil(returnedElement.selectedText)
-//    }
-//    
-//    func test_that_if_it_is_on_a_character_and_we_look_for_the_same_character_it_moves_to_that_next_occurrence() {
-//        let text = """
-//it you're already on a z for example and wanna go to the next z it should work
-//"""
-//        let element = AccessibilityTextElement(
-//            role: .textArea,
-//            value: text,
-//            length: 78,
-//            caretLocation: 23,
-//            selectedLength: 1,
-//            selectedText: "z",
-//            currentScreenLine: ScreenLine(
-//                fullTextValue: text,
-//                fullTextLength: 78,
-//                number: 2,
-//                start: 10,
-//                end: 25
-//            )!
-//        )
-//        
-//        let returnedElement = applyMoveBeingTested(to: "z", on: element)
-//        
-//        XCTAssertEqual(returnedElement.caretLocation, 62)
-//        XCTAssertEqual(returnedElement.selectedLength, 1)
-//        XCTAssertNil(returnedElement.selectedText)
-//    }
-    
+    func test_that_if_we_are_already_located_at_the_searchPattern_then_it_goes_to_the_next_found_and_does_not_get_stuck_at_the_current_one() {
+        let text = """
+ok time to play
+with multilines and play
+also with the caret getting
+blocked haha. play
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 87,
+            caretLocation: 11,
+            selectedLength: 1,
+            selectedText: "p",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 87,
+                number: 1,
+                start: 0,
+                end: 16
+            )!
+        )
+            
+        let returnedElement = applyMoveBeingTested(to: "play", on: element)
+        
+        XCTAssertEqual(returnedElement.caretLocation, 36)
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+
 }
