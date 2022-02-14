2de1727ba97a58eba0af3deafe097bed0bc4238d
Guill Lo <guill.bout@gmail.com>
2022-02-14 15:39:06 +0800

    add ASUT for NM h count

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_h_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_h_Tests.swift
@@ -4,10 +4,69 @@ import XCTest
 
 class ASUT_NM_h_Tests: ASUT_NM_BaseTests {
     
-    private func applyMoveBeingTested(on element: AccessibilityTextElement) -> AccessibilityTextElement {
-        return asNormalMode.h(on: element) 
+    private func applyMoveBeingTested(times count: Int = 1, on element: AccessibilityTextElement) -> AccessibilityTextElement {
+        return asNormalMode.h(times: count, on: element) 
     }
     
+}
+
+
+// count
+extension ASUT_NM_h_Tests {
+    
+    func test_that_it_implements_the_count_system() {
+        let text = "we gonna move in there with count 🈹️ awww"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 42,
+            caretLocation: 28,
+            selectedLength: 1,
+            selectedText: "c",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 42,
+                number: 1,
+                start: 0,
+                end: 42
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(times: 8, on: element)
+
+        XCTAssertEqual(returnedElement.caretLocation, 20)
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+    func test_that_it_stops_at_the_end_limit_if_the_count_goes_above_it() {
+        let text = """
+we gonna move
+in there with
+count 🈹️ awww
+"""
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 42,
+            caretLocation: 19,
+            selectedLength: 1,
+            selectedText: "c",
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 42,
+                number: 1,
+                start: 0,
+                end: 42
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(times: 69, on: element)
+
+        XCTAssertEqual(returnedElement.caretLocation, 14)
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
 }
 
 
