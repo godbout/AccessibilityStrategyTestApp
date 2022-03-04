1ee339986d50d3be390c63e5055a09252b07b44b
Guill Lo <guill.bout@gmail.com>
2022-03-04 19:14:20 +0800

    update ASNM dBb PGR test

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Supermen/ASUI_NM_dBb_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Supermen/ASUI_NM_dBb_Tests.swift
@@ -38,15 +38,15 @@ extension ASUI_NM_dBb_Tests {
 extension ASUI_NM_dBb_Tests {
     
     func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
-        let textInAXFocusedElement = "so we gonna⏰️⏰️trytouse cb here and see 😂️😂️ if it works ⏰️"
+        let textInAXFocusedElement = "so we gonna⏰️trytouse cb here and see 😂️😂️ if it works ⏰️"
         app.textFields.firstMatch.tap()
         app.textFields.firstMatch.typeText(textInAXFocusedElement)
         
         applyMove { asNormalMode.F(to: "u", on: $0) }
         let accessibilityElement = applyMoveBeingTested(appFamily: .pgR)
         
-        XCTAssertEqual(accessibilityElement.fileText.value, "so we gonna⏰️use cb here and see 😂️😂️ if it works ⏰️")
-        XCTAssertEqual(accessibilityElement.caretLocation, 13)
+        XCTAssertEqual(accessibilityElement.fileText.value, "so we gonnause cb here and see 😂️😂️ if it works ⏰️")
+        XCTAssertEqual(accessibilityElement.caretLocation, 11)
         XCTAssertEqual(accessibilityElement.selectedLength, 1)
         XCTAssertEqual(accessibilityElement.selectedText, "u")
     }
