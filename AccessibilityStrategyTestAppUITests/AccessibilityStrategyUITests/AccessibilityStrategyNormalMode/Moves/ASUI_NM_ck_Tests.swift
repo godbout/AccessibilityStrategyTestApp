f3106fc79aa3f237c4fcf836fe80bf4a5f5ab08d
Guill Lo <guill.bout@gmail.com>
2022-03-05 23:00:48 +0800

    add UIT NM ck

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ck_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ck_Tests.swift
@@ -1,8 +1,42 @@
-//
-//  ASUI_NM_ck_Tests.swift
-//  AccessibilityStrategyTestAppTests
-//
-//  Created by Guillaume Leclerc on 05/03/2022.
-//
+@testable import AccessibilityStrategy
+import XCTest
+import Common
 
-import Foundation
+
+class ASUI_NM_ck_Tests: ASUI_NM_BaseTests {
+    
+    private func applyMoveBeingTested(appFamily: AppFamily) -> AccessibilityTextElement {
+        var state = VimEngineState(appFamily: appFamily)
+        
+        return applyMove { asNormalMode.ck(on: $0, &state) }
+    }
+    
+}
+
+
+// PGR and Electron
+extension ASUI_NM_ck_Tests {
+
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = """
+but the indent should
+   i delete a line
+be kept
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.b(on: $0) }
+        let accessibilityElement = applyMoveBeingTested(appFamily: .pgR)
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+but the indent should
+  
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 24)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement.selectedText, "")
+    }
+    
+}
