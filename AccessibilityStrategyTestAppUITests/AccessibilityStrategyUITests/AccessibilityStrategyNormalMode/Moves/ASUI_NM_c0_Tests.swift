66097b1758558e2a0878e1d1f6007b6083cbfb30
Guill Lo <guill.bout@gmail.com>
2022-03-06 19:38:32 +0800

    add UIT for ASNM c0

---
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_c0_Tests.swift
@@ -0,0 +1,9 @@
+@testable import AccessibilityStrategy
+import XCTest
+import Common
+
+
+// this can't be tested automatically.
+// because once the start of the currentFileLine is reached with the AX,
+// there's no way to test an extra delete through tap :D
+class ASUI_NM_cZero_Tests: ASUI_NM_BaseTests {}
