b7ac58a541d51597df912523a7ecaafee6691811
Guill Lo <guill.bout@gmail.com>
2022-03-14 22:38:25 +0800

    add UIT ASVM <

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyVisualMode/ASUI_VM_<_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyVisualMode/ASUI_VM_<_Tests.swift
@@ -3,100 +3,101 @@ import XCTest
 import Common
 
 
-// see < for blah blah
-class ASUI_VM_rightChevron_Tests: ASUI_VM_BaseTests {
+// same for VMC and VML
+class ASUI_VM_leftChevron_Tests: ASUI_VM_BaseTests {
     
     var state = VimEngineState(visualStyle: .characterwise)
 
     private func applyMoveBeingTested(times count: Int = 1, appFamily: AppFamily = .auto) -> AccessibilityTextElement {
         state.appFamily = appFamily
         
-        return applyMove { asVisualMode.rightChevron(times: count, on: $0, state) }
+        return applyMove { asVisualMode.leftChevron(times: count, on: $0, state) }
     }
 
 }
 
 
 // count
-extension ASUI_VM_rightChevron_Tests {
-
+extension ASUI_VM_leftChevron_Tests {
+    
     func test_that_the_count_is_implemented() {
         let textInAXFocusedElement = """
-seems that even the normal
-🖕️ase fails LMAO
-sooome more
+    seems that even the normal
+                  🖕️ase fails LMAO
 """
         app.textViews.firstMatch.tap()
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
-       
-        applyMove { asNormalMode.zero(on: $0) }
-        applyMove { asVisualMode.vFromNormalMode(on: $0) }
-        applyMove { asVisualMode.b(on: $0, state) }
+        
+        applyMove { asNormalMode.gg(on: $0) }
+        applyMove { asVisualMode.vFromNormalMode(on: $0) } 
+        applyMove { asVisualMode.G(on: $0, state) } 
         let accessibilityElement = applyMoveBeingTested(times: 3)
-            
+        
         XCTAssertEqual(accessibilityElement.fileText.value, """
 seems that even the normal
-            🖕️ase fails LMAO
-            sooome more
+      🖕️ase fails LMAO
 """
         )
-        XCTAssertEqual(accessibilityElement.caretLocation, 39)
-        XCTAssertEqual(accessibilityElement.selectedLength, 3)
+        XCTAssertEqual(accessibilityElement.caretLocation, 0)
+        XCTAssertEqual(accessibilityElement.selectedLength, 1)
     }
     
 }
 
 
 // Both
-extension ASUI_VM_rightChevron_Tests {
+extension ASUI_VM_leftChevron_Tests {
     
-    func test_that_in_normal_setting_it_adds_4_spaces_at_the_beginning_of_the_lines_that_hold_some_or_the_whole_the_selection_and_sets_the_caret_to_the_firstNonBlank_of_the_first_line_of_the_selection() {
+    func test_that_in_normal_setting_it_removes_4_spaces_at_the_beginning_of_each_line_of_the_selection_and_sets_the_caret_to_the_first_non_blank_of_the_first_line() {
         let textInAXFocusedElement = """
 seems that even the normal
-  🖕️ase fails LMAO
+       🖕️ase fails LMAO
+       🖕️ase fails LMAO
+      heheh
 some more
-and more
 """
         app.textViews.firstMatch.tap()
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
        
-        applyMove { asNormalMode.G(times: 2, on: $0) }
-        applyMove { asVisualMode.vFromNormalMode(on: $0) }
-        applyMove { asVisualMode.j(on: $0, state) }
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.k(on: $0) } 
+        applyMove { asVisualMode.vFromNormalMode(on: $0) } 
+        applyMove { asVisualMode.k(times: 2, on: $0, state) } 
         let accessibilityElement = applyMoveBeingTested()
-            
+        
         XCTAssertEqual(accessibilityElement.fileText.value, """
 seems that even the normal
-      🖕️ase fails LMAO
-    some more
-and more
+   🖕️ase fails LMAO
+   🖕️ase fails LMAO
+  heheh
+some more
 """
         )
-        XCTAssertEqual(accessibilityElement.caretLocation, 33)
+        XCTAssertEqual(accessibilityElement.caretLocation, 30)
         XCTAssertEqual(accessibilityElement.selectedLength, 3)
     }
     
-    func test_that_it_does_not_shift_empty_lines() {
+    func test_that_it_removes_all_the_spaces_if_there_are_4_or_less() {
         let textInAXFocusedElement = """
-a line empty means with nothing
-
-or just a linefeed
+ now the line
+  😀️as just two
+ spaces
 """
         app.textViews.firstMatch.tap()
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
         
-        applyMove { asNormalMode.l(on: $0) }
+        applyMove { asNormalMode.gg(on: $0) }
         applyMove { asVisualMode.vFromNormalMode(on: $0) }
-        applyMove { asVisualMode.gg(on: $0, state) }
+        applyMove { asVisualMode.G(on: $0, state) }
         let accessibilityElement = applyMoveBeingTested()
         
         XCTAssertEqual(accessibilityElement.fileText.value, """
-    a line empty means with nothing
-
-    or just a linefeed
+now the line
+😀️as just two
+spaces
 """
         )
-        XCTAssertEqual(accessibilityElement.caretLocation, 4)
+        XCTAssertEqual(accessibilityElement.caretLocation, 0)
         XCTAssertEqual(accessibilityElement.selectedLength, 1)
     }
     
@@ -104,33 +105,31 @@ or just a linefeed
 
 
 // PGR and Electron
-extension ASUI_VM_rightChevron_Tests {
+extension ASUI_VM_leftChevron_Tests {
     
     func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
         let textInAXFocusedElement = """
 seems that even the normal
-  🖕️ase fails LMAO
-some more
-and more
+hehe
+       🖕️ase fails LMAO
 """
         app.textViews.firstMatch.tap()
         app.textViews.firstMatch.typeText(textInAXFocusedElement)
        
-        applyMove { asNormalMode.G(times: 2, on: $0) }
+        applyMove { asNormalMode.zero(on: $0) }
         applyMove { asVisualMode.vFromNormalMode(on: $0) }
-        applyMove { asVisualMode.j(on: $0, state) }
+        applyMove { asNormalMode.k(on: $0) }
         let accessibilityElement = applyMoveBeingTested(appFamily: .pgR)
-            
+        
         XCTAssertEqual(accessibilityElement.fileText.value, """
 seems that even the normal
-      🖕️ase fails LMAO
-    some more      🖕️ase fails LMAO
-    some more
-and more
+hehe
+   🖕️ase fails LMAhehe
+   🖕️ase fails LMAO
 """
         )
-        XCTAssertEqual(accessibilityElement.caretLocation, 33)
-        XCTAssertEqual(accessibilityElement.selectedLength, 3)
+        XCTAssertEqual(accessibilityElement.caretLocation, 27)
+        XCTAssertEqual(accessibilityElement.selectedLength, 1)
     }
     
 }
