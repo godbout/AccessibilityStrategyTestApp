e097098560dbc4683d5481bad9648d5d23d5c137
Guill Lo <guill.bout@gmail.com>
2022-03-21 20:09:34 +0800

    move ASNM H from UIT to UT, and add scroll test

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_H__Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_H__Tests.swift
@@ -2,7 +2,6 @@
 import XCTest
 
 
-// definitely UIT
 class ASUT_NM_H__Tests: ASUT_NM_BaseTests {
 
     private func applyMoveBeingTested(times count: Int = 1, on element: AccessibilityTextElement) -> AccessibilityTextElement {
@@ -12,34 +11,183 @@ class ASUT_NM_H__Tests: ASUT_NM_BaseTests {
 }
 
 
-// line
+// scroll
 extension ASUT_NM_H__Tests {
     
-    func test_that_it_sets_the_shouldScroll_to_false() {
-        let text = """
-this move does not stop at screen 🇫🇷️ines. it will just pass by
-them like nothing happened. that's how special it is.
-"""
+    func test_that_it_should_not_scroll() {
+        let text = ""
         let element = AccessibilityTextElement(
             role: .textArea,
             value: text,
-            length: 119,
-            caretLocation: 39,
-            selectedLength: 1,
-            selectedText: "i",
-            visibleCharacterRange: 0..<119,
+            length: 0,
+            caretLocation: 0,
+            selectedLength: 0,
+            selectedText: """
+
+        """,
+            visibleCharacterRange: 0..<0,
             currentScreenLine: ScreenLine(
                 fullTextValue: text,
-                fullTextLength: 119,
-                number: 2,
-                start: 39,
-                end: 66
+                fullTextLength: 0,
+                number: 1,
+                start: 0,
+                end: 0
             )!
         )
-        
+
         let returnedElement = applyMoveBeingTested(on: element)
 
         XCTAssertFalse(returnedElement.shouldScroll)
     }
-     
+
+}
+
+
+// count
+extension ASUT_NM_H__Tests {
+
+    func test_that_it_implements_the_count_system() {
+        let text = """
+ 😂k so now we're
+going to
+have very
+
+long lines
+so that
+   😂he H
+and
+
+M
+
+and
+L
+
+can be
+tested
+
+properly!
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 107,
+            caretLocation: 80,
+            selectedLength: 1,
+            selectedText: """
+        L
+        """,
+            visibleCharacterRange: 58..<107,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 107,
+                number: 13,
+                start: 80,
+                end: 82
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(times: 3, on: element)
+
+        XCTAssertEqual(returnedElement.caretLocation, 72)
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+    }
+
+    func test_that_if_the_count_is_too_high_it_ends_up_on_the_firstNonBlank_of_the_lowest_screenLine() {
+        let text = """
+ 😂k so now we're
+going to
+have very
+
+long lines
+so that
+   😂he H
+and
+
+M
+
+and
+L
+
+can be
+tested
+
+properly!
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 107,
+            caretLocation: 80,
+            selectedLength: 1,
+            selectedText: """
+        L
+        """,
+            visibleCharacterRange: 58..<107,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 107,
+                number: 13,
+                start: 80,
+                end: 82
+            )!
+        )
+
+        let returnedElement = applyMoveBeingTested(times: 69, on: element)
+
+        XCTAssertEqual(returnedElement.caretLocation, 98)
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+    }
+
+}
+
+
+// TextViews
+extension ASUT_NM_H__Tests {
+
+    func test_that_in_normal_setting_it_goes_to_the_firstNonBlank_of_the_highest_screenLine() {
+        let text = """
+ 😂k so now we're
+going to
+have very
+
+long lines
+so that
+   😂he H
+and
+
+M
+
+and
+L
+
+can be
+tested
+
+properly!
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 107,
+            caretLocation: 76,
+            selectedLength: 1,
+            selectedText: """
+        a
+        """,
+            visibleCharacterRange: 58..<107,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 107,
+                number: 12,
+                start: 76,
+                end: 80
+            )!
+        )
+
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement.caretLocation, 61)
+        XCTAssertEqual(returnedElement.selectedLength, 2)
+    }
+
 }
