8e63983b11db81a35d3a4354b2bf928468aed1b9
Guillaume Leclerc <guill.bout@gmail.com>
2022-04-04 21:14:30 +0800

    remove VA upperbound from tests as it doesn't exist anymore

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_zb_Tests.swift
@@ -0,0 +1,8 @@
+//
+//  ASUT_NM_zb_Tests.swift
+//  AccessibilityStrategyTestAppTests
+//
+//  Created by Guillaume Leclerc on 04/04/2022.
+//
+
+import Foundation
--- AccessibilityStrategyTestAppUITests/AccessibilityTextElementAdaptorUITests/fromAXFocusedElement/FullyVisibleAreaTests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityTextElementAdaptorUITests/fromAXFocusedElement/FullyVisibleAreaTests.swift
@@ -20,7 +20,6 @@ TextArea
         let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
         
         XCTAssertEqual(accessibilityElement?.fullyVisibleArea, 0..<101)
-        XCTAssertEqual(accessibilityElement?.fullyVisibleAreaUpperBoundLimit, 101)
     }
     
     func test_that_if_the_TextArea_is_filled_with_text_and_it_is_scrolled_to_the_top_then_the_fullyVisibleArea_is_correct() {
@@ -34,7 +33,6 @@ It was the White Rabbit, trotting slowly back again, and looking anxiously about
         let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
         
         XCTAssertEqual(accessibilityElement?.fullyVisibleArea, 0..<241)
-        XCTAssertEqual(accessibilityElement?.fullyVisibleAreaUpperBoundLimit, 240)
     }
     
     func test_that_if_the_TextArea_is_filled_with_text_and_it_is_scrolled_to_the_bottom_then_the_fullyVisibleArea_is_correct() {
@@ -47,7 +45,6 @@ It was the White Rabbit, trotting slowly back again, and looking anxiously about
         let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
         
         XCTAssertEqual(accessibilityElement?.fullyVisibleArea, 136..<378)
-        XCTAssertEqual(accessibilityElement?.fullyVisibleAreaUpperBoundLimit, 378)
     }
     
     func test_that_if_the_TextArea_is_filled_with_text_and_it_is_scrolled_to_the_bottom_and_the_text_ends_with_an_empty_line_then_the_fullyVisibleArea_is_correct() {
@@ -61,7 +58,6 @@ It was the White Rabbit, trotting slowly back again, and looking anxiously about
         let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
         
         XCTAssertEqual(accessibilityElement?.fullyVisibleArea, 159..<379)
-        XCTAssertEqual(accessibilityElement?.fullyVisibleAreaUpperBoundLimit, 379)
     }
 
     func test_that_if_the_TextArea_is_filled_with_text_and_there_is_leeway_on_the_top_and_at_the_bottom_then_the_fullyVisibleArea_is_correct() {
@@ -76,7 +72,6 @@ It was the White Rabbit, trotting slowly back again, and looking anxiously about
         let accessibilityElement = AccessibilityTextElementAdaptor.fromAXFocusedElement()
         
         XCTAssertEqual(accessibilityElement?.fullyVisibleArea, 262..<515)
-        XCTAssertEqual(accessibilityElement?.fullyVisibleAreaUpperBoundLimit, 514)
     }
 
 }
