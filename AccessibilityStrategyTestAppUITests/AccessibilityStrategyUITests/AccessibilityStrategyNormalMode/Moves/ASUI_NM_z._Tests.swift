671f2f189b50cfc30ffa05b28c1642eaa893ea8c
Guillaume Leclerc <guill.bout@gmail.com>
2022-04-08 15:54:22 +0800

    add UT UIT ASNM z.

---
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_z._Tests.swift
@@ -0,0 +1,85 @@
+import XCTest
+import AccessibilityStrategy
+
+
+class ASUI_NM_zDot_Tests: ASUI_NM_BaseTests {
+    
+    private func applyMoveBeingTested(times count: Int = 1) -> AccessibilityTextElement {
+        return applyMove { asNormalMode.zDot(times: count, on: $0) }
+    }
+    
+}
+
+
+// TextViews
+extension ASUI_NM_zDot_Tests {
+
+    func test_that_in_normal_setting_it_puts_the_currentScreenLine_in_the_middle_of_the_visible_area_and_sets_the_caretLocation_to_the_firstNonBlank_of_the_line() {
+        let textInAXFocusedElement = """
+ 😂k so now we're
+going to
+have very
+
+long lines
+so that
+   😂he H
+and
+
+M
+
+and
+L
+
+can be
+tested
+
+properly!
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.G(times: 8, on: $0) }
+        applyMove { asNormalMode.l(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+    
+        XCTAssertEqual(accessibilityElement.caretLocation, 67)
+        XCTAssertEqual(accessibilityElement.selectedLength, 1)
+        XCTAssertEqual(accessibilityElement.selectedText, "a")
+        XCTAssertEqual(accessibilityElement.fullyVisibleArea, 27..<81)
+    }
+    
+    func test_that_if_on_an_emptyLine_it_works_properly_and_the_emptyLine_ends_at_the_middle_of_the_visible_area() {
+        let textInAXFocusedElement = """
+ 😂k so now we're
+going to
+have very
+
+long lines
+so that
+   😂he H
+and
+
+M
+
+and
+L
+
+can be
+tested
+
+properly!
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.G(times: 9, on: $0) }
+        applyMove { asNormalMode.l(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+    
+        XCTAssertEqual(accessibilityElement.caretLocation, 71)
+        XCTAssertEqual(accessibilityElement.selectedLength, 1)
+        XCTAssertEqual(accessibilityElement.selectedText, "\n")
+        XCTAssertEqual(accessibilityElement.fullyVisibleArea, 37..<81)
+    }
+
+}
