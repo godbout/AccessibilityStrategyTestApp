cc52eeb7cc1de307a063c72163b7294e26ec5bc0
Guillaume Leclerc <guill.bout@gmail.com>
2022-06-13 00:46:18 +0800

    add UT VMC P for LYS Characterwise

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyVisualMode/Characterwise/ASUI_VMC_PWhenLastYankStyleWasCharacterwise__Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyVisualMode/Characterwise/ASUI_VMC_PWhenLastYankStyleWasCharacterwise__Tests.swift
@@ -1,8 +1,6 @@
-//
-//  ASUI_VMC_PForLastYankStyleCharacterwise__Tests.swift
-//  AccessibilityStrategyVisualModeUITests
-//
-//  Created by Guillaume Leclerc on 12/06/2022.
-//
+import XCTest
+@testable import AccessibilityStrategy
 
-import Foundation
+
+// see ASUI VML PWhenLastYankStyleWasLinewise for blah blah (yes, Linewise)
+class ASUI_VMC_PWhenLastYankStyleWasCharacterwise_Tests: ASUI_VM_BaseTests {}
