951714b345cfd87d0bd70951362796c9fae828af
Guillaume Leclerc <guill.bout@gmail.com>
2022-06-14 15:26:47 +0800

    refctor UT innerBrackets

---
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aBlock_Tests.swift
@@ -0,0 +1,8 @@
+//
+//  FT_aBlock_Tests.swift
+//  AccessibilityStrategyTestAppTests
+//
+//  Created by Guillaume Leclerc on 14/06/2022.
+//
+
+import Foundation
--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerBrackets_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerBrackets_Tests.swift
@@ -4,8 +4,16 @@ import XCTest
 
 // this is using nextUnmatched and previousUnmatched internally, which are already tested on their own
 // so here we have just a few cases for extra care, and got the special cases for this move as usual
-class FT_innerBrackets_Tests: XCTestCase {}
-
+class FT_innerBrackets_Tests: XCTestCase {
+    
+    private func applyFuncBeingTested(on text: String, using bracket: Character, startingAt caretLocation: Int) -> Range<Int>? {
+        let fileText = FileText(end: text.utf16.count, value: text)
+        
+        return fileText.innerBrackets(using: bracket, startingAt: caretLocation)
+    }
+       
+}
+    
 
 // Both
 extension FT_innerBrackets_Tests {
@@ -13,81 +21,69 @@ extension FT_innerBrackets_Tests {
     func test_that_if_there_is_no_bracket_then_it_returns_nil() {
         let text = "how dumb can you be LOL"
         
-        let fileText = FileText(end: text.utf16.count, value: text)
+        let innerBracketsRange = applyFuncBeingTested(on: text, using: "{", startingAt: 2)
         
-        XCTAssertNil(
-            fileText.innerBrackets(using: "(", startingAt: 2)
-        )
+        XCTAssertNil(innerBracketsRange)
     }
     
     func test_that_if_there_is_only_one_bracket_then_it_returns_nil_also() {
         let text = "hey ho there's only one { in there!"
         
-        let fileText = FileText(end: text.utf16.count, value: text)
+        let innerBracketsRange = applyFuncBeingTested(on: text, using: "{", startingAt: 15)
         
-        XCTAssertNil(
-            fileText.innerBrackets(using: "{", startingAt: 15)
-        )
+        XCTAssertNil(innerBracketsRange)
     }
     
     func test_that_if_there_are_two_matched_brackets_and_the_caret_is_before_them_then_it_returns_nil() {
         let text = "still no gourmet [ shit for that ] one"
         
-        let fileText = FileText(end: text.utf16.count, value: text)
+        let innerBracketsRange = applyFuncBeingTested(on: text, using: "[", startingAt: 3)
         
-        XCTAssertNil(
-            fileText.innerBrackets(using: "[", startingAt: 3)
-        )
+        XCTAssertNil(innerBracketsRange)
     }
     
     func test_that_if_there_are_two_matched_brackets_and_the_caret_is_after_them_then_it_returns_nil() {
         let text = "again no { gourmet shit } ducking hell"
         
-        let fileText = FileText(end: text.utf16.count, value: text)
+        let innerBracketsRange = applyFuncBeingTested(on: text, using: "{", startingAt: 33)
         
-        XCTAssertNil(
-            fileText.innerBrackets(using: "{", startingAt: 33)
-        )
+        XCTAssertNil(innerBracketsRange)
     }
     
     func test_that_if_there_are_two_matched_brackets_and_the_caret_is_between_them_then_it_can_find_the_text() {
         let text = "ahhhhhh finally the { gourmet shit } motherfuckers"
         
-        let fileText = FileText(end: text.utf16.count, value: text)
-        guard let innerBracketsRange = fileText.innerBrackets(using: "{", startingAt: 24) else { return XCTFail() }
+        let innerBracketsRange = applyFuncBeingTested(on: text, using: "{", startingAt: 24)
         
-        XCTAssertEqual(innerBracketsRange.lowerBound, 20)
-        XCTAssertEqual(innerBracketsRange.upperBound, 35)
+        XCTAssertEqual(innerBracketsRange?.lowerBound, 21)
+        XCTAssertEqual(innerBracketsRange?.upperBound, 35)
     }
     
     func test_extra_cautiously_that_it_matches_the_right_pair_when_there_are_more_than_two_brackets() {
         let text = "hmm ok so we ( gonna try again ( some more and ) see i ) guess"
         
-        let fileText = FileText(end: text.utf16.count, value: text)
-        guard let innerBracketsRange = fileText.innerBrackets(using: "(", startingAt: 51) else { return XCTFail() }
+        let innerBracketsRange = applyFuncBeingTested(on: text, using: "(", startingAt: 51)
         
-        XCTAssertEqual(innerBracketsRange.lowerBound, 13)
-        XCTAssertEqual(innerBracketsRange.upperBound, 55)
+        XCTAssertEqual(innerBracketsRange?.lowerBound, 14)
+        XCTAssertEqual(innerBracketsRange?.upperBound, 55)
     }
     
     func test_that_if_there_are_several_pairs_of_bracket_and_the_caret_location_is_on_an_opening_bracket_on_the_inside_pair_it_can_find_the_correct_range() {
         let text = "yeah ok so ( that fails ( somehow ) that is bad! )"
         
-        let fileText = FileText(end: text.utf16.count, value: text)
-        guard let innerBracketsRange = fileText.innerBrackets(using: "(", startingAt: 24) else { return XCTFail() }
+        let innerBracketsRange = applyFuncBeingTested(on: text, using: "(", startingAt: 24)
         
-        XCTAssertEqual(innerBracketsRange.lowerBound, 24)
-        XCTAssertEqual(innerBracketsRange.upperBound, 34)
+        XCTAssertEqual(innerBracketsRange?.lowerBound, 25)
+        XCTAssertEqual(innerBracketsRange?.upperBound, 34)
     }
     
     func test_that_if_there_are_several_pairs_of_bracket_and_the_caret_location_is_on_a_closing_bracket_on_the_inside_pair_it_can_find_the_correct_range() {
         let text = "yeah ok so ( that fails ( somehow ) that is bad! )"
         
-        let fileText = FileText(end: text.utf16.count, value: text)
-        guard let innerBracketsRange = fileText.innerBrackets(using: "(", startingAt: 34) else { return XCTFail() }
+        let innerBracketsRange = applyFuncBeingTested(on: text, using: "(", startingAt: 34)
         
-        XCTAssertEqual(innerBracketsRange.lowerBound, 24)
-        XCTAssertEqual(innerBracketsRange.upperBound, 34)
+        XCTAssertEqual(innerBracketsRange?.lowerBound, 25)
+        XCTAssertEqual(innerBracketsRange?.upperBound, 34)
     }
    
 }
@@ -105,11 +101,10 @@ var array = [
 ]
 """
         
-        let fileText = FileText(end: text.utf16.count, value: text)
-        guard let innerBracketsRange = fileText.innerBrackets(using: "[", startingAt: 27) else { return XCTFail() }
+        let innerBracketsRange = applyFuncBeingTested(on: text, using: "[", startingAt: 27)
         
-        XCTAssertEqual(innerBracketsRange.lowerBound, 12)
-        XCTAssertEqual(innerBracketsRange.upperBound, 40)
+        XCTAssertEqual(innerBracketsRange?.lowerBound, 13)
+        XCTAssertEqual(innerBracketsRange?.upperBound, 40)
     }
     
     // currently we don't skip brackets within quoted strings, but later we might have too.
@@ -123,11 +118,10 @@ func something() {
 }
 """
         
-        let fileText = FileText(end: text.utf16.count, value: text)
-        guard let innerBracketsRange = fileText.innerBrackets(using: "{", startingAt: 48) else { return XCTFail() }
+        let innerBracketsRange = applyFuncBeingTested(on: text, using: "{", startingAt: 48)
         
-        XCTAssertEqual(innerBracketsRange.lowerBound, 17)
-        XCTAssertEqual(innerBracketsRange.upperBound, 59)
+        XCTAssertEqual(innerBracketsRange?.lowerBound, 18)
+        XCTAssertEqual(innerBracketsRange?.upperBound, 59)
     }
     
 }
