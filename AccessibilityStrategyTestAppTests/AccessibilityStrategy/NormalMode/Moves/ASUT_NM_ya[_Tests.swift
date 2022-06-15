227da4441910c530f78174b3854349ac733a221c
Guillaume Leclerc <guill.bout@gmail.com>
2022-06-15 18:43:47 +0800

    add ASUT ya[]

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ya[_Tests.swift
@@ -0,0 +1,46 @@
+@testable import AccessibilityStrategy
+import XCTest
+import Common
+
+
+// see `yaB` for blah blah
+class ASUT_NM_yaLeftBracket_Tests: ASUT_NM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement) -> AccessibilityTextElement {
+        var state = VimEngineState()
+        
+        return asNormalMode.yaLeftBracket(on: element, &state)
+    }
+    
+}
+
+
+extension ASUT_NM_yaLeftBracket_Tests {
+
+    func test_that_it_calls_yABlock_with_the_correct_bracket_as_parameter() {
+        let text = "some text that [😂️ has some nice ] braces"
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 42,
+            caretLocation: 26,
+            selectedLength: 1,
+            selectedText: "m",
+            fullyVisibleArea: 0..<42,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 42,
+                number: 1,
+                start: 0,
+                end: 42
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement.caretLocation, 15)  
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+}
