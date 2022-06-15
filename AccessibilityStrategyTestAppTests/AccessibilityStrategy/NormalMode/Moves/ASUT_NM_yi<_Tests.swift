745c3e1699860c4cd6e933cecb8fa16bc51308a4
Guillaume Leclerc <guill.bout@gmail.com>
2022-06-15 15:03:53 +0800

    add UT for yi<

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_yi<_Tests.swift
@@ -0,0 +1,46 @@
+@testable import AccessibilityStrategy
+import XCTest
+import Common
+
+
+// see `yiB` for blah blah
+class ASUT_NM_yiLeftChevron_Tests: ASUT_NM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement) -> AccessibilityTextElement {
+        var state = VimEngineState()
+        
+        return asNormalMode.yiLeftChevron(on: element, &state)
+    }
+    
+}
+
+
+extension ASUT_NM_yiLeftChevron_Tests {
+
+    func test_that_it_calls_yInnerBlock_with_the_correct_bracket_as_parameter() {
+        let text = "some text that <😂️ has some nice > braces"
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 42,
+            caretLocation: 26,
+            selectedLength: 1,
+            selectedText: "m",
+            fullyVisibleArea: 0..<42,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 42,
+                number: 1,
+                start: 0,
+                end: 42
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement.caretLocation, 16)  
+        XCTAssertEqual(returnedElement.selectedLength, 3)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+}
