711e842688aca06c35af1405948739020115f9de
Guillaume Leclerc <guill.bout@gmail.com>
2022-06-16 18:52:09 +0800

    add UT UIT for da[

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_da[_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_da[_Tests.swift
@@ -1,8 +1,50 @@
-//
-//  ASUT_NM_da[_Tests.swift
-//  AccessibilityStrategyTestAppTests
-//
-//  Created by Guillaume Leclerc on 16/06/2022.
-//
+@testable import AccessibilityStrategy
+import XCTest
+import Common
 
-import Foundation
+
+// see daB for blah blah
+class ASNM_daLeftBracket_Tests: ASUT_NM_BaseTests {
+
+    private func applyMove(on element: AccessibilityTextElement) -> AccessibilityTextElement {
+        var state = VimEngineState(appFamily: .auto)
+        
+        return asNormalMode.daLeftBracket(on: element, &state)
+    }
+    
+}
+
+
+// Both
+extension ASNM_daLeftBracket_Tests {
+
+    func test_that_if_no_block_is_found_then_it_does_nothing() {
+        let text = "no fucking block in here"
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 24,
+            caretLocation: 16,
+            selectedLength: 1,
+            selectedText: """
+         
+        """,
+            fullyVisibleArea: 0..<24,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 24,
+                number: 1,
+                start: 0,
+                end: 24
+            )!
+        )
+        
+        let returnedElement = applyMove(on: element)
+        
+        XCTAssertEqual(returnedElement.caretLocation, 16)
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+}
+
