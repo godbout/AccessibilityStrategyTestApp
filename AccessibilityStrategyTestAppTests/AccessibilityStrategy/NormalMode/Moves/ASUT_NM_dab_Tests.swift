ff52ca8aadc2c9a647493f0f39ca99ba5f7cf5da
Guillaume Leclerc <guill.bout@gmail.com>
2022-06-17 18:05:21 +0800

    add UT UIT dab

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_dab_Tests.swift
@@ -0,0 +1,50 @@
+@testable import AccessibilityStrategy
+import XCTest
+import Common
+
+
+// see daB for blah blah
+class ASNM_dab_Tests: ASUT_NM_BaseTests {
+
+    private func applyMove(on element: AccessibilityTextElement) -> AccessibilityTextElement {
+        var state = VimEngineState(appFamily: .auto)
+        
+        return asNormalMode.dab(on: element, &state)
+    }
+    
+}
+
+
+// Both
+extension ASNM_dab_Tests {
+
+    func test_that_if_no_block_is_found_then_it_does_nothing() {
+        let text = "no fucking block in here"
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 24,
+            caretLocation: 16,
+            selectedLength: 1,
+            selectedText: """
+         
+        """,
+            fullyVisibleArea: 0..<24,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 24,
+                number: 1,
+                start: 0,
+                end: 24
+            )!
+        )
+        
+        let returnedElement = applyMove(on: element)
+        
+        XCTAssertEqual(returnedElement.caretLocation, 16)
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+}
+
