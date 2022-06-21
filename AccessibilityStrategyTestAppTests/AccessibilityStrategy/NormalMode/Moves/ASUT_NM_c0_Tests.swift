cdd7b8d17ab62c0833084f9d288e69704f11ba7f
Guillaume Leclerc <guill.bout@gmail.com>
2022-06-21 21:47:19 +0800

    add ASNM UT UIT cg0, dg0, refactor c0 d0 to ccg0 and ddg0

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_c0_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_c0_Tests.swift
@@ -3,150 +3,51 @@ import XCTest
 import Common
 
 
+// c0 calls ccg0. tests are there.
+// here we do one test to make sure that c0 sends the right line parameter to ccg0.
 class ASUT_NM_cZero_Tests: ASUT_NM_BaseTests {
     
     private func applyMoveBeingTested(on element: AccessibilityTextElement) -> AccessibilityTextElement {
         var state = VimEngineState(appFamily: .auto)
         
-        return applyMoveBeingTested(on: element, &state)
-    }
-    
-    private func applyMoveBeingTested(on element: AccessibilityTextElement, _ vimEngineState: inout VimEngineState) -> AccessibilityTextElement {
-        return asNormalMode.cZero(on: element, &vimEngineState) 
+        return asNormalMode.cZero(on: element, &state) 
     }
     
 }
 
 
-// Bip, copy deletion and LYS
 extension ASUT_NM_cZero_Tests {
-    
-    func test_that_if_the_caretLocation_is_at_the_start_of_the_currentFileLine_it_does_not_Bip_and_does_not_change_the_LastYankStyle_and_does_not_copy_anything() {
+
+    func test_that_a_FileLine_and_not_a_ScreenLine_is_sent_as_parameter_to_the_superman_move() {
         let text = """
-hello dear friend
-😂️hat's some text
-and also some more
+so we need to type something longer here
+to make sure that this is using the
+correct type of line heheheheheheh
 """
         let element = AccessibilityTextElement(
             role: .textArea,
             value: text,
-            length: 55,
-            caretLocation: 18,
-            selectedLength: 3,
-            selectedText: "😂️",
-            fullyVisibleArea: 0..<55,
-            currentScreenLine: ScreenLine(
-                fullTextValue: text,
-                fullTextLength: 55,
-                number: 2,
-                start: 18,
-                end: 37
-            )!
-        )
-        
-        copyToClipboard(text: "some fake shit")
-        var state = VimEngineState(lastMoveBipped: true, lastYankStyle: .linewise)
-        _ = applyMoveBeingTested(on: element, &state)
-        
-        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "some fake shit")
-        XCTAssertEqual(state.lastYankStyle, .linewise)
-        XCTAssertFalse(state.lastMoveBipped)
-    }
-    
-    func test_that_in_other_cases_it_also_does_not_Bip_but_it_changes_the_LastYankStyle_to_Characterwise_and_copies_from_the_caretLocation_to_the_start_of_the_currentFileLine() {
-        let text = """
-hello dear friend
-😂️hat's some text
-and also some more
-"""
-        let element = AccessibilityTextElement(
-            role: .textArea,
-            value: text,
-            length: 55,
-            caretLocation: 32,
+            length: 111,
+            caretLocation: 73,
             selectedLength: 1,
-            selectedText: "t",
-            fullyVisibleArea: 0..<55,
+            selectedText: """
+        t
+        """,
+            fullyVisibleArea: 0..<111,
             currentScreenLine: ScreenLine(
                 fullTextValue: text,
-                fullTextLength: 55,
-                number: 2,
-                start: 18,
-                end: 37
-            )!
-        )
-        
-        copyToClipboard(text: "some fake shit")
-        var state = VimEngineState(lastMoveBipped: true, lastYankStyle: .linewise)
-        _ = applyMoveBeingTested(on: element, &state)
-        
-        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "😂️hat's some ")
-        XCTAssertEqual(state.lastYankStyle, .characterwise)
-        XCTAssertFalse(state.lastMoveBipped)
-    }
-       
-}
-
-
-// both
-extension ASUT_NM_cZero_Tests {
-    
-    func test_that_in_normal_setting_it_deletes_up_to_the_start_of_the_currentFileLine() {
-        let text = "so that's a long line in a text field and well it's kindaVim i love it"
-        let element = AccessibilityTextElement(
-            role: .textField,
-            value: text,
-            length: 70,
-            caretLocation: 52,
-            selectedLength: 1,
-            selectedText: "k",
-            fullyVisibleArea: 0..<70,
-            currentScreenLine: ScreenLine(
-                fullTextValue: text,
-                fullTextLength: 70,
-                number: 1,
-                start: 0,
-                end: 70
+                fullTextLength: 111,
+                number: 4,
+                start: 64,
+                end: 77
             )!
         )
         
         let returnedElement = applyMoveBeingTested(on: element)
         
-        XCTAssertEqual(returnedElement.caretLocation, 0)
-        XCTAssertEqual(returnedElement.selectedLength, 52)
+        XCTAssertEqual(returnedElement.caretLocation, 41)
+        XCTAssertEqual(returnedElement.selectedLength, 32)
         XCTAssertEqual(returnedElement.selectedText, "")
     }
-    
-    // this test includes for empty text or empty lines
-    func test_that_if_the_caretLocation_is_at_the_start_of_the_currentFileLine_it_does_not_do_anything() {
-        let text = """
-hello dear friend
-😂️hat's some text
-and also some more
-"""
-        let element = AccessibilityTextElement(
-            role: .textArea,
-            value: text,
-            length: 55,
-            caretLocation: 37,
-            selectedLength: 1,
-            selectedText: "a",
-            fullyVisibleArea: 0..<55,
-            currentScreenLine: ScreenLine(
-                fullTextValue: text,
-                fullTextLength: 55,
-                number: 3,
-                start: 37,
-                end: 55
-            )!
-        )
-        
-        let returnedElement = applyMoveBeingTested(on: element)
-        
-        XCTAssertEqual(returnedElement.caretLocation, 37)
-        XCTAssertEqual(returnedElement.selectedLength, 0)
-        XCTAssertNil(returnedElement.selectedText)
-
-    }
-
+   
 }
--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_dg0_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_dg0_Tests.swift
@@ -4,19 +4,19 @@ import Common
 
 
 // see ddg0 for blah blah
-class ASUI_NM_d0_Tests: ASUI_NM_BaseTests {
+class ASUI_NM_dg0_Tests: ASUI_NM_BaseTests {
 
     private func applyMoveBeingTested() -> AccessibilityTextElement {
         var state = VimEngineState()
         
-        return applyMove { asNormalMode.dZero(on: $0, &state) }
+        return applyMove { asNormalMode.dgZero(on: $0, &state) }
     }
     
 }
 
 
 // test that we pass the right LineType parameter to ddg0
-extension ASUI_NM_d0_Tests {
+extension ASUI_NM_dg0_Tests {
         
     func test_that_a_FileLine_and_not_a_ScreenLine_is_sent_as_parameter_to_the_superman_move() {
         let textInAXFocusedElement = """
@@ -32,10 +32,10 @@ and of course this is in the case there is a linefeed at the end of the line.
                 
         XCTAssertEqual(accessibilityElement.fileText.value, """
 C will now work with file lines and is supposed to delete from the caret ☀️ to before the linefeed
-e there is a linefeed at the end of the line.
+and of course this is in e there is a linefeed at the end of the line.
 """
         )
-        XCTAssertEqual(accessibilityElement.caretLocation, 99)
+        XCTAssertEqual(accessibilityElement.caretLocation, 124)
         XCTAssertEqual(accessibilityElement.selectedLength, 1)
         XCTAssertEqual(accessibilityElement.selectedText, "e")
     }
