357f0cb4c2626dccd1e5edbef63bf42184010452
Guillaume Leclerc <guill.bout@gmail.com>
2022-07-03 14:13:46 +0800

    add UT for new yQuotedString, and updated all related UTs

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Helpers/ASUT_NM_yQuotedString_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Helpers/ASUT_NM_yQuotedString_Tests.swift
@@ -1,8 +1,345 @@
-//
-//  ASUT_NM_yQuotedString_Tests.swift
-//  AccessibilityStrategyTestAppTests
-//
-//  Created by Guillaume Leclerc on 03/07/2022.
-//
+@testable import AccessibilityStrategy
+import XCTest
+import Common
 
-import Foundation
+
+// see cQuotedString for blah blah. same principle.
+class ASUT_NM_yQuotedString_Tests: ASUT_NM_BaseTests {
+    
+    private func applyMove(using quote: QuoteType, on element: AccessibilityTextElement, _ vimEngineState: inout VimEngineState) -> AccessibilityTextElement {
+        asNormalMode.yQuotedString(element.currentFileLine.innerQuotedString, quote, on: element, &vimEngineState)
+    }
+    
+}
+
+
+// Bip, copy deletion and LYS
+extension ASUT_NM_yQuotedString_Tests {
+    
+    func test_that_when_it_finds_the_stuff_it_does_not_Bip_and_sets_the_LastYankStyle_to_Characterwise_and_copies_the_deletion() {
+        let text = """
+again multiline
+again
+and now `hohohohoho`
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 42,
+            caretLocation: 37,
+            selectedLength: 1,
+            selectedText: "h",
+            fullyVisibleArea: 0..<37,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 42,
+                number: 3,
+                start: 22,
+                end: 42
+            )!
+        )
+        
+        var state = VimEngineState(lastMoveBipped: true, lastYankStyle: .linewise)
+        _ = applyMove(using: .backtick, on: element, &state)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "hohohohoho")
+        XCTAssertFalse(state.lastMoveBipped)
+        XCTAssertEqual(state.lastYankStyle, .characterwise)
+    }
+    
+    func test_that_when_it_does_not_find_the_stuff_it_Bips_and_does_not_change_the_LastYankingStyle_and_does_not_copy_anything() {
+        let text = "some text without any double quote"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 34,
+            caretLocation: 23,
+            selectedLength: 1,
+            selectedText: "o",
+            fullyVisibleArea: 0..<34,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 34,
+                number: 1,
+                start: 0,
+                end: 34
+            )!
+        )
+        
+        copyToClipboard(text: "no double quote")
+        var state = VimEngineState(lastMoveBipped: true, lastYankStyle: .linewise)
+        _ = applyMove(using: .doubleQuote, on: element, &state)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "no double quote")
+        XCTAssertTrue(state.lastMoveBipped)
+        XCTAssertEqual(state.lastYankStyle, .linewise)
+    }
+    
+}
+
+
+// Both
+extension ASUT_NM_yQuotedString_Tests {
+    
+    func test_that_there_is_no_quote_it_does_not_move_or_copy_anything_and_does_not_touch_the_LastYankStyle() {
+        let text = "some text without any double quote"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 34,
+            caretLocation: 23,
+            selectedLength: 1,
+            selectedText: "o",
+            fullyVisibleArea: 0..<34,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 34,
+                number: 1,
+                start: 0,
+                end: 34
+            )!
+        )
+        
+        copyToClipboard(text: "no double quote")
+        var state = VimEngineState(lastMoveBipped: false, lastYankStyle: .linewise)
+        let returnedElement = applyMove(using: .doubleQuote, on: element, &state)
+        
+        XCTAssertEqual(state.lastYankStyle, .linewise)
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "no double quote")
+        XCTAssertEqual(returnedElement.caretLocation, 23)
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+    func test_that_there_is_only_one_quote_it_does_not_move_or_copy_anything_either_and_does_not_touch_the_LastYankStyle() {
+        let text = """
+now there's one " double quote
+"""
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 30,
+            caretLocation: 12,
+            selectedLength: 1,
+            selectedText: "o",
+            fullyVisibleArea: 0..<30,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 30,
+                number: 1,
+                start: 0,
+                end: 30
+            )!
+        )
+        
+        copyToClipboard(text: "only one double quote")
+        var state = VimEngineState(lastMoveBipped: false, lastYankStyle: .linewise)
+        let returnedElement = applyMove(using: .doubleQuote, on: element, &state)
+        
+        XCTAssertEqual(state.lastYankStyle, .linewise)
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "only one double quote")
+        XCTAssertEqual(returnedElement.caretLocation, 12)
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+    func test_that_if_there_are_two_quotes_and_the_caret_is_before_them_then_it_moves_the_caret_and_copy_the_text_and_sets_the_LastYankStyle_to_Characterwise() {
+        let text = """
+now there's
+two 'simple quotes' on the second line
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 50,
+            caretLocation: 14,
+            selectedLength: 1,
+            selectedText: "o",
+            fullyVisibleArea: 0..<50,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 50,
+                number: 2,
+                start: 12,
+                end: 50
+            )!
+        )
+        
+        var state = VimEngineState(lastMoveBipped: false, lastYankStyle: .linewise)
+        let returnedElement = applyMove(using: .singleQuote, on: element, &state)
+        
+        XCTAssertEqual(state.lastYankStyle, .characterwise)
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "simple quotes")
+        XCTAssertEqual(returnedElement.caretLocation, 17)  
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+    func test_that_if_there_are_two_quotes_and_the_caret_is_between_them_then_it_moves_the_caret_and_copy_the_text_and_sets_the_LastYankStyle_to_Characterwise() {
+        let text = """
+again multiline
+again
+and now `hohohohoho`
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 42,
+            caretLocation: 37,
+            selectedLength: 1,
+            selectedText: "h",
+            fullyVisibleArea: 0..<42,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 42,
+                number: 3,
+                start: 22,
+                end: 42
+            )!
+        )
+        
+        var state = VimEngineState(lastMoveBipped: false, lastYankStyle: .linewise)
+        let returnedElement = applyMove(using: .backtick, on: element, &state)
+        
+        XCTAssertEqual(state.lastYankStyle, .characterwise)
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "hohohohoho")
+        XCTAssertEqual(returnedElement.caretLocation, 31)  
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+    func test_that_if_there_are_two_quotes_and_the_caret_is_after_them_then_it_does_not_move_or_copy_anything_and_does_not_touch_the_LastYankStyle() {
+        let text = """
+double "quotes" before the caret
+"""
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 32,
+            caretLocation: 26,
+            selectedLength: 1,
+            selectedText: " ",
+            fullyVisibleArea: 0..<32,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 32,
+                number: 1,
+                start: 0,
+                end: 32
+            )!
+        )
+        
+        copyToClipboard(text: "caret after double quote")
+        var state = VimEngineState(lastMoveBipped: false, lastYankStyle: .linewise)
+        let returnedElement = applyMove(using: .doubleQuote, on: element, &state)
+        
+        XCTAssertEqual(state.lastYankStyle, .linewise)
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "caret after double quote")
+        XCTAssertEqual(returnedElement.caretLocation, 26)  
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+    func test_that_if_there_are_three_double_quotes_and_the_caret_is_not_after_all_of_them_then_it_moves_the_caret_and_copy_the_right_text_and_sets_the_LastYankStyle_to_Characterwise() {
+        let text = """
+heheheheh
+'quote' and some more' yeyeyeye
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 41,
+            caretLocation: 30,
+            selectedLength: 1,
+            selectedText: "e",
+            fullyVisibleArea: 0..<41,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 41,
+                number: 2,
+                start: 10,
+                end: 41
+            )!
+        )
+        
+        var state = VimEngineState(lastMoveBipped: false, lastYankStyle: .linewise)
+        let returnedElement = applyMove(using: .singleQuote, on: element, &state)
+        
+        XCTAssertEqual(state.lastYankStyle, .characterwise)
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), " and some more")
+        XCTAssertEqual(returnedElement.caretLocation, 17)  
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+    func test_that_if_there_are_four_quotes_and_the_caret_is_exactly_on_the_third_one_it_calculates_the_matching_pairs_and_copy_the_right_text_and_sets_the_LastYankStyle_to_Characterwise() {
+        let text = """
+now there's gonna
+`be` for `quotes` yep
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 39,
+            caretLocation: 27,
+            selectedLength: 1,
+            selectedText: "`",
+            fullyVisibleArea: 0..<39,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 39,
+                number: 2,
+                start: 18,
+                end: 39
+            )!
+        )
+        
+        var state = VimEngineState(lastMoveBipped: false, lastYankStyle: .linewise)
+        let returnedElement = applyMove(using: .backtick, on: element, &state)
+        
+        XCTAssertEqual(state.lastYankStyle, .characterwise)
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "quotes")
+        XCTAssertEqual(returnedElement.caretLocation, 28)  
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+}
+
+
+// emojis
+extension ASUT_NM_yQuotedString_Tests {
+    
+    func test_that_it_handles_emojis() {
+        let text = """
+need to deal with
+those💨️💨️💨️ fac"🍵️s 🥺️☹️😂️ h😀️ha👅️" hhohohoo🤣️
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 73,
+            caretLocation: 20,
+            selectedLength: 1,
+            selectedText: "o",
+            fullyVisibleArea: 0..<73,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 73,
+                number: 2,
+                start: 18,
+                end: 52
+            )!
+        )
+        
+        var state = VimEngineState(lastMoveBipped: false, lastYankStyle: .linewise)
+        let returnedElement = applyMove(using: .doubleQuote, on: element, &state)
+        
+        XCTAssertEqual(state.lastYankStyle, .characterwise)
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "🍵️s 🥺️☹️😂️ h😀️ha👅️")
+        XCTAssertEqual(returnedElement.caretLocation, 37)  
+        XCTAssertEqual(returnedElement.selectedLength, 3)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+}
--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_yi"_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_yi"_Tests.swift
@@ -1,6 +1,52 @@
 @testable import AccessibilityStrategy
 import XCTest
+import Common
 
 
-// just calling yInnerQuotedString. all the tests are there.
-class ASUT_NM_yiDoubleQuote_Tests: ASUT_NM_BaseTests {}
+// see ya' for blah blah
+class ASUT_NM_yiDoubleQuote_Tests: ASUT_NM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement, _ vimEngineState: inout VimEngineState) -> AccessibilityTextElement {
+        asNormalMode.yiDoubleQuote(on: element, &vimEngineState)
+    }
+    
+}
+
+
+extension ASUT_NM_yiDoubleQuote_Tests {
+    
+    func test_that_we_pass_the_right_function_and_the_right_quote_to_the_helper_function() {
+        let text = """
+ok so time to add
+some new ("shit in there") right?
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 51,
+            caretLocation: 23,
+            selectedLength: 1,
+            selectedText: """
+        n
+        """,
+            fullyVisibleArea: 0..<51,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 51,
+                number: 2,
+                start: 18,
+                end: 51
+            )!
+        )
+        
+        var state = VimEngineState(lastMoveBipped: false, lastYankStyle: .linewise)
+        let returnedElement = applyMoveBeingTested(on: element, &state)
+        
+        XCTAssertEqual(state.lastYankStyle, .characterwise)
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "shit in there")
+        XCTAssertEqual(returnedElement.caretLocation, 29)  
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+}
