e89ffc0611839d0e0b351afdf5ea137a6fe3f92d
Guillaume Leclerc <guill.bout@gmail.com>
2022-07-03 14:13:46 +0800

    add UT for new yQuotedString, and updated all related UTs

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ya"_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ya"_Tests.swift
@@ -1,8 +1,52 @@
-//
-//  ASUT_NM_ya"_Tests.swift
-//  AccessibilityStrategyTestAppTests
-//
-//  Created by Guillaume Leclerc on 03/07/2022.
-//
+@testable import AccessibilityStrategy
+import XCTest
+import Common
 
-import Foundation
+
+// see ya' for blah blah
+class ASUT_NM_yaDoubleQuote_Tests: ASUT_NM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement, _ vimEngineState: inout VimEngineState) -> AccessibilityTextElement {
+        asNormalMode.yaDoubleQuote(on: element, &vimEngineState)
+    }
+    
+}
+
+
+extension ASUT_NM_yaDoubleQuote_Tests {
+    
+    func test_that_we_pass_the_right_function_and_the_right_quote_to_the_helper_function() {
+        let text = """
+ok so time to add
+some new ("shit in there") right?
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 51,
+            caretLocation: 23,
+            selectedLength: 1,
+            selectedText: """
+        n
+        """,
+            fullyVisibleArea: 0..<51,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 51,
+                number: 2,
+                start: 18,
+                end: 51
+            )!
+        )
+        
+        var state = VimEngineState(lastMoveBipped: false, lastYankStyle: .linewise)
+        let returnedElement = applyMoveBeingTested(on: element, &state)
+        
+        XCTAssertEqual(state.lastYankStyle, .characterwise)
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "\"shit in there\"")
+        XCTAssertEqual(returnedElement.caretLocation, 28)  
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+}
