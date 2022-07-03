9f1629fbb0b5d96c4c4503ea53123ab56703ca76
Guillaume Leclerc <guill.bout@gmail.com>
2022-07-03 14:12:28 +0800

    add UT for new cQuotedString

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Helpers/ASUI_NM_cQuotedString_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Helpers/ASUI_NM_cQuotedString_Tests.swift
@@ -1,8 +1,39 @@
-//
-//  ASUI_NM_cQuotedString_Tests.swift
-//  AccessibilityStrategyTestAppTests
-//
-//  Created by Guillaume Leclerc on 03/07/2022.
-//
+@testable import AccessibilityStrategy
+import XCTest
+import Common
 
-import Foundation
+
+class ASUI_NM_cQuotedString_Tests: ASUI_NM_BaseTests {
+    
+    private func applyMoveBeingTested(using quote: QuoteType, appFamily: AppFamily) -> AccessibilityTextElement {
+        var state = VimEngineState(appFamily: appFamily)
+        
+        return applyMove { asNormalMode.cQuotedString($0.currentFileLine.innerQuotedString, quote, on: $0, &state) }
+    }
+    
+}
+
+
+// PGR and Electron
+extension ASUI_NM_cQuotedString_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = """
+finally dealing with the "real stuff"!
+"""
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.F(to: "l", on: $0) }
+        let accessibilityElement = applyMoveBeingTested(using: .doubleQuote, appFamily: .pgR)
+               
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+finally dealing with the "!
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 25)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement.selectedText, "")
+    }
+    
+}
