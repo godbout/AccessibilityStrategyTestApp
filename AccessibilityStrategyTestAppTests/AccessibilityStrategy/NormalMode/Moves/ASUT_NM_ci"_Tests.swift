246b6ac91da921ed5862a0003ee2f1d6f331e322
Guillaume Leclerc <guill.bout@gmail.com>
2022-07-03 14:11:57 +0800

    add parameters tests to ci"'` after the refactor

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ci"_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_ci"_Tests.swift
@@ -1,6 +1,51 @@
 @testable import AccessibilityStrategy
 import XCTest
+import Common
 
 
-// see ci'
-class ASNM_ciDoubleQuote_Tests: ASUT_NM_BaseTests {}
+// see ca' for blah blah
+class ASNM_ciDoubleQuote_Tests: ASUT_NM_BaseTests {
+
+    private func applyMoveBeingTested(on element: AccessibilityTextElement) -> AccessibilityTextElement {
+        var state = VimEngineState(appFamily: .auto)
+        
+        return asNormalMode.ciDoubleQuote(on: element, &state)
+    }
+    
+}
+
+
+extension ASNM_ciDoubleQuote_Tests {
+    
+    func test_that_we_pass_the_right_function_and_the_right_quote_to_the_helper_function() {
+        let text = """
+deliberately multiline
+because "it gonna" crash 😅️
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 51,
+            caretLocation: 28,
+            selectedLength: 1,
+            selectedText: """
+        s
+        """,
+            fullyVisibleArea: 0..<51,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 51,
+                number: 2,
+                start: 23,
+                end: 51
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement.caretLocation, 32)
+        XCTAssertEqual(returnedElement.selectedLength, 8)
+        XCTAssertEqual(returnedElement.selectedText, "")
+    }
+    
+}
