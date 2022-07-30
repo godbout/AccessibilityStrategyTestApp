534c2c277653bba9b33107be5b74e47fe62cf551
G <guill.bout@gmail.com>
2022-07-31 02:09:52 +0800

    --wip-- [skip ci]

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_beginningOfSentenceBackward_Tests/FT_beginningOfSentenceBackward_OnEmptyOrBlankLines_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_beginningOfSentenceBackward_Tests/FT_beginningOfSentenceBackward_OnEmptyOrBlankLines_Tests.swift
@@ -2,6 +2,8 @@
 import XCTest
 
 
+// TODO: HEHE
+// see Backward for blah blah
 class FT_beginningOfSentenceBackward_OnEmptyOrBlankLines_Tests: XCTestCase {
     
     private func applyFuncBeingTested(on text: String, startingAt caretLocation: Int) -> Int {
@@ -13,12 +15,39 @@ class FT_beginningOfSentenceBackward_OnEmptyOrBlankLines_Tests: XCTestCase {
 }
 
 
+// The 3 Cases:
+// - empty TextElement
+// - 2nd case is now gone!
+// - caret at the end of TextElement on own empty line
+extension FT_beginningOfSentenceBackward_OnEmptyOrBlankLines_Tests {
+    
+    func test_that_if_the_text_is_empty_then_it_returns_0() {
+        let text = ""
+        
+        let beginningOfSentenceBackwardLocation = applyFuncBeingTested(on: text, startingAt: 0)
+        
+        XCTAssertEqual(beginningOfSentenceBackwardLocation, 0)
+    }
+    
+    func test_that_if_the_caret_is_on_the_last_EmptyLine_it_goes_to_the_beginning_of_the_sentence() {
+        let text = """
+this move doesn't bip
+but of course at the last empty line
+it will. not move
+
+"""
+        let beginningOfSentenceBackwardLocation = applyFuncBeingTested(on: text, startingAt: 77)
+        
+        XCTAssertEqual(beginningOfSentenceBackwardLocation, 68)
+    }
+    
+}
+
+
 // TextViews
 extension FT_beginningOfSentenceBackward_OnEmptyOrBlankLines_Tests {
     
-    // TODO: this one
-    // TODO: plus stuck when already at the beginning of sentence
-    func test_that_paragraph_boundaries_are_also_sentence_boundaries() {
+    func test_that_it_does_not_get_stuck_in_the_middle_of_multiple_emptyLines() {
         let text = """
 so it's not gonna skip lines but stop
 at paragraph boundaries
@@ -29,9 +58,24 @@ at paragraph boundaries
 
 can check the impl of that
 """
-        let beginningOfSentenceForwardLocation = applyFuncBeingTested(on: text, startingAt: 84)
+        let beginningOfSentenceBackwardLocation = applyFuncBeingTested(on: text, startingAt: 62)
         
-        XCTAssertEqual(beginningOfSentenceForwardLocation, 62)
+        XCTAssertEqual(beginningOfSentenceBackwardLocation, 0)
+    }
+    
+    func test_that_if_the_caret_is_on_an_emptyLine_and_the_text_starts_by_multiple_emptyLines_then_it_goes_to_the_start() {
+        let text = """
+
+
+
+
+
+plenty of empty lines
+above
+"""
+        let beginningOfSentenceBackwardLocation = applyFuncBeingTested(on: text, startingAt: 3)
+        
+        XCTAssertEqual(beginningOfSentenceBackwardLocation, 0)
     }
     
 }
