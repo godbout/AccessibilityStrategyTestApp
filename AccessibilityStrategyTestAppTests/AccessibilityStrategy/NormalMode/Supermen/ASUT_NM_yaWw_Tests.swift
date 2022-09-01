8c75656e12be1652ac584974d13742d6f9744b29
G <guill.bout@gmail.com>
2022-09-01 22:04:11 +0800

    add UT for ASNM yaW yaw

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Supermen/ASUT_NM_yaWw_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Supermen/ASUT_NM_yaWw_Tests.swift
@@ -1,8 +1,151 @@
-//
-//  ASUT_NM_yaWw_Tests.swift
-//  AccessibilityStrategyTestAppTests
-//
-//  Created by Guillaume Leclerc on 01/09/2022.
-//
+@testable import AccessibilityStrategy
+import XCTest
+import Common
 
-import Foundation
+
+// see ciWw (yes ciWw) for blah blah
+class ASUT_NM_yaWw_Tests: ASUT_NM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement) -> AccessibilityTextElement {
+        var state = VimEngineState(appFamily: .auto)
+        
+        return applyMoveBeingTested(on: element, &state)
+    }
+        
+    private func applyMoveBeingTested(on element: AccessibilityTextElement, _ vimEngineState: inout VimEngineState) -> AccessibilityTextElement {
+        return asNormalMode.yaWw(on: element, using: element.fileText.aWord, &vimEngineState)
+    }
+    
+}
+
+
+// Bip, copy deletion and LYS
+extension ASUT_NM_yaWw_Tests {
+    
+    func test_that_when_it_finds_the_stuff_it_does_not_Bip_and_sets_the_LastYankStyle_to_Characterwise_and_copies_the_deletion() {
+        let text = "that's some cute      text in here don't you think?"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 51,
+            caretLocation: 14,
+            selectedLength: 1,
+            selectedText: "t",
+            fullyVisibleArea: 0..<51,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 51,
+                number: 1,
+                start: 0,
+                end: 51
+            )!
+        )
+        
+        copyToClipboard(text: "some fake shit")
+        var state = VimEngineState(lastMoveBipped: true, lastYankStyle: .linewise)
+        _ = applyMoveBeingTested(on: element, &state)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "cute      ")
+        XCTAssertFalse(state.lastMoveBipped)
+        XCTAssertEqual(state.lastYankStyle, .characterwise)
+    }
+        
+    func test_that_when_it_does_not_find_the_stuff_it_Bips_and_does_not_change_the_LastYankingStyle_and_does_not_copy_anything() {
+        let text = """
+some text
+and also a lot of spaces at the end of this line        
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 66,
+            caretLocation: 60,
+            selectedLength: 1,
+            selectedText: " ",
+            fullyVisibleArea: 0..<66,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 66,
+                number: 2,
+                start: 10,
+                end: 66
+            )!
+        )
+        
+        copyToClipboard(text: "some fake shit")
+        var state = VimEngineState(lastMoveBipped: false, lastYankStyle: .linewise)
+        _ = applyMoveBeingTested(on: element, &state)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "some fake shit")
+        XCTAssertTrue(state.lastMoveBipped)
+        XCTAssertEqual(state.lastYankStyle, .linewise)
+    }
+       
+}
+
+
+// Both
+extension ASUT_NM_yaWw_Tests {
+    
+    func test_that_when_it_finds_a_word_it_copies_the_correct_range_and_repositions_the_caret_properly() {
+        let text = "that's some cute      text in here don't you think?"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 51,
+            caretLocation: 14,
+            selectedLength: 1,
+            selectedText: "t",
+            fullyVisibleArea: 0..<51,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 51,
+                number: 1,
+                start: 0,
+                end: 51
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "cute      ")
+        XCTAssertEqual(returnedElement.caretLocation, 12)
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+
+    func test_that_when_it_cannot_find_a_word_the_caret_goes_to_the_end_limit_of_the_text() {
+        let text = """
+some text
+and also a lot of spaces at the end of this line        
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 66,
+            caretLocation: 60,
+            selectedLength: 1,
+            selectedText: " ",
+            fullyVisibleArea: 0..<66,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 66,
+                number: 2,
+                start: 10,
+                end: 66
+            )!
+        )
+        
+        copyToClipboard(text: "some fake shit")
+        var state = VimEngineState(lastMoveBipped: false, lastYankStyle: .linewise)
+        let returnedElement = applyMoveBeingTested(on: element, &state)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "some fake shit")
+        XCTAssertTrue(state.lastMoveBipped)
+        XCTAssertEqual(state.lastYankStyle, .linewise)
+        XCTAssertEqual(returnedElement.caretLocation, 60)
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+   }
+    
+}
