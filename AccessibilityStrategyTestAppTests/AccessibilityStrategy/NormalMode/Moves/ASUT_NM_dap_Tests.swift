f76e60250d6f64d81c3a80090b4dc19e5fcf1a78
G <guill.bout@gmail.com>
2022-09-01 18:13:09 +0800

    add ASUT ASUI dap

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_dap_Tests.swift
@@ -0,0 +1,6 @@
+@testable import AccessibilityStrategy
+import XCTest
+
+
+// in UIT. dap is not calling cap.
+class ASUT_NM_dap_Tests: ASUT_NM_BaseTests {}
---
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_dap_Tests.swift
@@ -0,0 +1,145 @@
+import XCTest
+@testable import AccessibilityStrategy
+import Common
+
+
+// careful. dip is special in the sense that blank lines are paragraph boundaries, which is not the case with {}
+class ASUI_NM_dap_Tests: ASUI_NM_BaseTests {
+    
+    private func applyMoveBeingTested(_ vimEngineState: inout VimEngineState) -> AccessibilityTextElement {
+        return applyMove { asNormalMode.dap(on: $0, &vimEngineState) }
+    }
+    
+    private func applyMoveBeingTested(appFamily: AppFamily = .auto) -> AccessibilityTextElement {
+        var state = VimEngineState(appFamily: appFamily)
+        
+        return applyMoveBeingTested(&state)
+    }
+    
+}
+
+
+// Bip, copy deletion and LYS
+extension ASUI_NM_dap_Tests {
+    
+    func test_that_if_there_is_no_aParagraph_found_it_Bips_and_does_not_copy_anything_and_does_not_change_the_LastYankStyle_to_Linewise() {
+        let textInAXFocusedElement = """
+like this will Bip
+
+
+
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.b(times: 2, on: $0) }
+        
+        copyToClipboard(text: "some fake shit")
+        var state = VimEngineState(lastMoveBipped: true, lastYankStyle: .characterwise)
+        _ = applyMoveBeingTested(&state)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "some fake shit")
+        XCTAssertEqual(state.lastYankStyle, .characterwise)
+        XCTAssertTrue(state.lastMoveBipped)
+    }
+    
+    func test_that_if_there_is_a_aParagraph_found_it_does_not_Bip_and_it_copies_the_deletion_and_it_sets_the_LastYankStyle_to_Linewise() {
+        let textInAXFocusedElement = """
+this will not
+
+
+
+Bip
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.zero(on: $0) }
+        
+        copyToClipboard(text: "some fake shit")
+        var state = VimEngineState(lastMoveBipped: true, lastYankStyle: .characterwise)
+        _ = applyMoveBeingTested(&state)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), """
+
+
+
+Bip
+"""
+        )
+        XCTAssertEqual(state.lastYankStyle, .linewise)
+        XCTAssertFalse(state.lastMoveBipped)
+    }
+    
+}
+
+
+extension ASUI_NM_dap_Tests {
+    
+    func test_that_the_block_cursor_ends_up_at_the_right_place() {
+        let textInAXFocusedElement = """
+this is to check
+that
+
+the block cursor
+ends up in the
+right
+
+place
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.h(on: $0) }
+        applyMove { asNormalMode.k(on: $0) }
+        applyMove { asNormalMode.b(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+this is to check
+that
+
+place
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 23)
+        XCTAssertEqual(accessibilityElement.selectedLength, 1)
+    }
+
+}
+
+
+// PGR and Electron
+extension ASUI_NM_dap_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = """
+this is to check
+that
+
+the block cursor
+ends up in the
+right
+
+place
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.h(on: $0) }
+        applyMove { asNormalMode.k(on: $0) }
+        applyMove { asNormalMode.b(on: $0) }
+        let accessibilityElement = applyMoveBeingTested(appFamily: .pgR)
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+this is to check
+that
+place
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 22)
+        XCTAssertEqual(accessibilityElement.selectedLength, 1)
+    }
+    
+}
