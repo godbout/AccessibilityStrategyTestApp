8c192a9b59e9f8596ff49add56b76c129fe22e71
G <guill.bout@gmail.com>
2022-09-01 17:48:40 +0800

    add ASUI NM cap

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cap_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cap_Tests.swift
@@ -1,8 +1,49 @@
-//
-//  ASUI_NM_cap_Tests.swift
-//  AccessibilityStrategyNormaModeUITests
-//
-//  Created by Guillaume Leclerc on 01/09/2022.
-//
+@testable import AccessibilityStrategy
+import XCTest
+import Common
 
-import Foundation
+
+class ASUI_NM_cap_Tests: ASUI_NM_BaseTests {
+    
+    private func applyMoveBeingTested(appFamily: AppFamily) -> AccessibilityTextElement {
+        var state = VimEngineState(appFamily: appFamily) 
+        
+        return applyMove { asNormalMode.cap(on: $0, &state) }
+    }
+    
+}
+
+
+// PGR and Electron
+extension ASUI_NM_cap_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_mode_it_tricks_the_system_and_eventually_modifies_text() {
+        let textInAXFocusedElement = """
+this is some text
+
+and cip
+so it should delete
+one more
+
+shit in PGR
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.l(on: $0) }
+        applyMove { asNormalMode.k(on: $0) }
+        applyMove { asNormalMode.k(on: $0) }
+        let accessibilityElement = applyMoveBeingTested(appFamily: .pgR)
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+this is some text
+
+shit in PGR
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 18)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement.selectedText, "")
+    }
+    
+}
