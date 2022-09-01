4064a26358d6da9fa3b4ad7dd6186e8b56eb4b44
G <guill.bout@gmail.com>
2022-09-01 22:04:11 +0800

    add UT for ASNM yaW yaw

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_yaW__Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_yaW__Tests.swift
@@ -1,8 +1,52 @@
-//
-//  ASUT_NM_yaW__Tests.swift
-//  AccessibilityStrategyTestAppTests
-//
-//  Created by Guillaume Leclerc on 01/09/2022.
-//
+@testable import AccessibilityStrategy
+import XCTest
+import Common
 
-import Foundation
+
+// see see yaWw
+class ASUT_NM_yaW__Tests: ASUT_NM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement) -> AccessibilityTextElement {
+        var state = VimEngineState(appFamily: .auto)
+        
+        return asNormalMode.yaW(on: element, &state)
+    }
+    
+}
+
+
+extension ASUT_NM_yaW__Tests {
+    
+    func test_that_we_pass_the_right_function_and_the_right_quote_to_the_helper_function() {
+        let text = """
+testing that we're sending
+the right-parameter to the funcs!
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 60,
+            caretLocation: 33,
+            selectedLength: 1,
+            selectedText: """
+        g
+        """,
+            fullyVisibleArea: 0..<60,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 60,
+                number: 2,
+                start: 27,
+                end: 60
+            )!
+        )
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "right-parameter ")
+        XCTAssertEqual(returnedElement.caretLocation, 31)
+        XCTAssertEqual(returnedElement.selectedLength, 1)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
+}
