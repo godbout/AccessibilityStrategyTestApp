f130af8cd1ea28391b7b21489ec42c5f3c6948a3
G <guill.bout@gmail.com>
2022-09-11 22:29:32 +0800

    add ASUT for VML )

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Linewise/ASUT_VML_)_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Linewise/ASUT_VML_)_Tests.swift
@@ -14,36 +14,229 @@ class ASUT_VML_rightParenthesis_Tests: ASUT_VM_BaseTests {
 }
 
 
-// nope
 extension ASUT_VML_rightParenthesis_Tests {
+    
+    func test_that_when_the_lineAtAnchor_is_before_the_lineAtHead_and_the_newLineAtHead_ends_after_the_lineAtHead_then_it_selects_from_the_start_of_the_lineAtAnchor_to_the_end_of_newLineAtHead() {
+        let text = """
+now we gonna
+type quite some lines
+because hehe
+that's gonna be
+a not
 
-    func test_that_this_move_does_not_exist_for_VisualModeStyle_Linewise() {
-        let text = "       that's some nice text in here yehe"
+so easy one
+hehe
+but actually easy yes
+"""
         let element = AccessibilityTextElement(
-            role: .textField,
+            role: .textArea,
             value: text,
-            length: 41,
-            caretLocation: 17,
-            selectedLength: 15,
-            selectedText: "e nice text in ",
-            fullyVisibleArea: 0..<41,
+            length: 109,
+            caretLocation: 13,
+            selectedLength: 35,
+            selectedText: """
+        type quite some lines
+        because hehe
+
+        """,
+            fullyVisibleArea: 0..<109,
             currentScreenLine: ScreenLine(
                 fullTextValue: text,
-                fullTextLength: 41,
-                number: 1,
-                start: 0,
-                end: 41
+                fullTextLength: 109,
+                number: 2,
+                start: 13,
+                end: 35
             )!
         )
         
-        AccessibilityStrategyVisualMode.anchor = 31
-        AccessibilityStrategyVisualMode.head = 17
+        AccessibilityStrategyVisualMode.anchor = 13
+        AccessibilityStrategyVisualMode.head = 47
         
         let returnedElement = applyMoveBeingTested(on: element)
 
-        XCTAssertEqual(returnedElement.caretLocation, 17)
-        XCTAssertEqual(returnedElement.selectedLength, 15)
-        XCTAssertNil(returnedElement.selectedText)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.anchor, 13)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.head, 70)
+        XCTAssertEqual(returnedElement.caretLocation, 13)
+        XCTAssertEqual(returnedElement.selectedLength, 58)
+    }
+    
+    func test_that_when_the_lineAtAnchor_is_after_the_lineAtHead_and_the_newLineAtHead_ends_before_the_lineAtAnchor_then_it_selects_from_the_start_of_the_newLineAtHead_to_the_end_of_lineAtAnchor() {
+        let text = """
+now we gonna
+type quite some lines
+because hehe
+that's gonna be
+a not
+
+so easy one
+hehe
+but actually easy yes
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 109,
+            caretLocation: 13,
+            selectedLength: 96,
+            selectedText: """
+        type quite some lines
+        because hehe
+        that's gonna be
+        a not
+
+        so easy one
+        hehe
+        but actually easy yes
+        """,
+            fullyVisibleArea: 0..<109,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 109,
+                number: 2,
+                start: 13,
+                end: 35
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 108
+        AccessibilityStrategyVisualMode.head = 13
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(AccessibilityStrategyVisualMode.anchor, 108)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.head, 70)
+        XCTAssertEqual(returnedElement.caretLocation, 70)
+        XCTAssertEqual(returnedElement.selectedLength, 39)
+    }
+
+    func test_that_when_the_lineAtAnchor_is_after_the_lineAtHead_and_the_newLineAtHead_ends_after_the_lineAtAnchor_then_it_selects_from_the_start_of_the_lineAtAnchor_to_the_end_of_newLineAtHead() {
+        let text = """
+now we gonna
+type quite some lines
+because hehe
+that's gonna be
+a not
+
+so easy one
+hehe
+but actually easy yes
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 109,
+            caretLocation: 13,
+            selectedLength: 35,
+            selectedText: """
+        type quite some lines
+        because hehe
+
+        """,
+            fullyVisibleArea: 0..<109,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 109,
+                number: 2,
+                start: 13,
+                end: 35
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 47
+        AccessibilityStrategyVisualMode.head = 13
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(AccessibilityStrategyVisualMode.anchor, 35)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.head, 70)
+        XCTAssertEqual(returnedElement.caretLocation, 35)
+        XCTAssertEqual(returnedElement.selectedLength, 36)
+    }
+    
+    func test_that_when_the_lineAtAnchor_is_the_same_as_the_lineAtHead_and_the_Anchor_is_before_the_Head_then_it_selects_from_the_start_of_the_lineAtAnchorSlashHead_to_the_end_of_newLineAtHead() {
+        let text = """
+now we gonna
+type quite some lines
+because hehe
+that's gonna be
+a not
+
+so easy one
+hehe
+but actually easy yes
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 109,
+            caretLocation: 48,
+            selectedLength: 16,
+            selectedText: """
+        that's gonna be
+
+        """,
+            fullyVisibleArea: 0..<109,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 109,
+                number: 4,
+                start: 48,
+                end: 64
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 48
+        AccessibilityStrategyVisualMode.head = 63
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(AccessibilityStrategyVisualMode.anchor, 48)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.head, 70)
+        XCTAssertEqual(returnedElement.caretLocation, 48)
+        XCTAssertEqual(returnedElement.selectedLength, 23)
+    }
+    
+    func test_that_when_the_lineAtAnchor_is_the_same_as_the_lineAtHead_and_the_Head_is_before_the_Anchor_then_it_selects_from_the_start_of_the_lineAtAnchorSlashHead_to_the_end_of_newLineAtHead() {
+        let text = """
+now we gonna
+type quite some lines
+because hehe
+that's gonna be
+a not
+
+so easy one
+hehe
+but actually easy yes
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 109,
+            caretLocation: 48,
+            selectedLength: 16,
+            selectedText: """
+        that's gonna be
+
+        """,
+            fullyVisibleArea: 0..<109,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 109,
+                number: 4,
+                start: 48,
+                end: 64
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 63
+        AccessibilityStrategyVisualMode.head = 48
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(AccessibilityStrategyVisualMode.anchor, 48)
+        XCTAssertEqual(AccessibilityStrategyVisualMode.head, 70)
+        XCTAssertEqual(returnedElement.caretLocation, 48)
+        XCTAssertEqual(returnedElement.selectedLength, 23)
     }
 
 }
