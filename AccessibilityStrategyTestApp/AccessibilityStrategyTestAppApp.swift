ebdb55fefba43b8b600fe4565ed2541b2939e6bd
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2023-05-30 00:59:05 +0800

    make the width of the test window correct again for XCode 14 and Ventura

--- AccessibilityStrategyTestApp/AccessibilityStrategyTestAppApp.swift
+++ AccessibilityStrategyTestApp/AccessibilityStrategyTestAppApp.swift
@@ -1,6 +1,17 @@
 import SwiftUI
 
 
+extension Scene {
+    func windowResizabilityContentSize() -> some Scene {
+        if #available(macOS 13.0, *) {
+            return windowResizability(.contentSize)
+        } else {
+            return self
+        }
+    }
+}
+
+
 @main
 struct AccessibilityStrategyTestAppApp: App {
 
@@ -8,6 +19,8 @@ struct AccessibilityStrategyTestAppApp: App {
         WindowGroup {
             UITestsView()
         }
+        .windowResizabilityContentSize()
     }
     
 }
+
