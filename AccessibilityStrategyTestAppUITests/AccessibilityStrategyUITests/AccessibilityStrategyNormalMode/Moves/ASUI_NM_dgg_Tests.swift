da90441739b624cde0a9c7a171fd22c62f2a97e6
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2024-11-03 17:30:56 +0800

    add new PGR NM dgg tests that are now possible coz we have webviews

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_dgg_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_dgg_Tests.swift
@@ -3,7 +3,6 @@ import XCTest
 import Common
 
 
-// there's no way to test PGR for this move. if you can't remember why think harder.
 class ASUI_NM_dgg_Tests: ASUI_NM_BaseTests {
     
     private func applyMoveBeingTested(_ vimEngineState: inout VimEngineState) -> AccessibilityTextElement {
@@ -93,8 +92,52 @@ first non blank limit
         XCTAssertEqual(accessibilityElement.fileText.value, "    😂️f the next line (which means this one)")
         XCTAssertEqual(accessibilityElement.caretLocation, 4)
         XCTAssertEqual(accessibilityElement.selectedLength, 3)
-        
     }
     
 }
  
+
+// PGR and Electron
+extension ASUI_NM_dgg_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_Mode_it_does_delete_or_paste_in_UI_Elements_receptive_to_PGR() {
+        let textInAXFocusedElement = """
+  ok so now we're having multiple lines
+and we will NOT be on the last one so after dgg
+deletes a ton of shits the caret will go at the
+first non blank limit
+    😂️f the next line (which means this one)
+"""
+        app.webViews.textViews.firstMatch.tap()
+        app.webViews.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.b(on: $0) }
+        let accessibilityElement = applyMoveBeingTested(appFamily: .pgR)
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, "    😂️f the next line (which means this one)")
+        XCTAssertEqual(accessibilityElement.caretLocation, 4)
+        XCTAssertEqual(accessibilityElement.selectedLength, 3)
+    }
+
+    func test_that_when_it_is_called_in_PGR_Mode_it_does_delete_or_paste_and_once_only_in_UI_Elements_NOT_receptive_to_PGR() {
+        let textInAXFocusedElement = """
+  ok so now we're having multiple lines
+and we will NOT be on the last one so after dgg
+deletes a ton of shits the caret will go at the
+first non blank limit
+    😂️f the next line (which means this one)
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.b(on: $0) }
+        let accessibilityElement = applyMoveBeingTested()
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, "    😂️f the next line (which means this one)")
+        XCTAssertEqual(accessibilityElement.caretLocation, 4)
+        XCTAssertEqual(accessibilityElement.selectedLength, 3)
+    }
+
+}
