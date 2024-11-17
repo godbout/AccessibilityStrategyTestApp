7db449565a5c3843ba5491ebcfb267a20cc6e26b
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2024-11-18 01:14:42 +0800

    fix typo in ASUT NM << comments

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_<<_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_<<_Tests.swift
@@ -2,6 +2,6 @@
 import XCTest
 
 
-// those are like d moves. we need to replace the caret positiona after the delete
+// those are like d moves. we need to replace the caret position after the delete
 // so UIT.
 class ASUT_NM_leftChevronLeftChevron_Tests: ASUT_NM_BaseTests {}
