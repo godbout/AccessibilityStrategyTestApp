8b9e61032722f1584f97ae9d0d561ead996d4d05
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2024-11-25 15:28:31 +0800

    kill a TODO

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_g^_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_g^_Tests.swift
@@ -2,9 +2,6 @@
 import XCTest
 
 
-// TODO: should we keep those UT here for motions that use ScreenLines?
-// forgot if we define them all in UT and then explain that we forward them to UI
-// or if we don't bother writing them in UT first?
 // moved to UI coz ScreenLine
 // even if not ultra necessary coz the move is not
 // using count so we could pass CurrentScreenLine but still.
