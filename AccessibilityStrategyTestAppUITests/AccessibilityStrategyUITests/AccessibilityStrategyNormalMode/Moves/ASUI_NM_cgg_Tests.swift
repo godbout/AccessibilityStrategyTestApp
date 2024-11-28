7dbc77077e69dffc6385f391da9885cc8037eeb4
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2024-11-28 19:45:20 +0800

    move UT NM cgg to UI NM cgg after PG-R Makeoverm

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cgg_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cgg_Tests.swift
@@ -14,6 +14,60 @@ class ASUI_NM_cgg_Tests: ASUI_NM_BaseTests {
 }
 
 
+// Both
+extension ASUI_NM_cgg_Tests {
+    
+    func test_that_it_deletes_the_line_up_to_the_firstNonBlankLimit() {
+        let textInAXFocusedElement = "    this is a single line ‼️‼️‼️"
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        applyMove { asNormalMode.F(to: "s", on: $0) }
+        
+        let accessibilityElement = applyMoveBeingTested()
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, "    ")
+        XCTAssertEqual(accessibilityElement.caretLocation, 4)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement.selectedText, "")
+    }
+    
+}
+
+
+// TextViews
+extension ASUI_NM_cgg_Tests {
+    
+    // this test contains blanks
+    func test_that_it_deletes_from_the_firstNonBlankLimit_of_the_current_line_to_the_end_of_the_TextView() {
+        let textInAXFocusedElement = """
+  blah blah some line
+some more
+haha geh
+need to deal with
+those faces 🥺️☹️😂️
+
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        applyMove { asNormalMode.gg(times: 3, on: $0) }
+        
+        let accessibilityElement = applyMoveBeingTested()
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+  
+need to deal with
+those faces 🥺️☹️😂️
+
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 2)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement.selectedText, "")
+    }
+    
+}
+
+
 // PGR and Electron
 extension ASUI_NM_cgg_Tests {
     
@@ -28,11 +82,11 @@ those faces 🥺️☹️😂️
 """
         app.webViews.textViews.firstMatch.tap()
         app.webViews.textViews.firstMatch.typeText(textInAXFocusedElement)
-        
         applyMove { asNormalMode.gg(on: $0) }
         applyMove { asNormalMode.gj(on: $0) }
         applyMove { asNormalMode.gj(on: $0) }
         applyMove { asNormalMode.f(to: "g", on: $0) }
+        
         let accessibilityElement = applyMoveBeingTested(appFamily: .pgR)
         
         XCTAssertEqual(accessibilityElement.fileText.value, """
