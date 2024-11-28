158e909515cf332f72129e4bbe9e42f6bd8b8d8b
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2024-11-28 19:21:43 +0800

    move UT NM ct to UI NM ct after PG-R Makeoverm

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ct_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_ct_Tests.swift
@@ -14,6 +14,105 @@ class ASUI_NM_ct_Tests: ASUI_NM_BaseTests {
 }
 
 
+// count
+extension ASUI_NM_ct_Tests {
+    
+    func test_that_it_implements_the_count_system() {
+        let textInAXFocusedElement = "here we gonna delete up to 🕑️ characters rather than 🦴️!"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.slash(to: "e up", on: $0) }
+        let accessibilityElement = applyMoveBeingTested(times: 2, to: "e")
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, "here we gonna deleter than 🦴️!")
+        XCTAssertEqual(accessibilityElement.caretLocation, 19)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement.selectedText, "")
+    }
+    
+}
+
+
+// TODO: update all the both to Both coz it's like TextFields and TextViews hehe
+// Both
+extension ASUI_NM_ct_Tests {
+    
+    func test_that_in_normal_setting_it_selects_the_text_from_the_caret_to_before_the_character_found() {
+        let textInAXFocusedElement = "gonna use ct on 🛳️🛳️🛳️🛳️🛳️🛳️ this sentence"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.e(times: 2, on: $0) }
+        let accessibilityElement = applyMoveBeingTested(to: "s")
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, "gonna uss sentence")
+        XCTAssertEqual(accessibilityElement.caretLocation, 8)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement.selectedText, "")
+    }
+    
+    func test_that_if_the_character_is_not_found_then_it_does_nothing() {
+        let textInAXFocusedElement = """
+gonna look
+for a character
+that is not there
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.gk(on: $0) }
+        applyMove { asNormalMode.l(times: 3, on: $0) }
+        
+        let accessibilityElement = applyMoveBeingTested(to: "z")
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+gonna look
+for a character
+that is not there
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 14)
+        XCTAssertEqual(accessibilityElement.selectedLength, 1)
+        XCTAssertEqual(accessibilityElement.selectedText, " ")
+    }
+    
+}
+
+
+// TextViews
+extension ASUI_NM_ct_Tests {
+    
+    func test_that_it_can_find_the_character_on_a_line_for_a_multiline() {
+        let textInAXFocusedElement = """
+ct on a multiline
+should work
+on a line
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.gk(on: $0) }
+        applyMove { asNormalMode.l(on: $0) }
+        
+        let accessibilityElement = applyMoveBeingTested(to: "w")
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+ct on a multiline
+swork
+on a line
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 19)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement.selectedText, "")
+    }
+    
+}
+
+
 // PGR and Electron
 extension ASUI_NM_ct_Tests {
     
