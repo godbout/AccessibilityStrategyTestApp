45c6cd2187b2e2bec7c7c6a37b1777a27eafccd9
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2024-11-28 20:30:55 +0800

    move UT NM cf to UI NM cf after PG-R Makeoverm

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cf_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cf_Tests.swift
@@ -5,10 +5,106 @@ import Common
 
 class ASUI_NM_cf_Tests: ASUI_NM_BaseTests {
     
-    private func applyMoveBeingTested(times count: Int = 1, to character: Character, appFamily: AppFamily) -> AccessibilityTextElement {
+    private func applyMoveBeingTested(times count: Int = 1, to character: Character, appFamily: AppFamily = .auto) -> AccessibilityTextElement {
         var state = VimEngineState(appFamily: appFamily)
         
         return applyMove { asNormalMode.cf(times: count, to: character, on: $0, &state) }
+    }
+    
+}
+
+
+// count
+extension ASUI_NM_cf_Tests {
+    
+    func test_that_it_implements_the_count_system() {
+        let textInAXFocusedElement = "here we gonna delete up to 🕑️ characters rather than 🦴️!"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.slash(to: "e up", on: $0) }
+        
+        let accessibilityElement = applyMoveBeingTested(times: 2, to: "e")
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, "here we gonna deletr than 🦴️!")
+        XCTAssertEqual(accessibilityElement.caretLocation, 19)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement.selectedText, "")
+    }
+    
+}
+
+
+// Both
+extension ASUI_NM_cf_Tests {
+    
+    func test_that_in_normal_setting_it_selects_the_text_from_the_caret_to_the_character_found() {
+        let textInAXFocusedElement = "😂️😂️😂️😂️ gonna use cf on this sentence"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.l(on: $0) }
+        
+        let accessibilityElement = applyMoveBeingTested(to: "s")
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, "😂️e cf on this sentence")
+        XCTAssertEqual(accessibilityElement.caretLocation, 3)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement.selectedText, "")
+    }
+    
+    func test_that_if_the_character_is_not_found_then_it_does_nothing() {
+        let textInAXFocusedElement = """
+gonna look
+for a character
+that is not there
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        applyMove { asNormalMode.zero(on: $0) }
+        applyMove { asNormalMode.gk(on: $0) }
+        applyMove { asNormalMode.l(times: 3, on: $0) }
+        
+        let accessibilityElement = applyMoveBeingTested(to: "z")
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+gonna look
+for a character
+that is not there
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 14)
+        XCTAssertEqual(accessibilityElement.selectedLength, 1)
+        XCTAssertEqual(accessibilityElement.selectedText, " ")
+    }
+    
+}
+
+
+// TextViews
+extension ASUI_NM_cf_Tests {
+    
+    func test_that_it_can_find_the_character_on_a_line_for_a_multiline() {
+        let textInAXFocusedElement = """
+cf on a multiline
+should work
+on a line
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        applyMove { asNormalMode.gg(times: 2, on: $0) }
+        
+        let accessibilityElement = applyMoveBeingTested(to: "w")
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+cf on a multiline
+ork
+on a line
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 18)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement.selectedText, "")
     }
     
 }
