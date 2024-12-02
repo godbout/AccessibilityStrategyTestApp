d63b61414bdbd9e3ba66740edc7ef783220499bb
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2024-12-02 12:42:21 +0800

    fix one ASUI NM da` test that was empty LMAO

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_da`_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_da`_Tests.swift
@@ -16,9 +16,19 @@ class ASUI_NM_daBacktick_Tests: ASUI_NM_BaseTests {
 
 extension ASUI_NM_daBacktick_Tests {
     
-    // TODO: ???
     func test_that_if_no_innerQuotedString_is_found_then_it_does_nothing() {
-        let text = "those shits work on ` single lines not on multiple lines"
+        let textInAXFocusedElement = "those shits work on ` single lines not on multiple lines"
+        app.textFields.firstMatch.tap()
+        app.textFields.firstMatch.typeText(textInAXFocusedElement)
+        applyMove { asNormalMode.l(on: $0) }
+        applyMove { asNormalMode.b(times: 10, on: $0) }
+
+        let accessibilityElement = applyMoveBeingTested()
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, "those shits work on ` single lines not on multiple lines")
+        XCTAssertEqual(accessibilityElement.caretLocation, 6)
+        XCTAssertEqual(accessibilityElement.selectedLength, 1)
+        XCTAssertEqual(accessibilityElement.selectedText, "s")
     }
     
     func test_that_the_block_cursor_is_repositioned_correctly_after_the_deletion() {
