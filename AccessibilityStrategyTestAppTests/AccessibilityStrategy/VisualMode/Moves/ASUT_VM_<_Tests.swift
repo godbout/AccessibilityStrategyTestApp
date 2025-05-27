7cfe993b38404efcc9e4eab6b9a1e2efbc3c92bb
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2025-05-28 00:54:38 +0800

    add wip of ASUT VM <

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/ASUT_VM_<_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/ASUT_VM_<_Tests.swift
@@ -1,8 +1,55 @@
-//
-//  ASUT_VM_<_Tests.swift
-//  AccessibilityStrategyTestAppTests
-//
-//  Created by Guillaume Leclerc on 28/05/2025.
-//
+@testable import AccessibilityStrategy
+import Common
+import XCTest
 
-import Foundation
+
+// TODO: this UT is missing. need to take from UIT
+class ASUT_VM_leftChevron_Tests: ASUT_VM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement, appFamily: AppFamily) -> AccessibilityTextElement {
+        asVisualMode.leftChevron(on: element, VimEngineState(appFamily: appFamily))
+    } 
+    
+}
+
+
+// clipboard management in PGR (coz applyMagicPaste)
+extension ASUT_VM_leftChevron_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_Mode_it_does_not_overwrite_the_Clipboard() {
+        let text = """
+seems that even the normal
+🖕️ase fails LMAO
+some more
+and more
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 63,
+            caretLocation: 27,
+            selectedLength: 19,
+            selectedText: """
+        🖕️ase fails LMAO
+        s
+        """,
+            fullyVisibleArea: 0..<63,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 63,
+                number: 2,
+                start: 27,
+                end: 45
+            )!
+        )
+        copyToClipboard(text: "some fake shit")
+        
+        AccessibilityStrategyVisualMode.anchor = 27
+        AccessibilityStrategyVisualMode.head = 45
+        
+        _ = applyMoveBeingTested(on: element, appFamily: .pgR)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "some fake shit")
+    }
+    
+}
