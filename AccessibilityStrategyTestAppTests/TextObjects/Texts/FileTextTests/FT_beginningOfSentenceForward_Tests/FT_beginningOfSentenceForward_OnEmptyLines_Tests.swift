03e3950c64bfc5e38bffbcea70b18850df56d2d1
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-01-15 18:20:55 +0800

    split beginningOfSentenceForward BL and EL in two files

---
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_beginningOfSentenceForward_Tests/FT_beginningOfSentenceForward_OnEmptyLines_Tests.swift
@@ -0,0 +1,97 @@
+@testable import AccessibilityStrategy
+import XCTest
+
+
+class FT_beginningOfSentenceForward_OnEmptyLines_Tests: XCTestCase {
+    
+    private func applyFuncBeingTested(on text: String, startingAt caretLocation: Int) -> Int {
+        let fileText = FileText(end: text.utf16.count, value: text)
+        
+        return fileText.beginningOfSentenceForward(startingAt: caretLocation)
+    }
+    
+}
+
+
+extension FT_beginningOfSentenceForward_OnEmptyLines_Tests {
+    
+    func test_that_if_the_text_is_empty_then_it_returns_0() {
+        let text = ""
+        
+        let beginningOfSentenceForwardLocation = applyFuncBeingTested(on: text, startingAt: 0)
+        
+        XCTAssertEqual(beginningOfSentenceForwardLocation, 0)
+    }
+    
+    func test_that_if_the_caret_is_on_the_last_EmptyLine_then_it_does_not_move() {
+        let text = """
+this move doesn't bip
+but of course at the last empty line
+it will not move
+
+"""
+        let beginningOfSentenceForwardLocation = applyFuncBeingTested(on: text, startingAt: 76)
+        
+        XCTAssertEqual(beginningOfSentenceForwardLocation, 76)
+    }
+       
+    func test_that_it_does_not_get_stuck_in_the_middle_of_multiple_EmptyLines() {
+        let text = """
+so it's not gonna skip lines but stop
+at paragraph boundaries
+
+
+
+
+
+can check the impl of that
+"""
+        let beginningOfSentenceForwardLocation = applyFuncBeingTested(on: text, startingAt: 62)
+        
+        XCTAssertEqual(beginningOfSentenceForwardLocation, 67)
+    }
+    
+    func test_that_if_the_caret_is_on_an_EmptyLine_and_the_text_ends_by_multiple_EmptyLines_then_it_goes_to_the_end() {
+        let text = """
+plenty of empty lines
+below
+
+
+
+
+
+
+
+"""
+        let beginningOfSentenceForwardLocation = applyFuncBeingTested(on: text, startingAt: 29)
+        
+        XCTAssertEqual(beginningOfSentenceForwardLocation, 34)
+    }
+    
+}
+
+
+// bugs found
+extension FT_beginningOfSentenceForward_OnEmptyLines_Tests {
+
+    func test_that_if_there_are_BlankLines_between_EmptyLines_then_it_stops_on_the_EmptyLine_after_the_last_BlankLine() {
+        let text = """
+so below there's some empty lines
+but also a two blank line in the middle!
+
+
+
+
+
+          
+   
+
+
+hehe
+"""
+        let beginningOfSentenceForwardLocation = applyFuncBeingTested(on: text, startingAt: 77)
+        
+        XCTAssertEqual(beginningOfSentenceForwardLocation, 95)
+    }
+    
+}
