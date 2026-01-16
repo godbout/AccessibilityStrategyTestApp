7e1431dc9f377ab146f0a9c2edb55269f82cbb8b
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-01-16 20:58:38 +0800

    split aParagraph UT into NS EL and BL

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aParagraph_Tests/FT_aParagraph_OnBlankLines_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aParagraph_Tests/FT_aParagraph_OnBlankLines_Tests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 
 // see innerParagraph (yes inner) NormalSetting for blah blah
-class FT_aParagraph_OnEmptyOrBlankLines_Tests: XCTestCase {
+class FT_aParagraph_OnBlankLines_Tests: XCTestCase {
     
     private func applyFuncBeingTested(on text: String, startingAt caretLocation: Int) -> Range<Int>? {
         let fileText = FileText(end: text.utf16.count, value: text)
@@ -14,156 +14,7 @@ class FT_aParagraph_OnEmptyOrBlankLines_Tests: XCTestCase {
 }
 
 
-// on Empty Lines
-// these tests contain Blanks
-extension FT_aParagraph_OnEmptyOrBlankLines_Tests {
-    
-    func test_that_for_an_EmptyLine_it_returns_nil() {
-        let text = ""
-        
-        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 0)
-        
-        XCTAssertNil(aParagraphRange)
-    }
-    
-    func test_that_for_a_single_EmptyLine_before_some_text_it_returns_the_correct_range() {
-        let text = """
-
-  so for innerParagraph blank lines are a boundary. it's an exception.
-"""
-        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 0)
-        
-        XCTAssertEqual(aParagraphRange?.lowerBound, 0)
-        XCTAssertEqual(aParagraphRange?.count, 71) 
-    }
-        
-    func test_that_for_two_EmptyLines_before_some_text_it_returns_the_correct_range() {
-        let text = """
-
-
-  so for innerParagraph blank lines are a boundary. it's an exception.
-"""
-        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 0)
-        
-        XCTAssertEqual(aParagraphRange?.lowerBound, 0)
-        XCTAssertEqual(aParagraphRange?.count, 72) 
-    }
-    
-    func test_that_for_a_single_EmptyLine_after_some_text_it_returns_nil() {
-        let text = """
-so for innerParagraph blank lines are a boundary. it's an exception.   
-
-"""
-        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 72)
-        
-        XCTAssertNil(aParagraphRange)
-    }
-        
-    func test_that_for_two_EmptyLines_after_some_text_it_returns_nil() {
-        let text = """
-so for innerParagraph blank lines are a boundary. it's an exception.
-
-
-"""
-        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 69)
-        
-        XCTAssertNil(aParagraphRange)
-    }
-        
-    func test_that_for_a_single_EmptyLine_surrounded_by_some_text_it_returns_the_correct_range() {
-        let text = """
-so for innerParagraph blank lines are a boundary. it's an exception.  
-
-  so for innerParagraph blank lines are a boundary. it's an exception.
-"""
-        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 71)
-        
-        XCTAssertEqual(aParagraphRange?.lowerBound, 71)
-        XCTAssertEqual(aParagraphRange?.count, 71) 
-    }
-    
-    func test_that_for_a_single_EmptyLine_surrounded_by_some_other_EmptyLines_it_returns_the_correct_range() {
-        let text = """
-some fucking lines
-
-
-some more  
-
- hehe
-
-
-"""
-        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 33)
-        
-        XCTAssertEqual(aParagraphRange?.lowerBound, 33)
-        XCTAssertEqual(aParagraphRange?.count, 7) 
-    }
-        
-    func test_that_for_multiple_EmptyLines_not_surrounded_by_text_returns_nil() {
-        let text = """
-
-
-
-
-"""
-        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 1)
-        
-        XCTAssertNil(aParagraphRange)
-    }
-    
-    func test_that_for_multiple_EmptyLines_before_some_text_it_returns_the_correct_range() {
-        let text = """
-
-
-
-       this text
-is a whole
-paragraph in
-   itself
-"""
-        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 1)
-        
-        XCTAssertEqual(aParagraphRange?.lowerBound, 0)
-        XCTAssertEqual(aParagraphRange?.count, 53) 
-    }
-    
-    func test_that_for_a_multiple_EmptyLines_after_some_text_it_returns_nil() {
-        let text = """
-this text
-is a whole
-paragraph in
-   itself 
-
-
-
-"""
-        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 45)
-        
-        XCTAssertNil(aParagraphRange)
-    }
-    
-    func test_that_for_a_multiple_EmptyLines_between_some_text_it_returns_the_correct_range() {
-        let text = """
-this text
-is a whole 
-
-
-
-  paragraph in
-   itself
-"""
-        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 23)
-        
-        XCTAssertEqual(aParagraphRange?.lowerBound, 22)
-        XCTAssertEqual(aParagraphRange?.count, 27) 
-    }
-    
-}
-
-
-// on Blank Lines
-// these tests contain Blank Lines.
-extension FT_aParagraph_OnEmptyOrBlankLines_Tests {
+extension FT_aParagraph_OnBlankLines_Tests {
 
     func test_that_for_an_BlankLine_it_returns_nil() {
         let text = "                      "
@@ -310,7 +161,7 @@ is a whole
 
 // mix of Empty and Blank Lines
 // see innerParagraph NormalSetting for more blah blah.
-extension FT_aParagraph_OnEmptyOrBlankLines_Tests {
+extension FT_aParagraph_OnBlankLines_Tests {
     
     func test_that_if_the_first_line_contains_Blanks_it_returns_nil() {
         let text = """
---
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aParagraph_Tests/FT_aParagraph_OnEmptyLines_Tests.swift
@@ -0,0 +1,159 @@
+@testable import AccessibilityStrategy
+import XCTest
+
+
+// see innerParagraph (yes inner) NormalSetting for blah blah
+class FT_aParagraph_OnEmptyLines_Tests: XCTestCase {
+    
+    private func applyFuncBeingTested(on text: String, startingAt caretLocation: Int) -> Range<Int>? {
+        let fileText = FileText(end: text.utf16.count, value: text)
+        
+        return fileText.aParagraph(startingAt: caretLocation)
+    }
+    
+}
+
+
+extension FT_aParagraph_OnEmptyLines_Tests {
+    
+    func test_that_for_an_EmptyLine_it_returns_nil() {
+        let text = ""
+        
+        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 0)
+        
+        XCTAssertNil(aParagraphRange)
+    }
+    
+    func test_that_for_a_single_EmptyLine_before_some_text_it_returns_the_correct_range() {
+        let text = """
+
+  so for innerParagraph blank lines are a boundary. it's an exception.
+"""
+        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 0)
+        
+        XCTAssertEqual(aParagraphRange?.lowerBound, 0)
+        XCTAssertEqual(aParagraphRange?.count, 71) 
+    }
+        
+    func test_that_for_two_EmptyLines_before_some_text_it_returns_the_correct_range() {
+        let text = """
+
+
+  so for innerParagraph blank lines are a boundary. it's an exception.
+"""
+        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 0)
+        
+        XCTAssertEqual(aParagraphRange?.lowerBound, 0)
+        XCTAssertEqual(aParagraphRange?.count, 72) 
+    }
+    
+    func test_that_for_a_single_EmptyLine_after_some_text_it_returns_nil() {
+        let text = """
+so for innerParagraph blank lines are a boundary. it's an exception.   
+
+"""
+        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 72)
+        
+        XCTAssertNil(aParagraphRange)
+    }
+        
+    func test_that_for_two_EmptyLines_after_some_text_it_returns_nil() {
+        let text = """
+so for innerParagraph blank lines are a boundary. it's an exception.
+
+
+"""
+        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 69)
+        
+        XCTAssertNil(aParagraphRange)
+    }
+        
+    func test_that_for_a_single_EmptyLine_surrounded_by_some_text_it_returns_the_correct_range() {
+        let text = """
+so for innerParagraph blank lines are a boundary. it's an exception.  
+
+  so for innerParagraph blank lines are a boundary. it's an exception.
+"""
+        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 71)
+        
+        XCTAssertEqual(aParagraphRange?.lowerBound, 71)
+        XCTAssertEqual(aParagraphRange?.count, 71) 
+    }
+    
+    func test_that_for_a_single_EmptyLine_surrounded_by_some_other_EmptyLines_it_returns_the_correct_range() {
+        let text = """
+some fucking lines
+
+
+some more  
+
+ hehe
+
+
+"""
+        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 33)
+        
+        XCTAssertEqual(aParagraphRange?.lowerBound, 33)
+        XCTAssertEqual(aParagraphRange?.count, 7) 
+    }
+        
+    func test_that_for_multiple_EmptyLines_not_surrounded_by_text_returns_nil() {
+        let text = """
+
+
+
+
+"""
+        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 1)
+        
+        XCTAssertNil(aParagraphRange)
+    }
+    
+    func test_that_for_multiple_EmptyLines_before_some_text_it_returns_the_correct_range() {
+        let text = """
+
+
+
+       this text
+is a whole
+paragraph in
+   itself
+"""
+        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 1)
+        
+        XCTAssertEqual(aParagraphRange?.lowerBound, 0)
+        XCTAssertEqual(aParagraphRange?.count, 53) 
+    }
+    
+    func test_that_for_a_multiple_EmptyLines_after_some_text_it_returns_nil() {
+        let text = """
+this text
+is a whole
+paragraph in
+   itself 
+
+
+
+"""
+        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 45)
+        
+        XCTAssertNil(aParagraphRange)
+    }
+    
+    func test_that_for_a_multiple_EmptyLines_between_some_text_it_returns_the_correct_range() {
+        let text = """
+this text
+is a whole 
+
+
+
+  paragraph in
+   itself
+"""
+        let aParagraphRange = applyFuncBeingTested(on: text, startingAt: 23)
+        
+        XCTAssertEqual(aParagraphRange?.lowerBound, 22)
+        XCTAssertEqual(aParagraphRange?.count, 27) 
+    }
+    
+}
