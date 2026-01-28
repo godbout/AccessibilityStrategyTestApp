8e91fb5e03658fc4fb954bed3bd84ac79d6586a9
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-01-28 23:45:04 +0800

    remove Optional in UT aSentence on BL

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_OnBlankLines_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_OnBlankLines_Tests.swift
@@ -4,7 +4,7 @@ import XCTest
 
 class FT_aSentence_OnBlankLines_Tests: XCTestCase {
     
-    private func applyFuncBeingTested(on text: String, startingAt caretLocation: Int) -> Range<Int>? {
+    private func applyFuncBeingTested(on text: String, startingAt caretLocation: Int) -> Range<Int> {
         let fileText = FileText(end: text.utf16.count, value: text)
         
         return fileText.aSentence(startingAt: caretLocation)
