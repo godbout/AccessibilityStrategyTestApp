1d9b4187386f556878636f45701a45afe603782d
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-02 17:32:50 +0800

    update UT aSentence func name to add not including newline

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_NormalSetting_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_NormalSetting_Tests.swift
@@ -136,7 +136,7 @@ and another one.
         XCTAssertEqual(innerSentenceRange.count, 14) 
     }
     
-    func test_that_if_the_caret_is_on_the_last_sentence_of_the_text_that_is_separated_from_another_sentence_above_by_a_newline_then_it_returns_from_the_beginning_of_the_last_sentence_to_the_end_of_the_text() {
+    func test_that_if_the_caret_is_on_the_last_sentence_of_the_text_that_is_separated_from_another_sentence_above_by_a_newline_then_it_returns_from_the_beginning_of_the_last_sentence_not_including_the_newline_to_the_end_of_the_text() {
         let text = """
 this is a line.
 then one more.
