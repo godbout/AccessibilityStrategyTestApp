fdb18db611ba67743acf256997c8f84a2fc65d2f
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-03 23:07:31 +0800

    move EL test to proper aSentence EL file

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_OnEmptyLines_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_OnEmptyLines_Tests.swift
@@ -29,3 +29,20 @@ extension FT_aSentence_OnEmptyLines_Tests {
 
 
 // TODO: already found some EL that fail lol. single EL
+
+
+// TODO: this is an EL test!
+//    func test_that_if_the_caret_is_on_the_first_line_of_the_text_that_is_an_EmptyLine_then_it_returns_from_the_beginning_of_the_text_to_the_end_of_the_second_line_not_including_the_trailing_newline() {
+//        let text = """
+//
+//this is a line.
+//then one more.
+//and another one.
+//"""
+//        
+//        let aSentenceRange = applyFuncBeingTested(on: text, startingAt: 0)
+//        
+//        XCTAssertEqual(aSentenceRange.lowerBound, 0)
+//        XCTAssertEqual(aSentenceRange.count, 16) 
+//    }
+    
