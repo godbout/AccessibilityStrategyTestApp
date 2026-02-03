fda66d10aee554969cfff2a12315b090029f6ee1
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-03 23:03:50 +0800

    add FL nextNonWhitespace Tests file, TODO for now

---
+++ AccessibilityStrategyTestAppTests/TextObjects/Lines/FileLineTests/FL_nextNonWhitespace_Tests.swift
@@ -0,0 +1,1 @@
+// TODO: do the tests if we find out we really need this func (after implementing `as` and `is`)
---
+++ AccessibilityStrategyTestAppTests/TextObjects/Lines/FileLineTests/FL_previousNonWhitespace_Tests.swift
@@ -0,0 +1,8 @@
+//
+//  FL_previousNonWhitespace_Tests.swift
+//  AccessibilityStrategyTestAppTests
+//
+//  Created by Guillaume Leclerc on 03/02/2026.
+//
+
+import Foundation
