fdd23b55d36575bb2869ff7d4bf4e630ef14d5d0
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-03 23:11:13 +0800

    add a whole bunch of UT cases for innerSentence

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentence_NormalSetting_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentence_NormalSetting_Tests.swift
@@ -108,3 +108,96 @@ extension FT_innerSentence_NormalSetting_Tests {
     }
     
 }
+
+
+// TextViews
+// basic
+extension FT_innerSentence_NormalSetting_Tests {
+    
+    func test_that_if_the_caret_is_on_a_sentence_separated_by_newlines_then_it_returns_from_the_beginning_of_that_sentence_to_the_end_of_that_sentence_not_including_the_leading_and_trailing_newlines() {
+        let text = """
+this is a line.
+then one more.
+and another one.
+"""
+        
+        let aSentenceRange = applyFuncBeingTested(on: text, startingAt: 26)
+        
+        XCTAssertEqual(aSentenceRange.lowerBound, 16)
+        XCTAssertEqual(aSentenceRange.count, 14) 
+    }
+    
+    func test_that_if_the_caret_is_on_a_sentence_separated_by_newlines_and_that_there_are_blanks_between_the_leading_newline_and_the_previous_dot_then_it_returns_from_the_beginning_of_that_sentence_including_the_leading_blanks_and_the_leading_newline_to_the_end_of_that_sentence_not_including_the_trailing_newline() {
+        let text = """
+this is a line.  
+then one more.
+and another one.
+"""
+        
+        let aSentenceRange = applyFuncBeingTested(on: text, startingAt: 27)
+        
+        XCTAssertEqual(aSentenceRange.lowerBound, 18)
+        XCTAssertEqual(aSentenceRange.count, 14) 
+    }
+    
+    func test_that_if_the_caret_is_on_the_last_sentence_of_the_text_that_is_separated_from_another_sentence_above_by_a_newline_then_it_returns_from_the_beginning_of_the_last_sentence_not_including_the_leading_newline_to_the_end_of_the_text() {
+        let text = """
+this is a line.
+then one more.
+and another one.
+"""
+        
+        let aSentenceRange = applyFuncBeingTested(on: text, startingAt: 41)
+        
+        XCTAssertEqual(aSentenceRange.lowerBound, 31)
+        XCTAssertEqual(aSentenceRange.count, 16) 
+    }
+    
+    func test_that_if_the_caret_is_on_the_last_sentence_of_the_text_that_is_separated_from_another_sentence_above_by_a_newline_and_that_there_are_blanks_between_that_newline_and_the_previous_dot_then_it_returns_from_the_beginning_of_the_last_sentence_including_the_leading_blanks_and_the_leading_newline_to_the_end_of_the_text() {
+        let text = """
+this is a line.
+then one more.  
+and another one.
+"""
+        
+        let aSentenceRange = applyFuncBeingTested(on: text, startingAt: 41)
+        
+        XCTAssertEqual(aSentenceRange.lowerBound, 33)
+        XCTAssertEqual(aSentenceRange.count, 16) 
+    }
+    
+}
+
+
+// TextViews
+// surrounded by EmptyLines
+extension FT_innerSentence_NormalSetting_Tests {
+    
+    func test_that_if_the_caret_is_on_the_second_sentence_of_the_text_and_the_first_line_is_an_EmptyLine_then_it_returns_from_the_beginning_of_the_second_sentence_not_including_the_leading_newline_to_the_end_of_that_second_sentence_not_including_the_trailing_newline() {
+        let text = """
+
+this is a line.
+then one more.
+and another one.
+"""
+        
+        let aSentenceRange = applyFuncBeingTested(on: text, startingAt: 3)
+        
+        XCTAssertEqual(aSentenceRange.lowerBound, 1)
+        XCTAssertEqual(aSentenceRange.count, 15) 
+    }
+    
+    func test_that_if_the_caret_is_on_a_sentence_separated_by_newlines_and_that_there_are_blanks_between_the_leading_newline_and_the_previous_dot_but_also_there_are_trailing_blanks_on_that_sentence_then_it_returns_from_the_beginning_of_that_sentence_not_including_the_leading_blanks_and_the_leading_newline_to_the_end_of_that_sentence_not_including_the_trailing_blanks() {
+        let text = """
+this is a line.  
+then one more.  
+and another one.
+"""
+
+        let aSentenceRange = applyFuncBeingTested(on: text, startingAt: 24)
+
+        XCTAssertEqual(aSentenceRange.lowerBound, 18)
+        XCTAssertEqual(aSentenceRange.count, 14)
+    }
+
+}
