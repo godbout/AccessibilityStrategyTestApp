89c4bb8a2fe24c0399b6e2270026f57e2b191c55
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-07 15:14:12 +0800

    add ASUT cas

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_cas_Tests.swift
@@ -0,0 +1,58 @@
+@testable import AccessibilityStrategy
+import XCTest
+import Common
+
+
+// this uses FT aParagraph that is already heavily tested.
+// so here we have tests specific to the move itself, like Bip, etc.
+class ASUT_NM_cas_Tests: ASUT_NM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement, _ vimEngineState: inout VimEngineState) -> AccessibilityTextElement {
+        return asNormalMode.cas(on: element, &vimEngineState) 
+    }
+    
+}
+
+
+// Bip, copy deletion and LYS
+extension ASUT_NM_cas_Tests {
+    
+    func test_that_it_always_does_not_Bip_and_sets_the_LastYankStyle_to_Characterwise_and_copies_the_deletion() {
+        let text = """
+ok so here
+
+we're gonna deal with sentences. and shit. lol.
+are you OK??
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 72,
+            caretLocation: 23,
+            selectedLength: 1,
+            selectedText: """
+         
+        """,
+            fullyVisibleArea: 0..<72,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 72,
+                number: 3,
+                start: 12,
+                end: 60
+            )!
+        )
+        copyToClipboard(text: "some fake shit")
+        
+        var vimEngineState = VimEngineState(lastMoveBipped: true, lastYankStyle: .linewise)
+        _ = applyMoveBeingTested(on: element, &vimEngineState)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), """
+we're gonna deal with sentences. 
+"""
+        )
+        XCTAssertEqual(vimEngineState.lastYankStyle, .characterwise)
+        XCTAssertFalse(vimEngineState.lastMoveBipped)
+    }
+    
+}
---
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_cas_Tests.swift
@@ -0,0 +1,8 @@
+//
+//  ASUI_NM_cas_Tests.swift
+//  AccessibilityStrategyTestAppTests
+//
+//  Created by G. on 07/02/2026.
+//
+
+import Foundation
