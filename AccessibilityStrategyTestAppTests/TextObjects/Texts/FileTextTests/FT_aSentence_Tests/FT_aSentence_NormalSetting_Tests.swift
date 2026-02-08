f530685bc3cac2bb8e490b8061e053e5d8b3df53
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-09 00:33:19 +0800

    add UT for as when no start range found and trailing blanks

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_NormalSetting_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_NormalSetting_Tests.swift
@@ -243,6 +243,19 @@ and another one.
         XCTAssertEqual(aSentenceRange.count, 29)
     }
 
+    func test_that_if_there_is_no_start_range_found_then_it_returns_from_the_beginning_of_the_text_to_the_end_of_the_current_sentence_including_the_trailing_blanks_and_not_including_the_trailing_newline() {
+        let text = """
+this is a line  
+then one more.  
+and another one.
+"""
+
+        let aSentenceRange = applyFuncBeingTested(on: text, startingAt: 25)
+
+        XCTAssertEqual(aSentenceRange.lowerBound, 0)
+        XCTAssertEqual(aSentenceRange.count, 33)
+    }
+
 }
 
 
