50b4a44a41ce303c453aa10898f0912b2f5cdb7c
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-08 16:48:30 +0800

    update UT func name regarding start match to clarify

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentence_NormalSetting_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentence_NormalSetting_Tests.swift
@@ -133,7 +133,7 @@ extension FT_innerSentence_NormalSetting_Tests {
         XCTAssertEqual(innerSentenceRange.count, 13)
     }
     
-    func test_that_we_actually_calculate_the_range_according_to_the_matches_upperBound_and_now_lowerBound_lol() {
+    func test_that_we_actually_calculate_the_range_according_to_the_last_start_match_upperBound_and_not_lowerBound_lol() {
         let text = """
 dumb. and." dumber.
 """
