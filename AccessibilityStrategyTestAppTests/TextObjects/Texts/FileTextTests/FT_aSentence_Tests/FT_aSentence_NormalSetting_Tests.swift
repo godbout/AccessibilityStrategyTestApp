44fa5ed9fcfdf43632559c15285d7d1b38ff013b
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-09 19:37:16 +0800

    add US as NS check begin of paragraph boundary!

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_NormalSetting_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_NormalSetting_Tests.swift
@@ -304,4 +304,20 @@ and another one.
         XCTAssertEqual(aSentenceRange.count, 15) 
     }
     
+    // TODO: failing. does as need the paragraph boundary check too??? :D
+    func test_that_if_there_is_no_start_range_found_then_it_returns_from_the_beginning_of_paragraph_backward_boundary_to_the_end_of_the_current_sentence() {
+        let text = """
+first line hehe
+
+above is an EL!
+which is a paragraph boundary which
+is also a sentence boundary!
+"""
+        
+        let innerSentence = applyFuncBeingTested(on: text, startingAt: 26)
+        
+        XCTAssertEqual(innerSentence.lowerBound, 17)
+        XCTAssertEqual(innerSentence.count, 15)
+    }
+        
 }
