2328599969f4256929e239680cb6a7a0141569cf
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-10 18:00:07 +0800

    ad UTs to is NS onBlank, coming from NonBlank, for TV surrounded by BL

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_NormalSetting_Tests/FT_aSentence_NormalSetting_onBlank_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_NormalSetting_Tests/FT_aSentence_NormalSetting_onBlank_Tests.swift
@@ -322,5 +322,19 @@ is also a sentence boundary!
 // TextViews
 // surrounded by BlankLines
 extension FT_aSentence_NormalSetting_onBlank_Tests {
+    
+    func test_that_if_there_is_no_start_range_found_then_it_does_not_stop_at_BlankLines_and_returns_from_the_beginning_of_the_text_to_the_end_of_the_current_sentence_including_the_trailing_blanks_but_not_the_trailing_newline() {
+        let text = """
+first line hehe
+       
+above is an BL not an EL!  
+and BL are NOT sentence boundaries!
+"""
+
+        let aSentenceRange = applyFuncBeingTested(on: text, startingAt: 38)
+
+        XCTAssertEqual(aSentenceRange.lowerBound, 0)
+        XCTAssertEqual(aSentenceRange.count, 51)
+    }
 
 }
