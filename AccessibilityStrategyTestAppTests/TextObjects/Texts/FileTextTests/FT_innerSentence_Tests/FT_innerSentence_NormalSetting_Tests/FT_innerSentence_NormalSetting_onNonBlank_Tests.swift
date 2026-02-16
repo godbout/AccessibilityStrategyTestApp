8a6f6c7952d53a6a917c832d56a536b58d830b54
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-16 09:36:07 +0800

    add UT is NS ONB for leading blanks

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentence_NormalSetting_Tests/FT_innerSentence_NormalSetting_onNonBlank_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentence_NormalSetting_Tests/FT_innerSentence_NormalSetting_onNonBlank_Tests.swift
@@ -91,6 +91,14 @@ dumb. and." dumber.
         XCTAssertEqual(innerSentenceRange.count, 10)
     }
     
+    func test_that_for_the_first_sentence_of_the_text_it_returns_from_the_start_of_sentence_including_the_leading_blanks_to_the_end_of_the_first_sentence_not_including_the_trailing_blank() {
+        let text = "   dumb. and dumber"  
+        let innerSentenceRange = applyFuncBeingTested(on: text, startingAt: 6)
+        
+        XCTAssertEqual(innerSentenceRange.lowerBound, 0)
+        XCTAssertEqual(innerSentenceRange.count, 8)
+    }
+    
 }
 
 
