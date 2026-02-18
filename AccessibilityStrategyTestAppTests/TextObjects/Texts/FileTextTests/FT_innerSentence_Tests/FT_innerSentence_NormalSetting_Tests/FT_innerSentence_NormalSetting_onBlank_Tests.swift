e5b6ba844845c260b91f86304caa623ee30d1fe0
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-18 16:32:49 +0800

    fix UT func name from last commit

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentence_NormalSetting_Tests/FT_innerSentence_NormalSetting_onBlank_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentence_NormalSetting_Tests/FT_innerSentence_NormalSetting_onBlank_Tests.swift
@@ -515,7 +515,7 @@ is also a sentence boundary!
         XCTAssertEqual(innerSentenceRange.count, 15)
     }
     
-    func test1_that_if_there_is_no_end_range_found_then_it_returns_from_the_beginning_of_the_current_sentence_to_end_of_the_current_sentence_not_including_any_trailing_newline_or_following_blanks() {
+    func test_that_if_there_is_no_end_range_found_then_it_returns_from_the_beginning_of_the_current_sentence_to_end_of_the_current_sentence_not_including_any_trailing_newline_or_following_blanks() {
         let text = """
 this is a line.
 then one more.
