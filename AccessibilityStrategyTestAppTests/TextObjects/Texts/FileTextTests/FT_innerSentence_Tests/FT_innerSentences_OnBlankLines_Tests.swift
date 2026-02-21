4f3f95acf161cb042d37110d35ff249c1b939785
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-21 14:14:33 +0800

    add UT for is on BlankLine

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentences_OnBlankLines_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentences_OnBlankLines_Tests.swift
@@ -60,4 +60,21 @@ is also a sentence boundary!
         XCTAssertEqual(innerSentenceRange.count, 35)
     }
     
+    func test_that_if_there_is_a_start_range_found_but_that_it_is_before_a_bunch_of_BlankLines_then_it_returns_from_the_beginning_of_that_group_of_BlankLines_to_the_end_of_that_group_of_BlankLines_not_including_the_trailing_newline() {
+        let text = """
+first line hehe.
+  
+    
+      
+above is an EL!  
+which is a paragraph boundary which
+is also a sentence boundary!
+"""
+        
+        let innerSentenceRange = applyFuncBeingTested(on: text, startingAt: 21)
+        
+        XCTAssertEqual(innerSentenceRange.lowerBound, 17)
+        XCTAssertEqual(innerSentenceRange.count, 14)
+    }
+    
 }
