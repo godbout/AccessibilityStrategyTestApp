c3520a53cac537792b99ad42d9b77918e4ed01ed
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-21 14:02:09 +0800

    add first UT for is on BlankLine :D

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentences_OnBlankLines_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentences_OnBlankLines_Tests.swift
@@ -11,3 +11,22 @@ class FT_innerSentence_OnBlankLines_Tests: XCTestCase {
     }
     
 }
+
+
+extension FT_innerSentence_OnBlankLines_Tests {
+    
+    func test_that_if_the_caret_is_on_the_first_BlankLine_the_text_then_it_returns_from_the_beginning_of_the_text_to_the_end_of_the_first_sentence_with_characters_not_including_the_trailing_newline() {
+        let text = """
+      
+this is a line.
+then one more.
+and another one.
+"""
+        
+        let innerSentenceRange = applyFuncBeingTested(on: text, startingAt: 3)
+        
+        XCTAssertEqual(innerSentenceRange.lowerBound, 0)
+        XCTAssertEqual(innerSentenceRange.count, 22) 
+    }
+    
+}
