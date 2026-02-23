315f8c805d68aa3bcfa474527b59749173a8c9d9
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-02-23 19:29:35 +0800

    add first UT for as OnBL

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_OnBlankLines_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_aSentence_Tests/FT_aSentence_OnBlankLines_Tests.swift
@@ -13,4 +13,20 @@ class FT_aSentence_OnBlankLines_Tests: XCTestCase {
 }
 
 
-// TODO: probably we wanna add simple BL here too first
+extension FT_aSentence_OnBlankLines_Tests {
+    
+    func test_that_if_the_caret_is_on_the_second_sentence_of_the_text_and_the_first_line_is_a_BlankLine_then_it_returns_from_the_beginning_of_the_text_to_the_end_of_that_second_sentence_not_including_the_trailing_newline() {
+        let text = """
+      
+this is a line.
+then one more.
+and another one.
+"""
+        
+        let aSentenceRange = applyFuncBeingTested(on: text, startingAt: 3)
+        
+        XCTAssertEqual(aSentenceRange.lowerBound, 0)
+        XCTAssertEqual(aSentenceRange.count, 22) 
+    }
+    
+}
