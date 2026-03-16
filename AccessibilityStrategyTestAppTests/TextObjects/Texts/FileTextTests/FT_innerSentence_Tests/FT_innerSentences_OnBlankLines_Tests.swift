31691a54f908a90b052f591735909fc93ed6514d
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-03-16 18:48:29 +0800

    add UT is regarding three ELs at end of text

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentences_OnBlankLines_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentences_OnBlankLines_Tests.swift
@@ -238,6 +238,24 @@ and another one
      
 
 
+"""
+            
+        let aSentenceRange = applyFuncBeingTested(on: text, startingAt: 57)
+        
+        XCTAssertEqual(aSentenceRange.lowerBound, 53)
+        XCTAssertEqual(aSentenceRange.count, 8) 
+    }
+    
+    func test_that_if_the_text_ends_with_three_EmptyLines_below_a_group_of_BlankLines_and_that_before_them_there_is_also_an_EmptyLine_then_same_as_above_it_returns_from_the_beginning_of_the_EmptyLine_to_the_first_Blank_of_the_next_line_included() {
+        let text = """
+this is a line.
+then one more.
+and another one  
+  
+
+  
+     
+
 
 
 """
