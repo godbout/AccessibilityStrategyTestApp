fcbea723bf3de704832cb6b32eb5f8b8dc767781
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-04-06 12:43:57 +0800

    remove TODO in UT innerSentence on EL coz done :D

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentence_OnEmptyLines_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentence_OnEmptyLines_Tests.swift
@@ -165,8 +165,6 @@ and another one.
         XCTAssertEqual(innerSentenceRange.count, 3) 
     }
 
-    // TODO: FR this one is failing and also see Things to think if we actually bother doing all those ones for now
-    // or if we jump direcly on the `as` Basic
     func test_that_if_the_text_starts_with_multiple_EmptyLines_and_the_caret_is_two_lines_above_the_first_normal_line_then_it_returns_from_the_beginning_of_the_EmptyLine_where_the_caret_is_to_the_last_EmptyLine_included_that_is_before_a_normal_line_and_that_means_that_it_does_NOT_include_the_leading_blanks_from_the_following_normal_line() {
         let text = """
 
