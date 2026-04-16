e706c303e09669ae81ac9044fb03cf930e8b0e97
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-04-16 10:46:32 +0800

    add UT for NM ccg^ when caret is at FirstNonBlankLimit

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Supermen/ASUT_NM_ccg^_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Supermen/ASUT_NM_ccg^_Tests.swift
@@ -19,16 +19,41 @@ class ASUT_NM_ccgCaret_Tests: ASUT_NM_BaseTests {
 }
 
 
-// TODO: FR
-
-
 // Bip, copy deletion and LYS
 extension ASUT_NM_ccgCaret_Tests {
-       
-}
-
-
-// TextFields and TextViews
-extension ASUT_NM_ccgCaret_Tests {
-
+    
+    func test_that_if_the_caret_is_at_the_FirstNonBlankLimit_it_does_not_Bip_and_does_not_change_the_LastYankStyle_and_does_not_copy_anything() {
+        let text = """
+    hello dear friend
+   😂️hat's some text
+  and also some more
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 64,
+            caretLocation: 25,
+            selectedLength: 3,
+            selectedText: """
+        😂️
+        """,
+            fullyVisibleArea: 0..<64,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 64,
+                number: 2,
+                start: 22,
+                end: 44
+            )!
+        )
+        copyToClipboard(text: "some fake shit")
+        
+        var vimEngineState = VimEngineState(lastMoveBipped: true, lastYankStyle: .linewise)
+        _ = applyMoveBeingTested(on: element, &vimEngineState)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "some fake shit")
+        XCTAssertEqual(vimEngineState.lastYankStyle, .linewise)
+        XCTAssertFalse(vimEngineState.lastMoveBipped)
+    }
+    
 }
