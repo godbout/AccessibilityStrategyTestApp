3a702773a00102f4dfac1a7a2e47e262da555bdc
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-04-18 07:30:43 +0800

    add UT for ASNM cg^

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Moves/ASUT_NM_cg^_Tests.swift
@@ -0,0 +1,6 @@
+@testable import AccessibilityStrategy
+import XCTest
+
+
+// see cg$ for blah blah
+class ASUT_NM_cgCaret_Tests: ASUT_NM_BaseTests {}
---
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Supermen/ASUI_NM_ccg^_Tests.swift
@@ -0,0 +1,8 @@
+//
+//  ASUI_NM_ccg^_Tests.swift
+//  AccessibilityStrategyTestAppTests
+//
+//  Created by G. on 17/04/2026.
+//
+
+import Foundation
