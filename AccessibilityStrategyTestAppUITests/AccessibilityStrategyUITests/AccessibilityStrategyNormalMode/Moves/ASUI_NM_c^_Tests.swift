819da02fb71a03b569c607b3b815c56a02701723
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-04-18 13:37:14 +0800

    add UIT for ASNM c^

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_c^_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_c^_Tests.swift
@@ -1,8 +1,64 @@
-//
-//  ASUI_NM_c^_Tests.swift
-//  AccessibilityStrategyTestAppTests
-//
-//  Created by G. on 18/04/2026.
-//
+@testable import AccessibilityStrategy
+import XCTest
+import Common
 
-import Foundation
+
+class ASUI_NM_cCaret_Tests: ASUI_NM_BaseTests {
+    
+    private func applyMoveBeingTested() -> AccessibilityTextElement {
+        var vimEngineState = VimEngineState(appFamily: .pgR)
+        
+        return applyMove { asNormalMode.cCaret(on: $0, &vimEngineState) }
+    }
+    
+}
+
+  
+// PGR and Electron + passing right LineType parameter to superman func
+extension ASUI_NM_cCaret_Tests {
+    
+    func test_that_when_it_is_called_in_PGR_Mode_it_does_delete_or_paste_in_UI_Elements_receptive_to_PGR_and_also_passes_the_right_LineType_parameter_to_the_superman_func() {
+        let textInAXFocusedElement = """
+  C will now work with file lines     and is supposed to delete from the caret ☀️ to before the linefeed
+and of course this is in the case there is a linefeed at the end of the line.
+"""
+        app.webViews.textViews.firstMatch.tap()
+        app.webViews.textViews.firstMatch.typeText(textInAXFocusedElement)
+        applyMove { asNormalMode.gg(on: $0) }
+        applyMove { asNormalMode.f(times: 3, to: "t", on: $0) }
+        
+        let accessibilityElement = applyMoveBeingTested()
+                
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+  te from the caret ☀️ to before the linefeed
+and of course this is in the case there is a linefeed at the end of the line.
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 2)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement.selectedText, "")
+    }
+    
+    func test_that_when_it_is_called_in_PGR_Mode_it_does_delete_or_paste_and_once_only_in_UI_Elements_NOT_receptive_to_PGR_and_also_passes_the_right_LineType_parameter_to_the_superman_func() {
+        let textInAXFocusedElement = """
+  C will now work with file lines and is     supposed to delete from the caret ☀️ to before the linefeed
+and of course this is in the case there is a linefeed at the end of the line.
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        applyMove { asNormalMode.gg(on: $0) }
+        applyMove { asNormalMode.f(times: 3, to: "t", on: $0) }
+        
+        let accessibilityElement = applyMoveBeingTested()
+                
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+  te from the caret ☀️ to before the linefeed
+and of course this is in the case there is a linefeed at the end of the line.
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 2)
+        XCTAssertEqual(accessibilityElement.selectedLength, 0)
+        XCTAssertEqual(accessibilityElement.selectedText, "")
+    }
+    
+}
