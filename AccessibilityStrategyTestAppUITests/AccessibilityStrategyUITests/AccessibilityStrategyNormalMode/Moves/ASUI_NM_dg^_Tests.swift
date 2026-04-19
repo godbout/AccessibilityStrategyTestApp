642a1ce919581d9c3735f17fd36d84337da0b640
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-04-19 14:01:10 +0800

    add UIT for ASNM dg^

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_dg^_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Moves/ASUI_NM_dg^_Tests.swift
@@ -1,8 +1,43 @@
-//
-//  ASUI_NM_dg^_Tests.swift
-//  AccessibilityStrategyTestAppTests
-//
-//  Created by G. on 19/04/2026.
-//
+import XCTest
+@testable import AccessibilityStrategy
+import Common
 
-import Foundation
+
+// see superman func for blah blah
+class ASUI_NM_dgCaret_Tests: ASUI_NM_BaseTests {
+
+    private func applyMoveBeingTested() -> AccessibilityTextElement {
+        var vimEngineState = VimEngineState()
+        
+        return applyMove { asNormalMode.dgCaret(on: $0, &vimEngineState) }
+    }
+    
+}
+
+
+// test that we pass the right LineType parameter to superman func
+extension ASUI_NM_dgCaret_Tests {
+        
+    func test_that_a_ScreenLine_and_not_a_FileLine_is_sent_as_parameter_to_the_superman_move() {
+        let textInAXFocusedElement = """
+C will now work with file lines and is supposed to delete from the caret ☀️ to before the linefeed
+   and of course this is       in the case there is a linefeed at the end of the line.
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        applyMove { asNormalMode.G(on: $0) }
+        applyMove { asNormalMode.f(times: 3, to: "e", on: $0) }
+        
+        let accessibilityElement = applyMoveBeingTested()
+                
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+C will now work with file lines and is supposed to delete from the caret ☀️ to before the linefeed
+   and of course this is       e there is a linefeed at the end of the line.
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 130)
+        XCTAssertEqual(accessibilityElement.selectedLength, 1)
+        XCTAssertEqual(accessibilityElement.selectedText, "e")
+    }
+    
+}
