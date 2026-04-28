782a7771cd860cdfa9f55ed756eca4ff35cdf990
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-04-28 22:43:00 +0800

    add first UT for ASNM ddg$ for EL and new copy and last yank style behaviors

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Supermen/ASUT_NM_ddg$_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/NormalMode/Supermen/ASUT_NM_ddg$_Tests.swift
@@ -1,6 +1,57 @@
 @testable import AccessibilityStrategy
 import XCTest
+import Common
 
 
-// UIT, PGR
-class ASUT_NM_ddg$_Tests: ASUT_NM_BaseTests {}
+// TODO: FR add some blah blah here?
+class ASUT_NM_ddg$_Tests: ASUT_NM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement) -> AccessibilityTextElement {
+        var vimEngineState = VimEngineState(appFamily: .auto)
+        
+        return applyMoveBeingTested(on: element, &vimEngineState)
+    }
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement, _ vimEngineState: inout VimEngineState) -> AccessibilityTextElement {
+        return asNormalMode.ddgDollarSign(using: element.currentFileLine, on: element, &vimEngineState) 
+    }
+
+}
+
+
+// Bip, copy deletion and LYS
+extension ASUT_NM_ddg$_Tests {
+    
+    func test_that_when_it_is_on_an_EmptyLine_it_does_not_Bip_and_does_not_change_the_LastYankStyle_to_Characterwise_and_does_not_copy_anything() {
+        let text = """
+hehe
+
+hoho
+"""
+        let element = AccessibilityTextElement(
+            role: .textArea,
+            value: text,
+            length: 10,
+            caretLocation: 5,
+            selectedLength: 0,
+            selectedText: "",
+            fullyVisibleArea: 0..<10,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 10,
+                number: 2,
+                start: 5,
+                end: 6
+            )!
+        )
+        copyToClipboard(text: "some fake shit")
+        
+        var vimEngineState = VimEngineState(lastMoveBipped: true, lastYankStyle: .linewise)
+        _ = applyMoveBeingTested(on: element, &vimEngineState)
+        
+        XCTAssertEqual(NSPasteboard.general.string(forType: .string), "some fake shit")
+        XCTAssertEqual(vimEngineState.lastYankStyle, .linewise)
+        XCTAssertFalse(vimEngineState.lastMoveBipped)
+    }
+    
+}
