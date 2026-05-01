b8753c872737e1e6fd08aaf6e73874af90f4736d
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-05-01 20:09:16 +0800

    add new UIT TextViews for ASNM ddg$

--- AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Supermen/ASUI_NM_ddg$_Tests.swift
+++ AccessibilityStrategyTestAppUITests/AccessibilityStrategyUITests/AccessibilityStrategyNormalMode/Supermen/ASUI_NM_ddg$_Tests.swift
@@ -36,3 +36,30 @@ extension ASUI_NM_ddgDollarSign_Tests {
     }
     
 }
+
+// TextViews
+extension ASUI_NM_ddgDollarSign_Tests {
+    
+    func test_that_if_a_file_line_ends_with_a_Newline_it_deletes_from_the_caret_to_before_that_Newline() {
+        let textInAXFocusedElement = """
+C will now work with file lines and is supposed to del☀️te from the caret ☀️ to before the linefeed
+and of course this is in the case there is a linefeed at the end of the line.
+"""
+        app.textViews.firstMatch.tap()
+        app.textViews.firstMatch.typeText(textInAXFocusedElement)
+        applyMove { asNormalMode.gg(on: $0) }
+        applyMove { asNormalMode.f(times: 3, to: "t", on: $0) }
+        
+        let accessibilityElement = applyMoveBeingTested()
+        
+        XCTAssertEqual(accessibilityElement.fileText.value, """
+C will now work with file lines and is supposed to del☀️
+and of course this is in the case there is a linefeed at the end of the line.
+"""
+        )
+        XCTAssertEqual(accessibilityElement.caretLocation, 54)
+        XCTAssertEqual(accessibilityElement.selectedLength, 2)
+        XCTAssertEqual(accessibilityElement.selectedText, "☀️")
+    }
+    
+}
