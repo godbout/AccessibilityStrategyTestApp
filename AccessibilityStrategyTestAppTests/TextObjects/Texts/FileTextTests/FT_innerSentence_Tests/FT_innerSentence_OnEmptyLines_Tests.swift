e2cb3a5786c04aaa11b697b49e22cd1c392c30be
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-05-05 12:13:53 +0800

    add new UT in innerSentence on EL coz new case found when only one trailing Blank lol

--- AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentence_OnEmptyLines_Tests.swift
+++ AccessibilityStrategyTestAppTests/TextObjects/Texts/FileTextTests/FT_innerSentence_Tests/FT_innerSentence_OnEmptyLines_Tests.swift
@@ -104,6 +104,21 @@ and another one
         XCTAssertEqual(innerSentenceRange.count, 2) 
     }
     
+    // TODO: FR name
+    func test_wot() {
+        let text = """
+this is a line.
+then one more.
+and another one 
+
+"""
+        
+        let innerSentenceRange = applyFuncBeingTested(on: text, startingAt: 48)
+        
+        XCTAssertEqual(innerSentenceRange.lowerBound, 46)
+        XCTAssertEqual(innerSentenceRange.count, 2) 
+    }
+    
     func test_that_if_there_is_no_end_range_found_and_that_there_are_trailing_Blanks_on_the_previous_line_then_it_returns_a_range_from_the_last_NonBlank_character_of_the_previous_line_not_included_to_the_trailing_newline_of_the_previous_line_not_included() {
         let text = """
 this is a line.
