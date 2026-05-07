cb73be0c72cc055dd834d5cf885278b19ac70d1a
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-05-07 13:40:12 +0800

    add second UT for VMC is

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC_is_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC_is_Tests.swift
@@ -47,5 +47,36 @@ extension ASUT_VMC_is_Tests {
         XCTAssertEqual(returnedElement.selectedLength, 30)
         XCTAssertNil(returnedElement.selectedText)
     }
+    
+    func test_that_if_the_Head_is_before_the_Anchor_it_extends_the_selection_to_the_beginning_of_the_inner_sentence_where_the_Head_is() {
+        let text = "brother. we're gonna need some sentences here. else it's not gonna work!"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 72,
+            caretLocation: 16,
+            selectedLength: 8,
+            selectedText: """
+        onna nee
+        """,
+            fullyVisibleArea: 0..<72,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 72,
+                number: 1,
+                start: 0,
+                end: 72
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 23
+        AccessibilityStrategyVisualMode.head = 16
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement.caretLocation, 9)
+        XCTAssertEqual(returnedElement.selectedLength, 15)
+        XCTAssertNil(returnedElement.selectedText)
+    }
 
 }
