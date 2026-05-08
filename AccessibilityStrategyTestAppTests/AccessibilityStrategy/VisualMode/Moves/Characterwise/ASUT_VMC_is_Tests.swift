681e87640e311246a3411f33ade42ce0c49eca05
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-05-08 13:55:20 +0800

    add UT for VMC is blocked when on end bound

--- AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC_is_Tests.swift
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Characterwise/ASUT_VMC_is_Tests.swift
@@ -113,73 +113,35 @@ extension ASUT_VMC_is_Tests {
         XCTAssertEqual(AccessibilityStrategyVisualMode.head, 45)
     }
     
-    // TODO: FR do this plus when there's a newline
-//    func test_that_it_does_not_get_blocked_when_the_Head_if_after_the_Anchor_and_the_caret_is_at_the_end_of_an_inner_word() {
-//        let text = """
-//so it seems that calculating the innerWord
-//is going to be different depending
-//on where the anchor and head are positioned
-//in relation to each other
-//"""
-//        let element = AccessibilityTextElement(
-//            role: .textArea,
-//            value: text,
-//            length: 147,
-//            caretLocation: 61,
-//            selectedLength: 6,
-//            selectedText: "ferent",
-//            fullyVisibleArea: 0..<147,
-//            currentScreenLine: ScreenLine(
-//                fullTextValue: text,
-//                fullTextLength: 147,
-//                number: 2,
-//                start: 43,
-//                end: 78
-//            )!
-//        )
-//        
-//        AccessibilityStrategyVisualMode.anchor = 61
-//        AccessibilityStrategyVisualMode.head = 66
-//       
-//        let returnedElement = applyMoveBeingTested(on: element)
-//        
-//        XCTAssertEqual(returnedElement.caretLocation, 61)
-//        XCTAssertEqual(returnedElement.selectedLength, 7)
-//        XCTAssertNil(returnedElement.selectedText)
-//    }
-//    
-//    func test_that_it_does_not_get_blocked_when_the_Head_if_before_the_Anchor_and_the_caret_is_at_the_beginning_of_an_inner_word() {
-//        let text = """
-//so it seems that calculating the innerWord
-//is going to be different depending
-//on where the anchor and head are positioned
-//in relation to each other
-//"""
-//        let element = AccessibilityTextElement(
-//            role: .textArea,
-//            value: text,
-//            length: 147,
-//            caretLocation: 58,
-//            selectedLength: 5,
-//            selectedText: "diffe",
-//            fullyVisibleArea: 0..<147,
-//            currentScreenLine: ScreenLine(
-//                fullTextValue: text,
-//                fullTextLength: 147,
-//                number: 2,
-//                start: 43,
-//                end: 78
-//            )!
-//        )
-//        
-//        AccessibilityStrategyVisualMode.anchor = 62
-//        AccessibilityStrategyVisualMode.head = 58
-//       
-//        let returnedElement = applyMoveBeingTested(on: element)
-//        
-//        XCTAssertEqual(returnedElement.caretLocation, 57)
-//        XCTAssertEqual(returnedElement.selectedLength, 6)
-//        XCTAssertNil(returnedElement.selectedText)
-//    }
-
+    func test_that_it_does_not_get_blocked_when_the_Head_if_after_the_Anchor_and_the_caret_is_at_the_end_of_an_inner_sentence() {
+        let text = "brother. we're gonna need some sentences here. else it's not gonna work!"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 72,
+            caretLocation: 2,
+            selectedLength: 6,
+            selectedText: """
+        other.
+        """,
+            fullyVisibleArea: 0..<72,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 72,
+                number: 1,
+                start: 0,
+                end: 72
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 2
+        AccessibilityStrategyVisualMode.head = 7
+       
+        let returnedElement = applyMoveBeingTested(on: element)
+        
+        XCTAssertEqual(returnedElement.caretLocation, 2)
+        XCTAssertEqual(returnedElement.selectedLength, 7)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+    
 }
