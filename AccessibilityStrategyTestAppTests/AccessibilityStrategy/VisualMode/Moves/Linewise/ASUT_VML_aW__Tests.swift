9e38368f1a140805398ed2e57fa7c200241890e5
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2026-05-09 16:08:35 +0800

    add ASUT VML aW

---
+++ AccessibilityStrategyTestAppTests/AccessibilityStrategy/VisualMode/Moves/Linewise/ASUT_VML_aW__Tests.swift
@@ -0,0 +1,49 @@
+import XCTest
+import AccessibilityStrategy
+import Common
+
+
+class ASUT_VML_aW__Tests: ASUT_VM_BaseTests {
+    
+    private func applyMoveBeingTested(on element: AccessibilityTextElement) -> AccessibilityTextElement {
+        let vimEngineState = VimEngineState(visualStyle: .linewise)
+        
+        return asVisualMode.aW(on: element, vimEngineState)
+    }
+   
+}
+
+
+// nope
+extension ASUT_VML_aW__Tests {
+
+    func test_that_this_move_does_not_exist_for_VisualModeStyle_Linewise() {
+        let text = "       that's some nice text in here yehe"
+        let element = AccessibilityTextElement(
+            role: .textField,
+            value: text,
+            length: 41,
+            caretLocation: 17,
+            selectedLength: 15,
+            selectedText: "e nice text in ",
+            fullyVisibleArea: 0..<41,
+            currentScreenLine: ScreenLine(
+                fullTextValue: text,
+                fullTextLength: 41,
+                number: 1,
+                start: 0,
+                end: 41
+            )!
+        )
+        
+        AccessibilityStrategyVisualMode.anchor = 31
+        AccessibilityStrategyVisualMode.head = 17
+        
+        let returnedElement = applyMoveBeingTested(on: element)
+
+        XCTAssertEqual(returnedElement.caretLocation, 17)
+        XCTAssertEqual(returnedElement.selectedLength, 15)
+        XCTAssertNil(returnedElement.selectedText)
+    }
+
+}
